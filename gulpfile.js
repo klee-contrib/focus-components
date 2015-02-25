10f29c03f514f6685bd9a8dd8933611505f517f3
Pierre <pierre.besson7@gmail.com>
2015-02-25 06:50:28 +0100

    [build] Build file improvement #2

--- gulpfile.js
+++ gulpfile.js
@@ -3,43 +3,53 @@ var gulp = require('gulp');
 //Build the JavaScript source.
 
 
-  var babelify = require("babelify");//es6
-  var browserify = require('browserify');//build the source
-  var source = require('vinyl-source-stream');
-  /**
-   * Build a js file from a given source.
-   * @param {string} directory - directory name
-   * @param {object} options   [description]
-   */
-  function jsBuild(directory, options){
-    options = options || {};
-    var entryFile = './'+ directory + '/'+ (options.entryFile || 'index.js');
-    var generatedFile = './'+ directory + '/'+  (options.generatedFile || "generated.js");
-    return browserify(({entries: [entryFile], extensions: ['.jsx']}))
-    .transform(babelify)
-    .bundle()
-    //Pass desired output filename to vinyl-source-stream
-    .pipe(source(generatedFile))
-    .pipe(gulp.dest('./'+ directory+ '/example/js'));
-  }
-  gulp.task('browserify', function(){
+var babelify = require("babelify"); //es6
+var browserify = require('browserify'); //build the source
+var source = require('vinyl-source-stream');
+/**
+ * Build a js file from a given source.
+ * @param {string} directory - directory name
+ * @param {object} options   [description]
+ */
+function jsBuild(directory, options) {
+	options = options || {};
+	var entryFile = './' + directory + '/' + (options.entryFile || 'index.js');
+	var generatedFile = './' + directory + '/' + (options.generatedFile ||
+		"generated.js");
+	return browserify(({
+			entries: [entryFile],
+			extensions: ['.jsx']
+		}))
+		.transform(babelify)
+		.bundle()
+		//Pass desired output filename to vinyl-source-stream
+		.pipe(source(generatedFile))
+		.pipe(gulp.dest('./' + directory + '/example/js'));
+}
+gulp.task('browserify', function() {
 
 
-  return browserify(({entries: ['./index.js'], extensions: ['.jsx']}))
-  .transform(babelify)
-  .bundle()
-  //Pass desired output filename to vinyl-source-stream
-  .pipe(source('focus-components.js'))
-  .pipe(gulp.dest('./dist/'))
-  .pipe(gulp.dest('./example/js'));
+	return browserify(({
+			entries: ['./index.js'],
+			extensions: ['.jsx']
+		}))
+		.transform(babelify)
+		.bundle()
+		//Pass desired output filename to vinyl-source-stream
+		.pipe(source('focus-components.js'))
+		.pipe(gulp.dest('./dist/'))
+		.pipe(gulp.dest('./example/js'));
 });
-
-//Each component build
-var components =  require('./package.json').components|| [];
-return components.forEach(function(component){
-  return jsBuild(component.path, {entryFile: "index.js", generatedFile: component.name+".js"});
+gulp.task('componentify', function() {
+	//Each component build
+	var components = require('./package.json').components || [];
+	return components.forEach(function(component) {
+		return jsBuild(component.path, {
+			entryFile: "index.js",
+			generatedFile: component.name + ".js"
+		});
+	});
 });
-
 
 
 
