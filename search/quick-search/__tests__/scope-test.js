86116a035f0715df5fbd9df3b1b8986225ee52c7
Pierre <pierre.besson7@gmail.com>
2015-03-01 23:37:12 +0100

    [search][quick-search] Add scope test.

--- search/quick-search/__tests__/scope-test.js
+++ search/quick-search/__tests__/scope-test.js
@@ -4,26 +4,22 @@
 // __tests__/qs-scope-test.js
 var React = require('react/addons');
 jest.dontMock('../scope.js');
+var Scope = React.createClass(require('../scope.js'));
+var TestUtils = React.addons.TestUtils;
+var scopes =  [
+  {code: null, label: "None", style: "qs-scope-none"},
+  {code: 1, label: "Scope1", style: "test2"},
+  {code: 2, label: "Scope2", style: "test3"},
+  {code: 3, label: "Scope3", style: "test4"}
+];
+var scope = 1;
+
+// Render a checkbox with label in the document
+var scope = TestUtils.renderIntoDocument(
+  React.createElement(Scope, {list: scopes, value: scope})
+);
 describe('## QS-Scope', function() {
-  it('The initialize state should not have a list', function() {
-    //
-    var Scope = React.createClass(require('../scope.js'));
-    var TestUtils = React.addons.TestUtils;
-    var scopes =  [
-      {code: null, label: "None", style: "qs-scope-none"},
-      {code: 1, label: "Scope1", style: "test2"},
-      {code: 2, label: "Scope2", style: "test3"},
-      {code: 3, label: "Scope3", style: "test4"}
-    ];
-    var scope = 1;
-
-    // Render a checkbox with label in the document
-    var scope = TestUtils.renderIntoDocument(
-      React.createElement(Scope, {list: scopes, value: scope})
-    );
-    //expect(1).toEqual(1);
-
-
+  it('the initialize state should not have a list', function() {
     // Verify that it's Off by default
     var scopeDown = TestUtils.findRenderedDOMComponentWithClass(
       scope, 'qs-scope-deploy-down');
@@ -36,5 +32,13 @@ describe('## QS-Scope', function() {
       checkbox, 'input');
     TestUtils.Simulate.change(input);
     expect(label.getDOMNode().textContent).toEqual('On');*/
+  });
+  it('the clicked state should have a list', function(){
+    var node = TestUtils.findRenderedDOMComponentWithClass(scope, 'qs-scope');
+    console.log(node);
+    React.addons.TestUtils.Simulate.click(node.getDOMNode());
+    var scopeUp = TestUtils.findRenderedDOMComponentWithClass(
+      scope, 'qs-scope-deploy-up');
+    expect(scopeUp).toBeDefined();
   });
 });
