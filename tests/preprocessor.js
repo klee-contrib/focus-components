839ab20af3fb586cdd0795d67bf1365519375acd
Pierre <pierre.besson7@gmail.com>
2015-03-01 17:03:43 +0100

    [test] Add working test configuration.

--- tests/preprocessor.js
+++ tests/preprocessor.js
@@ -1,9 +1,11 @@
 // preprocessor.js
 //require('harmonize')();
-//require('babel-jest');
+require('babel-jest');
+/*
 var ReactTools = require('react-tools');
 module.exports = {
   process: function(src) {
     return ReactTools.transform(src);
   }
 };
+*/
