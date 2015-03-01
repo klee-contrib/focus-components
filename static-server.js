a3bcfb48b409b45dcbc5edf67081ecd0b126fe82
Pierre <pierre.besson7@gmail.com>
2015-03-01 09:20:08 +0100

    [server] Add static file server foreach component declared in package.json

--- static-server.js
+++ static-server.js
@@ -1,15 +1,19 @@
-var static = require('node-static');
+var components = require('./package.json').components;
+var componentServers = {};
+components.map(function(component) {
+	var pth = '/' + component.path + '/example/';
+	console.log('route', component.name, 'path', pth);
+	componentServers[component.name] = pth; //new static.Server(pth);
+});
 
-//
-// Create a node-static server instance to serve the './public' folder
-//
-var file = new static.Server('./search/quick-search/example/');
+var express = require('express');
+var app = express();
+for (var componentName in componentServers) {
+	//console.log('route', "/" + componentName);
+	//console.log('path', __dirname + componentServers[componentName]);
+	app.use('/' + componentName,
+		express.static(__dirname + componentServers[componentName])
+	);
+}
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    }).resume();
-}).listen(8888);
+app.listen(3000);
