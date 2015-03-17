dd9da4ab74a7d8ce1c1edea503cf452be6fca469
Pierre Besson <pierre.besson7@gmail.com>
2015-03-17 11:09:33 +0100

    [init] Add focus domain initialization.

--- example/js/initFocus.js
+++ example/js/initFocus.js
@@ -1,3 +1,28 @@
+var domain =  {
+  "DO_TEXT": {
+    style: "do_text",
+    type: "text",
+    component: "PapaSinge",
+    validation: [{
+      type: "function",
+      value: function() {
+        return false;
+      }
+    }]
+  },
+  "DO_EMAIL": {
+    style: "do_email",
+    type: "email",
+    component: "PapaMail",
+    validation: [{
+      type: "function",
+      value: function() {
+        return true;
+      }
+    }]
+  }
+};
+focus.definition.domain.container.setAll(domain);
 /*global focus*/
 var entities ={
   "contact": {
