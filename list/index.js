6002b0a84f75b05820ff14c4dc302ef4bf71d6a4
Olivier Boitel <olivier.boitel@kleegroup.com>
2015-03-20 23:04:26 +0100

    [component][list] add timeline

--- list/index.js
+++ list/index.js
@@ -2,5 +2,6 @@ module.exports = {
 	actionBar: require('./action-bar'),
 	actionContextual: require('./action-contextual'),
 	selection: require('./selection'),
-    summary: require('./summary')
+    summary: require('./summary'),
+	timeline: require('./timeline')
 };
