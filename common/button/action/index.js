7a0ba2dc388f907d90a708fa5ba156a3dfea3c57
Pierre <pierre.besson7@gmail.com>
2015-05-04 14:14:23 +0200

    [button] doc and codE.

--- common/button/action/index.js
+++ common/button/action/index.js
@@ -2,53 +2,63 @@ var React = require('react');
 var builder = require('focus').component.builder;
 var Img = require('../../img').component;
 
-/**/
+/**
+ * Mixin button.
+ * @type {Object}
+ */
 var buttonMixin = {
-		getDefaultProps: function getInputDefaultProps() {
-			return {
-				type: 'submit',
-				action: undefined,
-				isPressed: false,
-				style: {},
-                label: undefined,
-                imgSrc: undefined
-			};
-		},
-		handleOnClick: function handleButtonOnclick() {
-			if (this.props.handleOnClick) {
-				return this.props.handleOnClick.apply(this, arguments);
-			}
-			if (!this.props.action || !this.action[this.props.action]) {
-				 console.warn('Your button action is not implemented');
-				return;
-			}
-			return this.action[this.props.action].apply(this, arguments);
-
-		},
-		getInitialState: function () {
-			return {
-				isPressed: this.props.isPressed
-			};
-		},
-		_className: function buttonClassName () {
-			return "btn btn-raised " + (this.props.style.className ? "btn-" + this.props.style.className : "");
-		},
-		renderPressedButton: function () {
-			return ( <button>Loading...</button>);
-        },
-        /**
-         * Render the button.
-         * @return {[type]} [description]
-         */
-        render: function renderInput() {
-            if (this.state.isPressed) {
-                return this.renderPressedButton();
-            }
-            if(this.props.imgSrc) {
-                return <Img src={this.props.imgSrc} onClick={this.handleOnClick} />;
-            }
-            return ( <button href="javascript:void(0)" onClick={this.handleOnClick} type={this.props.type} className={this._className()} >{this.props.label}</button>);
+    /** inheritedDoc */
+    getDefaultProps: function getInputDefaultProps() {
+      return {
+        type: 'submit',
+        action: undefined,
+        isPressed: false,
+        style: {},
+        label: undefined,
+        imgSrc: undefined
+      };
+    },
+    /**
+     * Clickhandler on the button.
+     */
+    handleOnClick: function handleButtonOnclick() {
+      if (this.props.handleOnClick) {
+        return this.props.handleOnClick.apply(this, arguments);
+      }
+      if (!this.props.action || !this.action[this.props.action]) {
+         console.warn('Your button action is not implemented');
+        return;
+      }
+      return this.action[this.props.action].apply(this, arguments);
+    },
+    /** inheritedDoc */
+    getInitialState: function getActionButtonInitialState() {
+      return {
+        isPressed: this.props.isPressed
+      };
+    },
+    /**
+     * ClassName of the button.
+     */
+    _className: function buttonClassName () {
+      return "btn btn-raised " + (this.props.style.className ? "btn-" + this.props.style.className : "");
+    },
+    /**
+    * Render the pressed state of the button.
+    */
+    renderPressedButton: function () {
+      return ( <button>Loading...</button>);
+    },
+    /** inheritedDoc */
+    render: function renderInput() {
+        if (this.state.isPressed) {
+            return this.renderPressedButton();
         }
+        if(this.props.imgSrc) {
+            return <Img src={this.props.imgSrc} onClick={this.handleOnClick} />;
+        }
+        return ( <button href="javascript:void(0)" onClick={this.handleOnClick} type={this.props.type} className={this._className()} >{this.props.label}</button>);
+    }
 };
 
 module.exports = builder(buttonMixin);
