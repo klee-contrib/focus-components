7f928dce7e761a5b244c0199d6657c796f2c641f
Pierre Besson <pierre.besson7@gmail.com>
2015-05-20 15:22:18 +0200

    Update index.js

--- common/index.js
+++ common/index.js
@@ -4,6 +4,7 @@ module.exports = {
   field: require('./field'),
   form: require('./form'),
   img: require('./img'),
+  i18n: require('./i18n'),
   icon: require('./icon'),
   input: require('./input'),
   label: require('./label'),
