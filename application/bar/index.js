d12eeb4944e9414b1f0543fda462fff3656b9970
Pierre Besson <pierre.besson7@gmail.com>
2015-05-22 14:27:49 +0200

    [bar] Add summary inside the bar.

---
+++ application/bar/index.js
@@ -0,0 +1,42 @@
+var builder = require('focus').component.builder;
+var React = require('react');
+var applicationStore = require('focus').application.builtInStore();
+
+var barMixin = {
+  getDefaultProps: function getCartridgeDefaultProps(){
+    return {
+      appName: "",
+      style: {}
+    };
+  },
+  /** @inheriteddoc */
+  getInitialState: function getCartridgeInitialState() {
+    return this._getStateFromStore();
+  },
+  /** @inheriteddoc */
+  componentWillMount: function cartridgeWillMount() {
+    applicationStore.addSummaryComponentChangeListener(this._handleComponentChange);
+  },
+  /** @inheriteddoc */
+  componentWillUnMount: function cartridgeWillUnMount(){
+    applicationStore.removeSummaryComponentChangeListener(this._onComponentChange);
+  },
+  _getStateFromStore: function getCartridgeStateFromStore(){
+    return {summaryComponent: applicationStore.getSummaryComponent() || {component: 'div', props: {}}};
+  },
+  _handleComponentChange: function _handleComponentChangeBarSummary(){
+    this.setState(this._getStateFromStore());
+  },
+  /** @inheriteddoc */
+  render: function renderBar() {
+    var className = `bar ${this.props.style.className}`;
+    return (
+      <div className={className} data-focus='bar'>
+        <div className='applicationName'>{this.props.appName}</div>
+        <this.state.summaryComponent.component {...this.state.summaryComponent.props}/>
+      </div>
+    );
+  }
+};
+
+module.exports = builder(barMixin);
