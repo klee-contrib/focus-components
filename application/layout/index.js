57b115db17198c4e9e08c7fdeba3a77aab83415c
Pierre Besson <pierre.besson7@gmail.com>
2015-06-03 19:34:02 +0200

    [layout] Update left menu from props.

--- application/layout/index.js
+++ application/layout/index.js
@@ -4,11 +4,17 @@ var AppHeader = require('./app-header');
 var stylableBehaviour = require('../../mixin/stylable');
 var contentActionsMixin = {
   mixins: [stylableBehaviour],
+  getDefaultProps: function getDefaultLayoutProps(){
+    return {
+      AppHeader: AppHeader
+    };
+  },
   /** @inheriteddoc */
   render: function renderActions() {
     return (
       <div className={this._getStyleClassName()} data-focus='layout'>
-        <AppHeader />
+        <this.props.AppHeader />
+        <div data-focus='menu-left'><this.props.MenuLeft /></div>
         <div data-focus='page-content'></div>
         <footer data-focus='footer'>Team Focus</footer>
         {this.props.children}
