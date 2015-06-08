fd39a40643ba2a27466eb5fea211eea62056a016
Pierre <pierre.besson7@gmail.com>
2015-06-09 01:42:27 +0200

    [loading-bar] Add to the default layout

--- application/layout/index.js
+++ application/layout/index.js
@@ -1,18 +1,21 @@
 var builder = require('focus').component.builder;
 var React = require('react');
 var AppHeader = require('./app-header');
+var LoadingBar = require('../loading-bar').component;
 var stylableBehaviour = require('../../mixin/stylable');
 var contentActionsMixin = {
   mixins: [stylableBehaviour],
   getDefaultProps: function getDefaultLayoutProps(){
     return {
-      AppHeader: AppHeader
+      AppHeader: AppHeader,
+      LoadingBar: LoadingBar
     };
   },
   /** @inheriteddoc */
   render: function renderActions() {
     return (
       <div className={this._getStyleClassName()} data-focus='layout'>
+        <this.props.LoadingBar />
         <this.props.AppHeader />
         <div data-focus='menu'><this.props.MenuLeft /></div>
         <div data-focus='page-content'></div>
