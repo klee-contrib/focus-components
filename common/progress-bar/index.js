25e3b12a8a31a065c93a25e07e0461fb1ae3ae43
Olivier Boitel <olivier.boitel@kleegroup.com>
2015-06-09 11:01:30 +0200

    [component] codestyle

--- common/progress-bar/index.js
+++ common/progress-bar/index.js
@@ -4,17 +4,16 @@ var React = require('react');
 var builder = require('focus').component.builder;
 
 var progressMixin = {
-
+  /**@inheritDoc**/
   render: function() {
-
     var completed = +this.props.completed;
-    if (completed < 0) {completed = 0};
-    if (completed > 100) {completed = 100};
+    if (completed < 0) {completed = 0; }
+    if (completed > 100) {completed = 100; }
 
     var style = {
       backgroundColor: this.props.color || '#0BD318',
       width: completed + '%',
-      transition: "width 200ms",
+      transition: 'width 200ms',
       height: this.props.height || 10
     };
 
