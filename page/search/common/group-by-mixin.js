36f0519ba0c96e464108d719cec986c83e93876d
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-09 19:38:46 +0200

    [search] Give group the current query

--- page/search/common/group-by-mixin.js
+++ page/search/common/group-by-mixin.js
@@ -82,7 +82,7 @@ let GroupByMixin = {
     renderGroupByBlock(groupKey, list, maxRows) {
         let GroupWrapper = this.props.groupComponent;
         return (
-            <GroupWrapper data-focus="group-result-container" groupKey={groupKey} showAll={this.changeGroupByMaxRows}>
+            <GroupWrapper data-focus="group-result-container" groupKey={groupKey} query={this.state.query} showAll={this.changeGroupByMaxRows}>
                 {this.getSingleTypeResultList(groupKey, list, maxRows)}
             </GroupWrapper>
         );
