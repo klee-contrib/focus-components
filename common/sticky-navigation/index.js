1b7e5bdc9ec8601fb56418bd7acf5b1f1d79cf7b
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-11 15:21:09 +0200

    [sticky-navigation] Finished component

--- common/sticky-navigation/index.js
+++ common/sticky-navigation/index.js
@@ -3,21 +3,63 @@
 let builder = require('focus').component.builder;
 let type = require('focus').component.types;
 
+// Mixins
 
+let Stylabe =  require('../../mixin/stylable');
+
+
+/**
+ * Sticky navigation component.
+ * Listen to a scroll, and sets an active class to the currently displayed element.
+ */
 let StickyNavigation = {
+    /**
+     * Stylable mixin.
+     */
+    mixins: [Stylabe],
+    /**
+     * Get the default props.
+     * By default, listen to the body element
+     * @return {Object} the default properties
+     */
     getDefaultProps() {
         return ({
-            scrollSpyTargetSelector: 'body',
-            titleSelector: '[data-menu]'
+            titleSelector: '[data-menu]',
+            scrolledElementSelector: 'body',
+            triggerHeight: 50
         });
     },
-    componentDidMount() {
-        this._scrollCarrier = document.querySelector(this.props.scrollSpyTargetSelector);
-        this._attachScrollSpy();
+    /**
+     * Props validation
+     */
+    propTypes: {
+        titleSelector: type('string'),
+        scrolledElementSelector: type('string'),
+        triggerHeight: type('number'),
+        style: type('object')
     },
+    /**
+     * Component did mount, attach the scroll spy
+     */
+    componentDidMount() {
+        this.setState({
+            titleList: this._getTitleList()
+        });
+        this._scrollCarrier = this.props.scrollSpyTargetSelector ? document.querySelector(this.props.scrollSpyTargetSelector) : document;
+        this._attachScrollSpy();
+        this._scrollSpy();
+    },
+    /**
+     * Component will unmount, detach the scroll spy
+     */
     componentWillUnmount() {
         this._detachScrollSpy();
     },
+    /**
+     * Get the menu items list
+     * @return {Array} the menu items
+     * @private
+     */
     _getTitleList() {
         let rawTitleList = document.querySelectorAll(this.props.titleSelector);
         return [].map.call(rawTitleList, (titleElement) => {
@@ -28,137 +70,77 @@ let StickyNavigation = {
             }
         });
     },
+    /**
+     * Attach the scroll spy
+     * @private
+     */
     _attachScrollSpy() {
         this._scrollCarrier.addEventListener('scroll', this._scrollSpy);
         this._scrollCarrier.addEventListener('resize', this._scrollSpy);
     },
+    /**
+     * Detach the scroll spy
+     * @private
+     */
     _detachScrollSpy() {
         this._scrollCarrier.removeEventListener('scroll', this._scrollSpy);
         this._scrollCarrier.removeEventListener('resize', this._scrollSpy);
     },
+    /**
+     * The scroll event handler
+     * @private
+     */
     _scrollSpy() {
-
-    },
-    _renderList() {
-        let titleList = this._getTitleList();
-        return titleList.map((title) => {
-            return <div onClick={this._linkClickHandler(title)} key={title.id}>{title.label}</div>;
-        });
-    },
-    _linkClickHandler(title) {
-        return () => {
-            this._scrollCarrier.scrollTop = title.offsetTop;
+        let scrollPosition = document.querySelector(this.props.scrolledElementSelector).scrollTop;
+        if (this.state && this.state.titleList) {
+            this.state.titleList.forEach((title) => {
+                if (Math.abs(title.offsetTop - scrollPosition) < this.props.triggerHeight) {
+                    this.setState({
+                        activeTitle: title.id
+                    });
+                }
+            });
         }
     },
+    /**
+     * Render the items list
+     * @return {XML} the rendered HTML
+     * @private
+     */
+    _renderList() {
+        if (this.state && this.state.titleList) {
+            return this.state.titleList.map((title) => {
+                return (
+                    <div className={this.state.activeTitle == title.id && 'active'} onClick={this._linkClickHandler(title)} key={title.id}>
+                        {title.label}
+                    </div>
+                );
+            });
+        }
+    },
+    /**
+     * Item click handler.
+     * Set the scroll to display the clicked item
+     * @param {Object} title - the clicked item object
+     * @return {Function} hte click handler
+     * @private
+     */
+    _linkClickHandler(title) {
+        return () => {
+            document.querySelector(this.props.scrolledElementSelector).scrollTop = title.offsetTop;
+        }
+    },
+    /**
+     * Render the component
+     * @return {XML} the rendered component
+     */
     render() {
         return (
-            <nav data-focus='sticky-navigation'>
+            <nav data-focus='sticky-navigation' className={this._getStyleClassName()}>
                 {this._renderList()}
             </nav>
         )
     }
 };
 
-
-
-/**
- * Mixin component for the sticky navigation.
- * @type {Object}
- */
-let stickyNavigationMixin = {
-
-    /** @inheritedDoc */
-    displayName: 'sticky-navigation',
-
-    /** @inheritedDoc */
-    getDefaultProps() {
-        return {
-            /**
-             * Selector for the content to be watched.
-             * @type {String}
-             */
-            contentSelector: undefined,
-            /**
-             * Identifier of the Navbar created for the sticky navigation.
-             * @type {String}
-             */
-            navBarId: 'navbar',
-            /**
-             * Selector for the title elements to display in the sticky navigation.
-             * @type {String}
-             */
-            titleSelector: '[data-menu]',
-            /**
-             * Style informations such as the className.
-             * @type {Object}
-             */
-            style: {}
-        };
-    },
-    /** @inheritedDoc */
-    propTypes: {
-      contentSelector: type('string'),
-      navBarId: type('string'),
-      titleSelector: type('string'),
-      style: type('object')
-    },
-    /** @inheritedDoc */
-    getInitialState() {
-      return {menuList: []};
-    },
-    /** @inheritedDoc */
-    componentDidMount() {
-        //The list is processed only when the component is mounted.
-        this._buildMenuList();
-        //Set bootstrap data attributes to register the spy.
-        this._registerScrollSpyAttributes();
-    },
-    /**
-     * Build the menu list from the title attributes read in the selector.
-     */
-    _buildMenuList() {
-      //Get all title elements form the DOM elements read in the selector.
-      let titleListElements = document.querySelectorAll(this.props.titleSelector);
-      let menuList = [];
-      for(let key in titleListElements) {
-          menuList.push(this._renderLink(titleListElements[key]));
-      }
-      //Update the menu list into the state.
-      this.setState({menuList: menuList});
-    },
-    _registerScrollSpyAttributes(){
-      let content = document.querySelector(this.props.contentSelector);
-      content.setAttribute('data-spy', 'scroll');
-      content.setAttribute('data-target', '#' + this.props.navBarId);
-    },
-    /** @inheritedDoc */
-    render() {
-        let className = `bs-docs-sidebar hidden-print affix ${this.props.style.className}`;
-        return(
-          <nav className={className} id={this.props.navBarId} data-focus="sticky-navigation" data-spy="affix" data-offset-top="60" data-offset-bottom="0">
-            <ul className="nav bs-docs-sidenav" role="tablist">
-              {this.state.menuList}
-            </ul>
-          </nav>
-        );
-    },
-    /**
-     * Render the list of links.
-     * @param anchor
-     * @returns {JSX}
-     */
-    _renderLink(title) {
-        if(title.getAttribute) {
-            let active = this.state.currentLink ==  title.getAttribute("id") ? 'active' : '';
-            return (<li><div className={active} onClick={this._linkClick(title.getAttribute("id"))}>{title.innerText}</div></li>);
-        }
-    },
-    _linkClick(id) {
-        return () => {
-            Backbone.history.navigate('#' + id);
-            this.setState({currentLink: id});
-        };
-    }
-};
-
-module.exports =  builder(StickyNavigation);
+module.exports = builder(StickyNavigation);
