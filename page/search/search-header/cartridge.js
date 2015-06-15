869c950ee84df201d6099da76bd263f3d91767b7
Pierre Besson <pierre.besson7@gmail.com>
2015-06-15 22:01:32 +0200

    [search] cartridge , upadte mixi n use

--- page/search/search-header/cartridge.js
+++ page/search/search-header/cartridge.js
@@ -1,13 +1,13 @@
 // Components
-let SearchBar = require('../../../search/search-bar').component;
 var searchHeaderMixin = require('./mixin');
+var React = require('react');
 module.exports = React.createClass({
   mixins: [searchHeaderMixin],
   render() {
       return (
           <div className='cartridge-search' data-focus='cartridge-search'>
             <h1>{this.i18n('search.cartridge.title')}</h1>
-            <SearchBar value={this.props.query} scope={this.props.scope} scopes={this.props.scopeList} loading={this.state.isLoadingSearch} handleChange={this._prepareSearch}/>
+            {this._SearchBarComponent()}
           </div>
       );
   }
