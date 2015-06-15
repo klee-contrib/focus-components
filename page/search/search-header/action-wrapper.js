7ebd422c17938d2429dae428ad82835a0a6872ab
Pierre Besson <pierre.besson7@gmail.com>
2015-06-15 16:06:56 +0200

    [search-wrapper] Fix bug in the wrapper

--- page/search/search-header/action-wrapper.js
+++ page/search/search-header/action-wrapper.js
@@ -2,7 +2,7 @@ var currentTimeout;
 function actionWrapper(searchAction){
   return function(){
     const DEFAULT_TIMEOUT = 5000;
-    var context = this;
+    var context = this || {};
     var args = arguments;
     if(currentTimeout){
       //Cancel previous search action.
