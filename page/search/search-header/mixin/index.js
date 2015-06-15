725db3347d6a03e918706933c9c62502da0656e7
Pierre Besson <pierre.besson7@gmail.com>
2015-06-15 21:59:31 +0200

    [search-header] Rewrite mixin in order to call runSearhc.

--- page/search/search-header/mixin/index.js
+++ page/search/search-header/mixin/index.js
@@ -3,6 +3,8 @@
 let i18nMixin =require('../../../../common/i18n').mixin;
 let searchBehaviour = require('../../common/search-mixin').mixin;
 var searchWrappedAction = require('../action-wrapper');
+let SearchBar = require('../../../../search/search-bar').component;
+var React = require('react');
 
 module.exports = {
     mixins: [i18nMixin, searchBehaviour],
@@ -11,7 +13,21 @@ module.exports = {
         isLoading: false
       }
     },
+    _runSearch(){
+      var criteria = this.refs.searchBar.getValue();
+      return this.props.searchAction(this._buildSearchCriteria(criteria.scope, criteria.query))
+    },
+    _SearchBarComponent(){
+      return <SearchBar
+                ref='searchBar'
+                value={this.props.query}
+                scope={this.props.scope}
+                scopes={this.props.scopeList}
+                loading={this.state.isLoadingSearch}
+                handleChange={this._runSearch}
+              />;
+    },
     componentWillMount(){
-      this._prepareSearch = searchWrappedAction(this.search, this);
+      this._prepareSearch = searchWrappedAction(this._runSearch, this);
     }
 };
