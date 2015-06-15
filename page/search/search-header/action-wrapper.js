377b31a7fc5504fa14927756b9576e3f39a17855
Pierre Besson <pierre.besson7@gmail.com>
2015-06-15 17:51:17 +0200

    [action-wrapper] Add an action wrapper props.

--- page/search/search-header/action-wrapper.js
+++ page/search/search-header/action-wrapper.js
@@ -1,8 +1,8 @@
+const DEFAULT_TIMEOUT = 5000;
 var currentTimeout;
-function actionWrapper(searchAction){
+function actionWrapper(searchAction, context){
   return function(){
-    const DEFAULT_TIMEOUT = 5000;
-    var context = this || {};
+    context = context || this || {};
     var args = arguments;
     if(currentTimeout){
       //Cancel previous search action.
