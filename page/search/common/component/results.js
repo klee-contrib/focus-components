c01777d717d1c8cca8936e1225f554a7fc835c0a
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-26 13:58:34 +0200

    [search] Add results component

---
+++ page/search/common/component/results.js
@@ -0,0 +1,45 @@
+// Dependencies
+
+let builder = require('focus').component.builder;
+let omit = require('lodash/object/omit');
+let map = require('lodash/collection/map');
+
+/**
+ * Results component, used to render the results, grouped or ungrouped
+ * @type {Object}
+ */
+let Results = {
+    /**
+     * Render a single group of results, using the group component given as a prop.
+     * @param  {array} list the results list
+     * @param  {string} key  the group key
+     * @return {HMTL}      the rendered group
+     */
+    _renderSingleGroup(list, key) {
+        return <this.props.groupComponent groupKey={key} list={list} showAllHandler={this.props.showAllHandler}/>;
+    },
+    /**
+     * Render the empty component given as a prop when the result map is empty.
+     * @return {HMTL}      the rendered component
+     */
+    _renderEmptyResults() {
+        return <this.props.emptyComponent/>;
+    },
+    /**
+     * Render the whole component
+     * @return {HMTL}      the rendered component
+     */
+    render() {
+        // If there is no result, render the given empty component
+        if (this.props.totalCount === 0) {
+            return this._renderEmptyResults();
+        }
+        // Filter groups with no results
+        let resultsMap = omit(this.props.resultsMap, (list) => {
+            return list.length === 0;
+        });
+        return map(resultsMap, this._renderSingleGroup);
+    }
+};
+
+module.exports = builder(Results);
