cda1d867bd30ad4bb7ab00f1b6b17631f9c86be8
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-28 22:32:21 +0200

    [search-header] Listen to store changes and trigger search

--- page/search/search-header/mixin/index.js
+++ page/search/search-header/mixin/index.js
@@ -8,8 +8,20 @@ let storeBehaviour = require('../../../../common/mixin/store-behaviour');
 
 let SearchBar = require('../../../../search/search-bar').component;
 
+// Actions
+
+let actionBuilder = require('../../../../search/action-builder');
+
 module.exports = {
     mixins: [i18nMixin, referenceBehaviour, storeBehaviour],
+    getDefaultProps() {
+        return ({
+            action: actionBuilder({ // TODO check if it is possible to call a prop inside the getDefaultProps
+                service: this.props.service,
+                identifier: 'ADVANCED_SEARCH'
+            })
+        });
+    },
     getInitialState() {
         return {
             isLoading: false
@@ -17,13 +29,22 @@ module.exports = {
     },
     componentWillMount() {
         this._loadReference();
+        Focus.search.builtInStore.advancedSearchStore.addQueryChangeListener(this._triggerSearch);
+        Focus.search.builtInStore.advancedSearchStore.addScopeChangeListener(this._triggerSearch);
+    },
+    componentWillUnmount() {
+        Focus.search.builtInStore.advancedSearchStore.removeQueryChangeListener(this._triggerSearch);
+        Focus.search.builtInStore.advancedSearchStore.removeScopeChangeListener(this._triggerSearch);
+    },
+    _triggerSearch() {
+        this.props.action.search();
     },
     _SearchBarComponent() {
         return <SearchBar
             ref='searchBar'
             scopes={this.state.reference.scopes}
             loading={this.state.isLoadingSearch}
-            identifier='ADVANCED_SEARCH'
+            action={this.props.action}
             store={Focus.search.builtInStore.advancedSearchStore}
             service={this.props.service}
             />;
