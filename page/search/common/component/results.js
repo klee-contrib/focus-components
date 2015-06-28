b5d81f818d172cd8b36e108a50a5624a78249876
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-28 19:15:30 +0200

    [search results] Add a component to present the results, grouped or not

--- page/search/common/component/results.js
+++ page/search/common/component/results.js
@@ -3,10 +3,14 @@
 let builder = require('focus').component.builder;
 let omit = require('lodash/object/omit');
 let map = require('lodash/collection/map');
+let reduce = require('lodash/collection/reduce');
+let clone = require('lodash/lang/clone');
+let keys = require('lodash/object/keys');
 
 // Components
 
 let DefaultEmpty = require('./default-empty-component');
+let ListSelection = require('../../../list/selection').list.component;
 
 /**
  * Results component, used to render the results, grouped or ungrouped
@@ -20,22 +24,69 @@ let Results = {
     getDefaultProps() {
         return {
             emptyComponent: DefaultEmpty,
-            renderSingleGroupDecoration: true
+            renderSingleGroupDecoration: true,
+            initialRowsCount: 3,
+            showMoreAdditionalRows: 5,
+            scopeFacetKey: 'FCT_SCOPE',
+            resultsMap: undefined,
+            totalCount: undefined,
+            groupComponent: undefined,
+            lineComponentMapper: undefined,
+            idField: undefined,
+            isSelection: undefined,
+            lineSelectionHandler: undefined,
+            lineClickHandler: undefined,
+            scrollReachedBottomHandler: undefined,
+            lineOperationList: undefined,
+            scrollParentSelector: undefined,
+            selectionStatus: undefined,
+            groupByKey: undefined,
+            scopeSelectionHandler: undefined
         };
+    },
+    getInitialState() {
+        return ({
+            groupsRowsCounts: this._initGroupsRowsCountsFromMap(this.props.resultsMap)
+        });
+    },
+    componentWillReceiveNewProps(nextProps) {
+        this.setState({
+            groupsRowsCounts: this._initGroupsRowsCountsFromMap(nextProps.resultsMap)
+        });
+    },
+    _initGroupsRowsCountsFromMap(resultsMap) {
+        return reduce(resultsMap, (result, list, key) => {
+            result[key] = this.props.initialRowsCount;
+            return result;
+        }, {});
     },
     /**
      * Render a single group of results, using the group component given as a prop.
      * @param  {array} list the results list
      * @param  {string} key  the group key
+     * @param  {int} count  the group's results count
+     * @param  {bool} isUnique  is this the only rendered group
      * @return {HMTL}      the rendered group
      */
-    _renderSingleGroup(list, key) {
+    _renderSingleGroup(list, key, count, isUnique) {
         let Group = this.props.groupComponent;
-        return (
-            <Group groupKey={key} list={list} showAllHandler={this.props.showAllHandler}>
-
-            </Group>
-        );
+        if (isUnique) {
+            if (this.props.renderSingleGroupDecoration) {
+                return (
+                    <Group groupKey={key} showAllHandler={this._getShowAllHandler(key)} showMoreHandler={this._getShowMoreHandler(key)} isUnique={true}>
+                        {this._renderResultsList(list, key, count, true)}
+                    </Group>
+                );
+            } else {
+                return this._renderResultsList(list, key, count, true);
+            }
+        } else {
+            return (
+                <this.props.groupComponent groupKey={key} showAllHandler={this._getShowAllHandler(key)} showMoreHandler={this._getShowMoreHandler(key)}>
+                    {this._renderResultsList(list, key, count)}
+                </this.props.groupComponent>
+            );
+        }
     },
     /**
      * Render the empty component given as a prop when the result map is empty.
@@ -43,6 +94,44 @@ let Results = {
      */
     _renderEmptyResults() {
         return <this.props.emptyComponent/>;
+    },
+    _renderResultsList(list, key, count, isUnique) {
+        let LineComponent = this.props.lineComponentMapper(key, list);
+        let hasMoreData = isUnique && list.length === count;
+        return (
+            <ListSelection
+                data-focus='results-list'
+                data={list}
+                idField={this.props.idField}
+                isSelection={this.props.isSelection}
+                onSelection={this.props.lineSelectionHandler}
+                onLineClick={this.props.lineClickHandler}
+                fetchNextPage={this.props.scrollReachedBottomHandler}
+                hasMoreData={hasMoreData}
+                operationList={this.props.lineOperationList}
+                lineComponent={LineComponent}
+                parentSelector={this.props.scrollParentSelector}
+                selectionStatus={this.props.selectionStatus}
+            />
+        );
+    },
+    _getShowAllHandler(key) {
+        return () => {
+            if (this.props.groupByKey === this.props.scopeFacetKey) {
+                this.props.scopeSelectionHandler(key);
+            } else {
+                let facetKey = this.props.groupByKey;
+                let facetValue = key;
+                // TODO handle this as a facet selection
+            }
+        };
+    },
+    _getShowMoreHandler(key) {
+        return () => {
+            let groupsRowsCounts = clone(this.state.groupsRowsCounts);
+            groupsRowsCounts[key] = groupsRowsCounts[key] ? groupsRowsCounts[key] + this.props.showMoreAdditionalRows : this.props.initialRowsCount;
+            this.setState({groupsRowsCounts});
+        };
     },
     /**
      * Render the whole component
@@ -57,7 +146,20 @@ let Results = {
         let resultsMap = omit(this.props.resultsMap, (list) => {
             return list.length === 0;
         });
-        return map(resultsMap, this._renderSingleGroup);
+        // Get the count for each group
+        let groupCounts = this._getGroupsCounts();
+        // Check if there is only one group left
+        if (keys(resultsMap).length === 1) {
+            let key = keys(resultsMap)[0];
+            let list = resultsMap[key];
+            let count = groupCounts[key];
+            return this._renderSingleGroup(list, key, count, true);
+        } else {
+            return map(resultsMap, (list, key) => {
+                let count = groupCounts[key];
+                this._renderSingleGroup(list, key, count);
+            });
+        }
     }
 };
 
