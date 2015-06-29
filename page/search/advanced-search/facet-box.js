2f45b2f582069ed7b8092cbd502894519ac2622b
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-29 16:26:06 +0200

    [facet-box] Separate the facet box in a different component

---
+++ page/search/advanced-search/facet-box.js
@@ -0,0 +1,54 @@
+// Dependencies
+
+let builder = require('focus').component.builder;
+
+// Components
+
+let FacetBox = require('../../../search/facet-box').component;
+
+let Box = {
+    /**
+     * Get the default props
+     * @return {object} the default props
+     */
+    getDefaultProps() {
+        return ({
+            facets: {},
+            selectedFacets: {},
+            facetConfig: {},
+            action: undefined
+        });
+    },
+    /**
+     * Facet selection handler
+     * @param  {object}  facetComponentData The new facet box state, given by the FacetBox component
+     * @param  {Boolean} isDisableGroup     override the groupinKey ?
+     */
+    _onFacetSelection(facetComponentData, isDisableGroup) {
+        let newProperties = {
+            selectedFacets: facetComponentData.selectedFacetList,
+            openedFacetList: {}
+        };
+        if (isDisableGroup) {
+            newProperties.groupingKey = undefined;
+        }
+        this.props.action.updateProperties(newProperties);
+    },
+    /**
+     * Render the component
+     * @return {HTML} the rendered component
+     */
+    render() {
+        return (
+            <FacetBox
+                data-focus='advanced-search-facet-box'
+                facetList={this.props.facets}
+                selectedFacetList={this.props.selectedFacets}
+                config={this.props.facetConfig}
+                dataSelectionHandler={this._onFacetSelection}
+            />
+        );
+    }
+};
+
+module.exports = builder(Box);
