c1fabd39ab62d0bf2b33c945d0d46c09a8eecb7f
Stanislas Bernard <bernardstanislas@gmail.com>
2015-06-30 18:10:20 +0200

    [facet-box] Fix facet selection (partial)

--- page/search/advanced-search/facet-box.js
+++ page/search/advanced-search/facet-box.js
@@ -26,8 +26,7 @@ let Box = {
      */
     _onFacetSelection(facetComponentData, isDisableGroup) {
         let newProperties = {
-            selectedFacets: facetComponentData.selectedFacetList,
-            openedFacetList: {}
+            selectedFacets: facetComponentData.selectedFacetList
         };
         if (isDisableGroup) {
             newProperties.groupingKey = undefined;
