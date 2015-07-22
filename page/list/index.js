61111bcb559271db8992830f0681ed773440dad4
Pierre Besson <pierre.besson7@gmail.com>
2015-07-22 09:25:50 +0200

    [list] Add folder and main purpose.

---
+++ page/list/index.js
@@ -0,0 +1,7 @@
+//The purpose of this module is to deal with autonomous lists.
+//If you need lists inside a form please see the listFor helper function in a form.
+//The following lists can
+//- be loaded from a criteria (or without) (the criteria can be the result of a form)
+//- be paginated
+//- be displayed in any list container.
+module.exports = {};
