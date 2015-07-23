3fd49acaeb21963615ae17db0ae92364dba88da9
Pierre Besson <pierre.besson7@gmail.com>
2015-07-23 17:40:20 +0200

    [list page] Add mixin.

---
+++ page/list/mixin/store-connector.js
@@ -0,0 +1,41 @@
+let React = require('react');
+let isEqual = require('lodash/lang/isEqual');
+module.exports = function connectToStores(Component, stores, pickProps, getState) {
+  return class StoreConnector extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = this.getStateFromStores(props);
+      this.handleStoresChanged = this.handleStoresChanged.bind(this);
+    }
+
+    getStateFromStores(props) {
+      return getState(pickProps(props));
+    }
+
+    componentDidMount() {
+      stores.forEach(store =>
+        store.addChangeListener(this.handleStoresChanged)
+      );
+    }
+
+    componentWillReceiveProps(nextProps) {
+      if (!isEqual(pickProps(nextProps), pickProps(this.props))) { //isEqual
+        this.setState(this.getStateFromStores(nextProps));
+      }
+    }
+
+    componentWillUnmount() {
+      stores.forEach(store =>
+        store.removeChangeListener(this.handleStoresChanged)
+      );
+    }
+
+    handleStoresChanged() {
+      this.setState(this.getStateFromStores(this.props));
+    }
+
+    render() {
+      return <Component {...this.props} {...this.state} />;
+    }
+  };
+}
