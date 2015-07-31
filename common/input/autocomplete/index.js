37017ee302af969a96855827e5dd65d0a80c655a
Stanislas Bernard <bernardstanislas@gmail.com>
2015-07-31 13:10:30 +0200

    Add documentation

--- common/input/autocomplete/index.js
+++ common/input/autocomplete/index.js
@@ -6,13 +6,27 @@ let builder = Focus.component.builder;
 let types = Focus.component.types;
 let find = require('lodash/collection/find');
 
+/**
+ * Autocomplete component.
+ * Get a pickList as an input, then let the user type and suggests values from the picklist.
+ * Can force values in the input field to be taken from the pick list only.
+ * @type {Object}
+ */
 let Autocomplete = {
+    /**
+     * Component will mount.
+     * Check if the Awesomplete library is in the Window object.
+     */
     componentWillMount() {
         // Check if Awesomplete is set in Window
         if (!window.Awesomplete) {
             throw new Error('Please include Awesomplete to your application. See http://leaverou.github.io/awesomplete/ for more information');
         }
     },
+    /**
+     * Component did mount.
+     * Initiates the Awesomplete object.
+     */
     componentDidMount() {
         let {input} = this.refs;
         let {pickList} = this.props;
@@ -21,6 +35,10 @@ let Autocomplete = {
         });
         this._awesomeplete.input.addEventListener('awesomplete-select', event => this._selectionHandler(event.text));
     },
+    /**
+     * Default props.
+     * @return {Object} default props
+     */
     getDefaultProps() {
         return {
             code: '',
@@ -29,6 +47,10 @@ let Autocomplete = {
             validate: true
         };
     },
+    /**
+     * Prop validation
+     * @type {Object}
+     */
     propTypes: {
         pickList: types('array'),
         selectionHandler: types('function'),
@@ -36,12 +58,22 @@ let Autocomplete = {
         validate: types('bool'),
         value: types('string')
     },
+    /**
+     * Initial state.
+     * Retrieve the value from the provided code and pick list.
+     * @return {Object} initial state
+     */
     getInitialState() {
         const {code} = this.props;
         return ({
             value: this._getValueFromCode(code)
         });
     },
+    /**
+     * Component will receive props.
+     * Update the pick list, and try to resolve the new value.
+     * @param  {Object} nextProps new props
+     */
     componentWillReceiveProps(nextProps) {
         let {pickList, code} = nextProps;
         code = code || ''; // Value can be missing from the new props, in case the parent only changed the list for example
@@ -49,32 +81,62 @@ let Autocomplete = {
         this._awesomeplete._list = this._extractListFromData(pickList);
         this.setState({value});
     },
+    /**
+     * Selection handler.
+     * If a selection handler is set in the props, send it the selected pick.
+     * Also, set a flag to tell the blur listener not to empty the value, because the selection, as it is a click outside the input, raises a blur event.
+     * @param  {String} value selected value from the dropdown list
+     */
     _selectionHandler(value) {
         const {selectionHandler} = this.props;
         if (selectionHandler) {
             const {pickList} = this.props;
-            const selectedCode = find(pickList, {value}).code;
-            selectionHandler(selectedCode);
+            const selectedPick = find(pickList, {value});
+            selectionHandler(selectedPick);
         }
         this._isSelecting = true; // Private flag to tell the blur listener not to replace the value
         this.setState({value});
     },
+    /**
+     * Extract list of suggestions from pick list
+     * @param  {Object} data the pick list
+     * @return {Array}      the suggestion array
+     */
     _extractListFromData(data) {
         return data.map(datum => datum.value);
     },
+    /**
+     * Get code from value in the pick list
+     * @param  {String} value the value
+     * @return {String} the code
+     */
     _getCodeFromValue(value) {
         const {pickList} = this.props;
         const pick = find(pickList, {value});
         return pick ? pick.code : pick;
     },
+    /**
+     * Get value from code in the pick list
+     * @param  {String} code the code
+     * @param  {Object} pickList=this.props.pickList  optional pick list to resolve the value from
+     * @return {String} value
+     */
     _getValueFromCode(code, pickList=this.props.pickList) {
         const pick = find(pickList, {code});
         return pick ? pick.value : '';
     },
+    /**
+     * Get the current code
+     * @return {String} the code
+     */
     getValue() {
         const {value} = this.state;
         return this._getCodeFromValue(value);
     },
+    /**
+     * On input blur.
+     * If validate is set in the props, validate the current value and erase it if not valid.
+     */
     _onInputBlur() {
         const {value} = this.state;
         const {validate} = this.props;
@@ -84,10 +146,18 @@ let Autocomplete = {
         }
         this._isSelecting = false;
     },
+    /**
+     * On input change
+     * @param  {Object} event change event
+     */
     _onInputChange(event) {
         const {value} = event.target;
         this.setState({value});
     },
+    /**
+     * Render the edit mode
+     * @return {HTML} rendered element
+     */
     _renderEdit() {
         let {value} = this.state;
         let {_onInputBlur, _onInputChange} = this;
@@ -95,12 +165,20 @@ let Autocomplete = {
             <input onBlur={_onInputBlur} onChange={_onInputChange} ref='input' value={value}/>
         );
     },
+    /**
+     * Render the consult mode
+     * @return {HTML} rendered element
+     */
     _renderConsult() {
         let {value} = this.state;
         return (
             <span>{value}</span>
         );
     },
+    /**
+     * Render
+     * @return {HTML} rendered element
+     */
     render() {
         let {isEdit} = this.props;
         let {_renderEdit, _renderConsult} = this;
