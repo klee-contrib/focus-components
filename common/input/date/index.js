c06ba8f9e59d912bd6a695342cb4e692ac72ca9d
Pierre Besson <pierre.besson7@gmail.com>
2015-08-04 12:08:03 +0200

    [date] Fix paerntElement issue #146

--- common/input/date/index.js
+++ common/input/date/index.js
@@ -1,35 +1,36 @@
-var jQuery = require('jquery');
 //Dependencies.
 ////http://www.daterangepicker.com/#ex2
-var builder = require('focus').component.builder;
-var React = require('react');
-var inputTextMixin = require('../text').mixin;
+let builder = require('focus').component.builder;
+let React = require('react');
+let inputTextMixin = require('../text').mixin;
 let assign = require('object-assign');
 /**
  * Input text mixin.
  * @type {Object}
  */
-var inputDateMixin = {
-  /** @inheritdoc */
-  mixins: [inputTextMixin],
-  /** @inheritdoc */
-  componentDidMount: function inputDateDidMount(){
-    if(!jQuery.fn.daterangepicker){
-      console.warn('The jquery daterangepicker plugin should be loaded: see https://github.com/dangrossman/bootstrap-daterangepicker.');
+let inputDateMixin = {
+    /** @inheritdoc */
+    mixins: [inputTextMixin],
+    /** @inheritdoc */
+    componentDidMount(){
+        let jQuery = require('jquery');
+        if(!jQuery.fn.daterangepicker){
+            console.warn('The jquery daterangepicker plugin should be loaded: see https://github.com/dangrossman/bootstrap-daterangepicker.');
+        }
+        let component = this;
+        //If the domains set options.
+        let propsOptions = this.props.options && this.props.options.dateRangePicker ? this.props.options.dateRangePicker : {};
+        //console.log('parentEL............', `div [data-reactid="${React.findDOMNode(this).parentElement.getAttribute('data-reactid')}"]`);
+        let dateRangeOptions = assign( propsOptions, {
+          //Check if the parentElement is the correct container.
+          parentEl: `[data-reactid="${React.findDOMNode(this).parentElement.getAttribute('data-reactid')}"]`,
+          singleDatePicker: true,
+          showDropdowns: true
+        });
+        jQuery(React.findDOMNode(this)).daterangepicker(dateRangeOptions, (start)=>{ ///*, end, label*/
+            component.setState({value: start.format('L')});
+        });
     }
-    var component = this;
-    //If the domains set options.
-    let propsOptions = this.props.options && this.props.options.dateRangePicker ? this.props.options.dateRangePicker : {};
-    console.log('parentEL............', `div [data-reactid="${React.findDOMNode(this).getAttribute('data-reactid')}"]`);
-    let dateRangeOptions = assign( propsOptions, {
-      parentEl: `div [data-reactid="${React.findDOMNode(this).getAttribute('data-reactid')}"]`,
-      singleDatePicker: true,
-      showDropdowns: true
-    });
-    jQuery(React.findDOMNode(this)).daterangepicker(dateRangeOptions, function(start){ ///*, end, label*/
-      component.setState({value: component.props.formatter(start.toDate())});
-    });
-  }
 };
 
 
