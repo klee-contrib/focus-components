49c1ddc5c025609d8b71753bc8c449e11edd65ad
Pierre Besson <pierre.besson7@gmail.com>
2015-08-19 15:50:07 +0200

    [form] Add a validation mixin, add _fieldsValidation and _customValidation methods.

---
+++ src/common/form/mixin/index.js
@@ -0,0 +1,9 @@
+let referenceBehaviour = require('./reference-behaviour');
+let actionBehaviour = require('./action-behaviour');
+let validationBehaviour = require('./validation-behaviour');
+
+module.exports = {
+    actionBehaviour,
+    referenceBehaviour,
+    validationBehaviour
+};
