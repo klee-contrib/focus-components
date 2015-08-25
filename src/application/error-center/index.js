fe8ee2ad6e03d047f2e2ff9ca41b565c7464be9d
Pierre Besson <pierre.besson7@gmail.com>
2015-08-25 17:44:02 +0200

    [error] improve error component add react errors.

--- src/application/error-center/index.js
+++ src/application/error-center/index.js
@@ -1,51 +1,60 @@
-var builder = require('focus').component.builder;
-var React = require('react');
-var type = require('focus').component.types;
-var assign = require('object-assign');
-var errorCenter = {
-  getDefaultProps(){
-    return {
-      source: window,
-      errors: [],
-      isErrorsVisible: false,
-      numberDisplayed: 3
+const {builder, types} = require('focus').component;
+const React = require('react');
+const REACT_NOT_COMPONENT_ERROR = 'Uncaught TypeError: undefined is not a function';
+const REACT_NOT_COMPONENT_MESSAGE = 'Check your console errors, it seems you are trying to create a component from something which is not a component.';
+const errorCenter = {
+    displayName: 'ErrorCenter',
+    /** @inheriteddoc */
+    getDefaultProps(){
+        return {
+            source: window,
+            errors: [],
+            isErrorsVisible: false,
+            numberDisplayed: 3
+        }
+    },
+    /** @inheriteddoc */
+    getInitialState(){
+        return {errors: this.props.errors, isErrorsVisible: this.props.isErrorsVisible};
+    },
+    /** @inheriteddoc */
+    componentWillMount() {
+        this.props.source.onerror = ((e)=>{
+            console.log('errors', e);
+            let {errors} = this.state;
+            errors.push(e);
+            this.setState({errors: errors});
+        });
+        console.log('this.props', this.props , 'ERR0R', this.props.source.onerror);
+
+    },
+    _toggleVisible(){
+        this.setState({isErrorsVisible: !this.state.isErrorsVisible})
+    },
+    _renderErrors(){
+        const {errors, isErrorsVisible} = this.state;
+        const {numberDisplayed} = this.props;
+        const errorLength = errors.length;
+        return (
+            <div data-focus='error-center'>
+                <div data-focus='error-counter'>
+                    <i className="fa fa-times-circle"></i>{errorLength}
+                </div>
+                <div data-focus='error-actions'>
+                    <i className='fa fa-refresh' onClick={()=>{window.location.reload(); }}></i>
+                    <i className={`fa fa-arrow-circle-o-${isErrorsVisible ? 'up' : 'down'}`} onClick={this._toggleVisible}></i>
+                    <i className='fa fa-trash-o' onClick={()=>{this.setState({errors: []}); }}></i>
+                </div>
+                <ul data-focus='error-stack'>
+                  {isErrorsVisible ? errors.slice(errorLength - numberDisplayed, errorLength).map((err)=>{ const e = REACT_NOT_COMPONENT_ERROR === err ? REACT_NOT_COMPONENT_MESSAGE : err; return <li>{e}</li>; }) : null}
+                </ul>
+            </div>
+        );
+    },
+    /** @inheriteddoc */
+    render() {
+        return this.state.errors.length > 0 ? this._renderErrors() : null;
     }
-  },
-  getInitialState(){
-    return {errors: this.props.errors, isErrorsVisible: this.props.isErrorsVisible};
-  },
-  /** @inheriteddoc */
-  componentWillMount() {
-    this.props.source.onerror = ((e)=>{
-      let errs = this.state.errors;
-      errs.push(e);
-      this.setState({errors: errs});
-    });
-  },
-  _toggleVisible(){
-    this.setState({isErrorsVisible: !this.state.isErrorsVisible})
-  },
-  _renderErrors(){
-    return (
-      <div data-focus='error-center'>
-        <div data-focus='error-counter'>
-          <i className="fa fa-times-circle"></i>{this.state.errors.length}
-        </div>
-        <div data-focus='error-actions'>
-          <i className='fa fa-refresh' onClick={()=>{window.location.reload();}}></i>
-          <i className={`fa fa-arrow-circle-o-${this.state.isErrorsVisible ? 'up' : 'down'}`} onClick={this._toggleVisible}></i>
-          <i className='fa fa-trash-o' onClick={()=>{this.setState({errors: []})}}></i>
-        </div>
-        <ul data-focus='error-stack'>
-          {this.state.isErrorsVisible ? this.state.errors.slice(this.state.errors.length - this.props.numberDisplayed, this.state.errors.length).map((err)=>{return <li>{err}</li>}) : null}
-        </ul>
-      </div>
-    );
-  },
-  /** @inheriteddoc */
-  render() {
-    return this.state.errors.length > 0 ? this._renderErrors() : null;
-  }
 };
 
 module.exports = builder(errorCenter);
