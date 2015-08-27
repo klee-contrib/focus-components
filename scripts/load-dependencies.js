81f51015d010e94c3f90b3d95a89fdb42bc4b63f
Pierre Besson <pierre.besson7@gmail.com>
2015-08-27 10:58:44 +0200

    [loader material]

--- scripts/load-dependencies.js
+++ scripts/load-dependencies.js
@@ -1,15 +1,11 @@
 const loadScript = window.loadScript;
 const loadStyle = window.loadStyle;
-//bootstrap
-const BOOTSTRAP_JS = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.js',
-BOOTSTRAP_CSS = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.css',
-FONTAWESOME_CSS = '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css';
-//bootstrap material
-const BOOTSTRAP_MATERIAL_CSS = '//cdn.jsdelivr.net/bootstrap.material-design/0.3.0/css/material.min.css',
-BOOTSTRAP_MATERIAL_RIPPLE_CSS = '//cdn.jsdelivr.net/bootstrap.material-design/0.3.0/css/ripples.min.css',
-BOOTSTRAP_MATERIAL_FONT = '//cdn.jsdelivr.net/bootstrap.material-design/0.3.0/css/material-wfont.min.css',
-BOOTSTRAP_MATERIAL_JS = '//cdn.jsdelivr.net/bootstrap.material-design/0.3.0/js/material.min.js',
-BOOTSTRAP_MATERIAL_RIPPLE_JS = '//cdn.jsdelivr.net/bootstrap.material-design/0.3.0/js/ripples.min.js';
+//font awesome
+const FONTAWESOME_CSS = '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css';
+// google material
+const GOOGLE_MATERIAL_JS = 'https://storage.googleapis.com/code.getmdl.io/1.0.4/material.min.js',
+ GOOGLE_MATERIAL_CSS = 'https://storage.googleapis.com/code.getmdl.io/1.0.4/material.indigo-pink.min.css',
+ GOOGLE_MATERIAL_FONT = 'https://fonts.googleapis.com/icon?family=Material+Icons';
 //React
 const REACT_JS = 'https://fb.me/react-0.13.3.js',
 BABEL_TRANSFORMER = 'https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser.js';
@@ -36,20 +32,12 @@ const FOCUS_INIT = '/focus-components/example/js/initFocus.js';
  * @return {Promise} - Promise of the JS loading.
  */
 function loadJS(){
-    // Bootstrap.
-    function bootstrapPromise(){
-        return loadScript(BOOTSTRAP_JS).then(()=>{
-            return Promise.all([
-                    loadScript(BOOTSTRAP_MATERIAL_JS),
-                    loadScript(BOOTSTRAP_MATERIAL_RIPPLE_JS)
-                ]);
-        });
-    }
+
     function focusPromise(){return loadScript(FOCUS_JS).then(()=>{return loadScript(FOCUS_COMPONENTS_JS); })}
     return Promise.all([loadScript(JQUERY), loadScript(REACT_JS), loadScript(LODASH), loadScript(BABEL_TRANSFORMER)])
     .then(()=>{
         return Promise.all([
-            bootstrapPromise(),
+            loadScript(GOOGLE_MATERIAL_JS),
             loadScript(BACKBONE),
             loadScript(I18N),
             loadScript(MOMENT).then(()=>{return loadScript(DATE_RANGE_PICKER_JS); }),
@@ -65,11 +53,8 @@ function loadJS(){
  * @return {Promise} - Promise of all css load.
  */
 function loadCSS(){
-    return Promise.all([loadStyle(BOOTSTRAP_CSS), loadStyle(FONTAWESOME_CSS)]).then(()=>{
+    return Promise.all([loadStyle(GOOGLE_MATERIAL_CSS), loadCSS(GOOGLE_MATERIAL_FONT), loadStyle(FONTAWESOME_CSS)]).then(()=>{
         return Promise.all([
-            loadStyle(BOOTSTRAP_MATERIAL_CSS),
-            loadStyle(BOOTSTRAP_MATERIAL_RIPPLE_CSS),
-            //loadStyle(BOOTSTRAP_MATERIAL_FONT),
             loadStyle(DATE_RANGE_PICKER_CSS)
         ]);
     }).then(()=>{
@@ -77,7 +62,7 @@ function loadCSS(){
     });
 }
 function loadAssets(){
-    return Promise.all([loadJS(),loadCSS()]).then((s)=>{
+    return Promise.all([loadJS(), loadCSS()]).then((s)=>{
         console.info('All style and scripts loaded', s);
     }, (err)=>{
         console.error('A problem occurs on the loadin of one or sevral scripts of style files.', err);
