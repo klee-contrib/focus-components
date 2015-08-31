19d7a795b8cda93a4b706fc5399692fd69a89187
Stanislas Bernard <bernardstanislas@gmail.com>
2015-08-31 16:48:39 +0200

    [example] Clean gulp and node

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
+// Dependencies
+
 var gulp = require('gulp');
-
-//Build the JavaScript source.
-
-
 var babelify = require("babelify"); //es6
 var browserify = require('browserify'); //build the source
 var watchify = require('watchify'); //build the source
@@ -10,8 +8,8 @@ var source = require('vinyl-source-stream');
 
 
 /**
- * LINT
- */
+* LINT
+*/
 var src = 'src/**/*.js';
 var srcCSS = 'src/**/*.scss';
 var sources = [src];
@@ -35,9 +33,6 @@ gulp.task('eslint', function() {
 		},
 		rules: {
 			"valid-jsdoc": [2, {
-				/*  "prefer": {
-					"return": "return"
-				},*/
 				"requireParamDescription": true
 			}],
 			"quotes": [0]
@@ -45,32 +40,22 @@ gulp.task('eslint', function() {
 	};
 	var format = "compact"; //"compact", "checkstyle", "jslint-xml", "junit" and "tap".
 	gulp
-		.src(sources)
-		.pipe(eslint(options))
-		//.pipe(eslint.format(undefined, process.stdout))
-		//.pipe(eslint.failOnError())
-		.pipe(eslint.formatEach(format, process.stderr));
-	//.on('error', gutil.log);
+	.src(sources)
+	.pipe(eslint(options))
+	.pipe(eslint.formatEach(format, process.stderr));
 });
 
 /****************************
-  JS BUILD
+JS BUILD
 **********************/
-
-/**
- * Build a js file from a given source.
- * @param {string} directory - directory name
- * @param {object} options   [description]
- */
-function jsBuild(directory, options) {
-	options = options || {};
-	var entryFile = './' + directory + '/' + (options.entryFile || 'index.js');
-	var generatedFile = (options.generatedFile || "generated.js");
-	var literalify = require('literalify');
-	return browserify(({
-			entries: [entryFile],
+function generateGulpBuildTask(name) {
+	gulp.task(name, function() {
+		var literalify = require('literalify');
+		var build = name === "browserify" ? browserify : watchify;
+		return build(({
+			entries: ['./src/index.js'],
 			extensions: ['.jsx'],
-			standalone: "FocusComponents." + directory.replace('/', '.')
+			standalone: 'FocusComponents'
 		}))
 		.transform({
 			global: true
@@ -84,143 +69,29 @@ function jsBuild(directory, options) {
 		.transform(babelify)
 		.bundle()
 		//Pass desired output filename to vinyl-source-stream
-		.pipe(source(generatedFile))
-		.pipe(gulp.dest('./' + directory + '/example/js/'));
-}
-/*gulp.task('browserify', function() {
-	var literalify = require('literalify');
-	return browserify(({
-			entries: ['./index.js'],
-			extensions: ['.jsx'],
-			standalone: "focus-components"
-		}))
-		.transform({
-			global: true
-		}, literalify.configure({
-			react: 'window.React',
-			focus: 'window.focus'
-		}))
-		.transform(babelify)
-		.bundle()
-		//Pass desired output filename to vinyl-source-stream
 		.pipe(source('focus-components.js'))
-		.pipe(gulp.dest('./dist/'))
-		.pipe(gulp.dest('./example/js'));
-});
-*/
-function build(name){
-	gulp.task(name, function() {
-		var literalify = require('literalify');
-		var build = name === "browserify" ? browserify : watchify;
-		return build(({
-				entries: ['./src/index.js'],
-				extensions: ['.jsx'],
-				standalone: 'FocusComponents'
-			}))
-			.transform({
-				global: true
-			}, literalify.configure({
-				react: 'window.React',
-				focus: 'window.Focus',
-				jquery: 'window.jQuery',
-				backbone: 'window.Backbone',
-				moment: 'window.moment'
-			}))
-			.transform(babelify)
-			.bundle()
-			//Pass desired output filename to vinyl-source-stream
-			.pipe(source('focus-components.js'))
-			.pipe(gulp.dest('./dist/js'))
-			.pipe(gulp.dest('./example/js'))
-			.pipe(gulp.dest('../focus-demo/ui/vendor'));
+		.pipe(gulp.dest('./dist/js'))
+		.pipe(gulp.dest('./example/js'))
+		.pipe(gulp.dest('../focus-demo/ui/vendor'));
 	});
 }
-build("browserify");
-build("watchify");
-gulp.task('componentify-js', function() {
-	//Each component build
-	var components = require('./package.json').components || [];
-	return components.forEach(function(component) {
-		return jsBuild('src' + component.path, {
-			entryFile: "index.js",
-			generatedFile: component.name + ".js"
-		});
-	});
-});
+generateGulpBuildTask("browserify");
+generateGulpBuildTask("watchify");
+
 
 /*************************************
-  STYLE BUILD
+STYLE BUILD
 *********************************/
-function styleBuild(directory, options) {
-	options = options || {};
-	var sass = require('gulp-sass');
-	var concat = require('gulp-concat');
-	var generatedFile = (options.generatedFile || "component.css");
-	return gulp.src([directory + '/**/*.scss'])
-		.pipe(sass())
-		.pipe(concat(generatedFile))
-		.pipe(gulp.dest(directory + '/example/css/'));
-}
-
-gulp.task('componentify-style', function() {
-	//Each component build
-	var components = require('./package.json').components || [];
-	return components.forEach(function(component) {
-		return styleBuild('src' + component.path, {
-			generatedFile: component.name + ".css"
-		});
-	});
-});
 
 gulp.task('style', function() {
 	var sass = require('gulp-sass');
 	var concat = require('gulp-concat');
 	gulp.src(['src/**/*.scss'])
-		.pipe(sass())
-		.pipe(concat('focus-components.css'))
-		.pipe(gulp.dest('./example/css/'))
-		.pipe(gulp.dest('./dist/css'))
-		.pipe(gulp.dest('../focus-demo/ui/vendor'));
+	.pipe(sass())
+	.pipe(concat('focus-components.css'))
+	.pipe(gulp.dest('./example/css/'))
+	.pipe(gulp.dest('./dist/css'))
+	.pipe(gulp.dest('../focus-demo/ui/vendor'));
 });
 
 gulp.task('build', ['browserify', 'style']);
-gulp.task('componentify', ['componentify-js', 'componentify-style',
-	'componentify-img'
-]);
-
-/****************************
-  IMAGE BUILD
-****************************/
-function imgBuild(directory, options) {
-	options = options || {};
-	return gulp.src([directory + '/assets/img/*.{svg,gif,png}'])
-		.pipe(gulp.dest(directory + '/example/img/'))
-        .pipe(gulp.dest('./dist/img/'));
-}
-gulp.task('componentify-img', function() {
-	var components = require('./package.json').components || [];
-	return components.forEach(function(component) {
-		return imgBuild(component.path);
-	});
-
-});
-
-//Copy the focus-components directory into another repository.
-gulp.task('focus-components-npm', ['style', 'browserify'], function() {
-	var react = require('gulp-react');
-	var babel = require('gulp-babel');
-	var gulpif = require('gulp-if');
-	return gulp.src(['package.json', 'index.js',
-			'{spec,search,list,form,common,example,page,application}/**/*.{js,css}'
-		])
-		.pipe(gulpif(/[.]js$/, react({
-			harmony: true
-		})))
-		.pipe(gulpif(/[.]js$/, babel()))
-		.pipe(gulp.dest('../rodolphe/app/node_modules/focus-components/'));
-});
-gulp.task('watch',['build'], function(){
-	gulp.watch(['package.json','index.js',src, srcCSS],['build', 'style']);
-});
-
-//Build the style woth sass.
--- static-server.js
+++ static-server.js
@@ -1,79 +1,108 @@
+(function() {
+	var components = getComponents();
+	var localServerOptions = {
+		port: 3000
+	};
+	createLocalServer(components, localServerOptions);
+	generateGithubPage(components);
+})();
 
-var components = require('./package.json').components.sort(function(a, b){
-	if(a.name > b.name){
-		return 1;
-	}
-	if (a.name < b.name){
-		return -1;
-	}
-	return 0;
-});
-var componentServers = {};
-var port = "3000";
 /**
-* Creates a component map with all existing components.
-*/
-components.map(function(component) {
-	var pth = '/src/' + component.path + '/example/';
-	console.log('route', component.name, 'path', pth);
-	componentServers[component.name] = pth; //new static.Server(pth);
-});
+ * Get the components to serve, as an array looking like [{name, path}, ...]
+ * @return {array} Components to serve
+ */
+function getComponents() {
+	var componentsConfig = require('./package.json').components.sort(function(a, b){
+		if(a.name > b.name){
+			return 1;
+		}
+		if (a.name < b.name){
+			return -1;
+		}
+		return 0;
+	});
+	return componentsConfig.reduce(function(components, component) {
+		var path = '/src/' + component.path + '/example/';
+		console.log('route', component.name, 'path', path);
+		components[component.name] = path;
+		return components;
+	}, {});
+}
+
 /**
-* Render the existing components list with links.
-*/
-function componentsUrl(opts) {
+ * Create local web server.
+ * @param  {array} components        components to serve
+ * @param  {object} localServerOptions server options
+ */
+function createLocalServer(components, localServerOptions) {
+	var express = require('express');
+	var app = express();
+
+	// Add static paths
+
+	Object.keys(components).map(function(componentName) {
+		var component = components[componentName];
+		app.use('/' + componentName, express.static(__dirname + component));
+	});
+
+	app.use('/focus-components/example', express.static(__dirname + "/example"));
+	app.use('/focus-components/dist', express.static(__dirname + "/dist"));
+
+	// Generate content
+
+	var localServerContent = buildPageHtml(localServerOptions, components);
+
+	// Add a special 404 with all existing components and their urls.
+	app.use(function(req, res, next) {
+		res.status(404).send(localServerContent);
+	});
+
+	app.listen(localServerOptions.port, function() {
+		console.log('Application started on port: ', localServerOptions.port)
+	});
+}
+
+/**
+ * Generate Github page file, then save it to "components.html"
+ * @param  {array} components        components to generate the file from
+ */
+function generateGithubPage(components) {
+	var githubPageContent = buildPageHtml({isGhPage: true, urlRoot: 'kleegroup.github.io/focus-components'}, components);
+	var fs = require('fs');
+	fs.writeFile(__dirname + '/components.html', githubPageContent, function(err) {
+		if (err) {
+			throw err;
+		}
+		console.log("Github page file was successfully saved !");
+	});
+}
+
+/**
+ * Build components links
+ * @param  {object} opts the options
+ * @return {HTML}      the rendered links
+ */
+function buildComponentsLinks(opts, components) {
 	opts = opts || {};
-	var isGhPage = opts.isGhPage || false,
-	urlRoot = 'http://' + (opts.urlRoot || 'localhost'),
-	prt = (opts.port !== undefined && opts.port !== '') ? (':' + port + '/') : '';
-	var sb = "";
-	for (var component in componentServers) {
-		sb = sb +
-			"<li><a href='" + urlRoot +
-			prt  + (isGhPage ? componentServers[component]: component) + "'>" +
-			component +
-			"</a><img src ='http://react-components.com/img/react.svg' /></li>";
-	}
-	return sb;
+	var urlRoot = 'http://' + (opts.urlRoot || 'localhost');
+	var portSuffix = opts.port ? (':' + opts.port) : '';
+	var isGhPage = opts.isGhPage || false;
+	return Object.keys(components).reduce(function(links, componentName) {
+		var component = components[componentName];
+		links += "<li><a href='" + urlRoot + portSuffix + '/' + componentName
+		+ "'>" + componentName + "</a><img src ='http://react-components.com/img/react.svg' /></li>";
+		return links;
+	}, '');
 }
 
-//Creates a static server for each components example.
-var express = require('express');
-var app = express();
-for (var componentName in componentServers) {
-	app.use('/' + componentName,
-		express.static(__dirname + componentServers[componentName])
-	);
-}
-function buildPage(opts){
-	//var style = ' <style>* { box-sizing: border-box; } ul{width: 100%; flex-wrap: wrap; list-style-type: none; display: flex; margin: -1em 0 0 -1em; align-items: flex-start;  min-width: 25%;}   li{flex-grow: 1; flex: 1; padding: 1em 0 0 1em; align-self: flex-start;}</style>';
+/**
+ * Build static page HTML
+ * @param  {object} opts       page options
+ * @param  {array} components
+ * @return {HTML}            The rendered HTML
+ */
+function buildPageHtml(opts, components){
 	var fonts ="<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>"
 	var style ='<style>body{color: white; background-color: blue; font-family: "Lato", sans-serif;} ul { display: flex; flex-wrap: wrap;  list-style-type: none;} li { background: url("https://facebook.github.io/react/img/logo_og.png") bottom center no-repeat; margin-top: 7px; margin-right: 7px; padding-top: 10px; height: 50px; background: white; flex: 1;	  min-width: 20%; border: 2px solid red;} li a{ text-align: center} li img{width: 20px; }</style> ';
-	return fonts + style + '<p>The component you requested does not exists</p>' +
-		'<h1>Components available</h1><ul>' + componentsUrl(opts) +
-		'</ul>';
+	return fonts + style + '<h1>Components available</h1><ul>' + buildComponentsLinks(opts, components) + '</ul>';
 }
-var page =buildPage({port: port});
-var ghPage =buildPage({isGhPage: true, urlRoot: 'kleegroup.github.io/focus-components', port: ''});
-
-app.use('/focus-components/example', express.static(__dirname + "/example"));
-app.use('/focus-components/dist', express.static(__dirname + "/dist"));
-//Add a special 404 with all existing components and their urls.
-app.use(function(req, res, next) {
-	res.status(404)
-		.send(page);
-});
-
-//Start the application.
-app.listen(port, function() {
-	console.log('application started on port: ', port)
-});
-//Save the ghPagefile.
-//
-var fs = require('fs');
-fs.writeFile(__dirname + '/components.html', ghPage, function(err) {
-		if(err) {
-				return console.log(err);
-		}
-		console.log("The file was saved!");
-});
