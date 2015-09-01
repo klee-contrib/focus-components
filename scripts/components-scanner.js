8268a6b612b79c6f433cc859769dba295dcca097
Pierre Besson <pierre.besson7@gmail.com>
2015-09-01 15:23:58 +0200

    [component-scanner] Add components and filePath.

--- scripts/components-scanner.js
+++ scripts/components-scanner.js
@@ -1,11 +1,15 @@
 var pick = require('lodash/object/pick');
 var dir = require('node-dir');
 var components = [];
+var exampleIndex = {};
 dir.readFiles('/dev/focus-components/src',
     {match: /package.json/},
-    function(err, content, next) {
+    function(err, content, filename, next) {
         if (err) {throw err;}
-        components.push(pick(JSON.parse(content), 'name','version','description','keywords','photo'));
+        var componentInfos = JSON.parse(content);
+        //push the components into the list
+        components.push(pick(componentInfos, 'name','version','description','keywords','photo'));
+        exampleIndex[componentInfos.name] = filename;
         console.log('content:', content);
         next();
     },
@@ -13,4 +17,5 @@ dir.readFiles('/dev/focus-components/src',
         if (err) {throw err;}
         console.log('finished reading files:', files);
         console.log('components', components);
+        console.log('exampleIndex', exampleIndex);
 });
