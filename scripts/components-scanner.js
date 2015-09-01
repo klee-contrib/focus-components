84155adaa112330b9428567c082ad7cb4d29a646
Pierre Besson <pierre.besson7@gmail.com>
2015-09-01 18:13:09 +0200

    [scanner] Add path.

--- scripts/components-scanner.js
+++ scripts/components-scanner.js
@@ -2,6 +2,13 @@ var pick = require('lodash/object/pick');
 var dir = require('node-dir');
 var components = [];
 var exampleIndex = {};
+var fs = require('fs');
+function _processFileName(fileName){
+    var path = fileName.split('\\src\\')[1];
+    console.log('PATHHHHHHHHHHHH', path);
+    return {dot: path.replace(/\\/g, '.'), path: '../src/' + path.replace(/\\/g, '/')};
+}
+
 dir.readFiles('/dev/focus-components/src',
     {match: /package.json/},
     function(err, content, filename, next) {
@@ -9,8 +16,13 @@ dir.readFiles('/dev/focus-components/src',
         var componentInfos = JSON.parse(content);
         //push the components into the list
         components.push(pick(componentInfos, 'name','version','description','keywords','photo'));
-        exampleIndex[componentInfos.name] = filename;
-        console.log('content:', content);
+        var processName = _processFileName(filename);
+        console.log('fn', filename, 'process', processName);
+         exampleIndex[processName.dot] = {
+             path :  processName.path/*,
+             content: fs.readFileSync(filename + '/example/index.js')*/
+         };
+        //console.log('content:', content);
         next();
     },
     function(err, files){
