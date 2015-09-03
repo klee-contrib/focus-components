aaab747f6ea1aef01b5693ebb3a9c722f955ab00
Thomas <tgallon@gmail.com>
2015-09-03 11:29:26 +0200

    [input-date] react mode of the picker date library

--- src/common/input/date/index.js
+++ src/common/input/date/index.js
@@ -3,14 +3,82 @@
 const {builder, types} = require('focus').component;
 const React = require('react');
 const InputText = require('../text').component;
-//let assign = require('object-assign'); /// TODO TGN : do we keep this ?
 const moment = require('moment');
 const DateRangePicker = require('react-bootstrap-daterangepicker');
+
+const defaultLocale = {
+    format: 'MM/DD/YYYY',
+    separator: ' - ',
+    applyLabel: 'Apply',
+    cancelLabel: 'Cancel',
+    fromLabel: 'From',
+    toLabel: 'To',
+    customRangeLabel: 'Custom',
+    daysOfWeek: [
+        'Su',
+        'Mo',
+        'Tu',
+        'We',
+        'Th',
+        'Fr',
+        'Sa'
+    ],
+    monthNames: [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ],
+    firstDay: 1
+};
+
+// const defaultLocale = {
+//     format: 'DD/MM/YYYY',
+//     separator: ' - ',
+//     applyLabel: 'Appliquer',
+//     cancelLabel: 'Abandonner',
+//     fromLabel: 'De',
+//     toLabel: 'à',
+//     customRangeLabel: 'Personnalisée',
+//     daysOfWeek: [
+//         'Di',
+//         'Lu',
+//         'Ma',
+//         'Me',
+//         'Je',
+//         'Ve',
+//         'Sa'
+//     ],
+//     monthNames: [
+//         'Janvier',
+//         'Février',
+//         'Mars',
+//         'Avril',
+//         'Mai',
+//         'Juin',
+//         'Juillet',
+//         'Août',
+//         'Septembre',
+//         'Octobre',
+//         'Novembre',
+//         'Décembre'
+//     ],
+//     firstDay: 1
+// };
+
 /**
 * Input text mixin.
 * @type {Object}
 */
-const inputDateMixin = {
+const InputDateMixin = {
     /**
     * Tag name.
     */
@@ -19,103 +87,73 @@ const inputDateMixin = {
     /** @inheritdoc */
     getDefaultProps() {
         return {
-            value: moment(),
             drops: 'down', // possible values: up, down
-            locale: {
-                format: 'DD/MM/YYYY',
-                separator: ' - ',
-                applyLabel: 'Appliquer',
-                cancelLabel: 'Abandonner',
-                fromLabel: 'De',
-                toLabel: 'à',
-                customRangeLabel: 'Personnalisée',
-                daysOfWeek: [
-                    'Di',
-                    'Lu',
-                    'Ma',
-                    'Me',
-                    'Je',
-                    'Ve',
-                    'Sa'
-                ],
-                monthNames: [
-                    'Janvier',
-                    'Février',
-                    'Mars',
-                    'Avril',
-                    'Mai',
-                    'Juin',
-                    'Juillet',
-                    'Août',
-                    'Septembre',
-                    'Octobre',
-                    'Novembre',
-                    'Décembre'
-                ],
-                firstDay: 1
-            },
             showDropdowns: true,
-            singleDatePicker: true
+            locale: defaultLocale
         };
     },
     /** @inheritdoc */
     propTypes: {
-        error: types('string'),
-        name: types('string'),
-        value: types('object'),
-        placeHolder: types('string'),
-        startDate: types('string'),
-        endDate: types('string'),
         drops: types('string'),
-        showDropdowns: types('bool'),
-        singleDatePicker: types('bool'),
-        locale: types('object')
+        error: types('string'),
+        formatter: types('func'),
+        locale: types('object').isRequired,
+        name: types('string').isRequired,
+        placeHolder: types('string'),
+        value: types('object').isRequired
     },
     /** @inheritdoc */
     getInitialState() {
         return {
-            value: this.props.value,
+            value: moment(this.props.value)
         };
     },
+    /** @inheritdoc */
+    componentWillReceiveProps(newProps){
+        if(newProps !== undefined){
+            this.setState({
+                value: moment(newProps.value)
+            });
+        }
+    },
     /**
-     * Action when selection date event.
-     * @param  {event} event
-     * @param  {picker} picker date values
-     */
+    * Action when selection date event.
+    * @param  {event} event
+    * @param  {picker} picker date values
+    */
     handleEvent(event, picker){
         this.setState({
-			value: picker.startDate
-		});
+            value: picker.startDate
+        });
         //React.findDOMNode(this.refs.inputDateText).focus();
     },
     /**
-     * Get the selected date.
-     * @return {object} selected date
-     */
+    * Get the selected date.
+    * @return {object} selected date
+    */
     getValue() {
-        return this.state.value;
+        return this.state.value.toISOString();
     },
     /**
-     * Get formatted value.
-     * @return {string} formattedValue
-     */
+    * Get formatted value.
+    * @return {string} formattedValue
+    */
     getFormattedValue() {
-        //TODO TGN apply focus domain format.
         return moment(this.state.value).format(this.props.locale.format);
     },
     /** @inheritdoc */
     render() {
         const {value} = this.state;
-        const {drops, error, locale, name, placeHolder, showDropdowns, singleDatePicker} = this.props;
+        const {drops, error, locale, name, placeHolder, showDropdowns} = this.props;
         const formattedValue = this.getFormattedValue();
         return (
-            <div data-focus="input-date">
-            <DateRangePicker autoApply='true' drops={drops} locale={locale} onEvent={this.handleEvent} opens='center' showDropdowns={showDropdowns} singleDatePicker={singleDatePicker} startDate={value}>
-                <InputText error={error} name={name} placeHolder={placeHolder} ref="inputDateText" value={formattedValue} />
+            <div data-focus='input-date'>
+            <DateRangePicker autoApply='true' drops={drops} locale={locale} onEvent={this.handleEvent} opens='center' showDropdowns={showDropdowns} singleDatePicker={true} startDate={value}>
+            <InputText error={error} name={name} placeHolder={placeHolder} ref="inputDateText" value={formattedValue} />
             </DateRangePicker>
             </div>
         );
     }
 };
 
-module.exports = builder(inputDateMixin);
+module.exports = builder(InputDateMixin);
