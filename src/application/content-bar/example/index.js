88c9f744867f0ac0c0851280bfcbc8a6f4779433
Pierre Besson <pierre.besson7@gmail.com>
2015-09-07 15:54:08 +0200

    [content bar]

---
+++ src/application/content-bar/example/index.js
@@ -0,0 +1,17 @@
+const ContentBar = FocusComponents.application.contentBar.component;
+const ContentBarExample =  React.createClass({
+    render(){
+        return (
+            <ContentBar>
+                <img src="http://lorempixel.com/400/200" />
+                <img src="http://lorempixel.com/400/200" />
+                <img src="http://lorempixel.com/400/200" />
+                <img src="http://lorempixel.com/400/200" />
+                <img src="http://lorempixel.com/400/200" />
+                <img src="http://lorempixel.com/400/200" />
+            </ContentBar>
+        );
+    }
+});
+
+return <ContentBarExample/>;
