ba9941787a04ede111a4358db5179c9ff46136f4
Thomas <tgallon@gmail.com>
2015-09-08 14:54:41 +0200

    [block] refactor example mdl + add to catalog

--- src/common/block/example/index.js
+++ src/common/block/example/index.js
@@ -1,25 +1,135 @@
 const Block = FocusComponents.common.block.component;
+const formMixin = FocusComponents.common.form.mixin;
 
-const BlockSample = React.createClass({
+const domain =  {
+    "DO_TEXT": {
+        style: "do_text",
+        type: "text",
+        component: "PapaSinge",
+        validation: [{
+            type: "function",
+            value: function() {
+                return false;
+            }
+        }]
+    }
+};
+
+Focus.definition.domain.container.setAll(domain);
+const entities = {
+    "contact": {
+        "firstName": {
+            "domain": "DO_TEXT",
+            "required": false
+        }
+    }
+};
+Focus.definition.entity.container.setEntityConfiguration(entities);
+
+const BlockSample1 = React.createClass({
+    definitionPath: 'contact',
+    mixins: [formMixin],
+    hasLoad: false,
     /**
     * Render the component.
     * @return {object} React node
     */
-    render() {
+    renderContent() {
         return (
-            <Block title="Here is the title">
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <p>Here is the content.</p>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-            </Block>
+            <div>
+                <h3>Block without actions</h3>
+                <Block title="Here is the title">
+                    <br/>
+                    <br/>
+                    <p>Here is the content.</p>
+                    <br/>
+                    <br/>
+                </Block>
+            </div>
         );
     }
 });
 
-return <BlockSample/>;
+const BlockSample2 = React.createClass({
+    definitionPath: 'contact',
+    mixins: [formMixin],
+    hasLoad: false,
+    /**
+    * Render the component.
+    * @return {object} React node
+    */
+    renderContent() {
+        return (
+            <div>
+                <h3>Block with actions (default)</h3>
+                <Block title="Here is the title" actions={this._renderActions}>
+                    <br/>
+                    <br/>
+                    <p>Here is the content.</p>
+                    <br/>
+                    <br/>
+                </Block>
+            </div>
+        );
+    }
+});
+
+const BlockSample3 = React.createClass({
+    definitionPath: 'contact',
+    mixins: [formMixin],
+    hasLoad: false,
+    /**
+    * Render the component.
+    * @return {object} React node
+    */
+    renderContent() {
+        return (
+            <div>
+                <h3>Block with actions (bottom)</h3>
+                <Block title="Here is the title" actions={this._renderActions} actionsPosition="bottom">
+                    <br/>
+                    <br/>
+                    <p>Here is the content.</p>
+                    <br/>
+                    <br/>
+                </Block>
+            </div>
+        );
+    }
+});
+
+const BlockSample4 = React.createClass({
+    definitionPath: 'contact',
+    mixins: [formMixin],
+    hasLoad: false,
+    /**
+    * Render the component.
+    * @return {object} React node
+    */
+    renderContent() {
+        return (
+            <div>
+                <h3>Block with actions (both)</h3>
+                <Block title="Here is the title" actions={this._renderActions} actionsPosition="both">
+                    <br/>
+                    <br/>
+                    <p>Here is the content.</p>
+                    <br/>
+                    <br/>
+                </Block>
+            </div>
+        );
+    }
+});
+
+return (
+    <div>
+        <BlockSample1 />
+        <br/>
+        <BlockSample2 />
+        <br/>
+        <BlockSample3 />
+        <br/>
+        <BlockSample4 />
+    </div>
+);
--- src/common/block/index.js
+++ src/common/block/index.js
@@ -1,8 +1,10 @@
 const React = require('react');
+const oneOf = React.PropTypes.oneOf;
 const {builder, types} = require('focus').component;
 const i18nBehaviour = require('../../common/i18n/mixin');
 const styleBehaviour = require('../../mixin/stylable');
 const Title = require('../title').component;
+const {includes} = require('lodash/collection');
 
 /**
 * Mixin used in order to create a block.
@@ -10,17 +12,26 @@ const Title = require('../title').component;
 */
 const blockMixin = {
     mixins: [i18nBehaviour, styleBehaviour],
+
+    /** @inheritdoc */
+    getDefaultProps() {
+        return {
+            actionsPosition: 'top'
+        };
+    },
+
     /** @inheritedDoc */
     propTypes: {
-        title: types('string'),
-        actions: types('func')
+        actions: types('func'),
+        actionsPosition: oneOf(['both', 'bottom', 'top']),
+        title: types('string')
     },
     /**
     * Header of theblock function.
     * @return {[type]} [description]
     */
     heading() {
-        if(this.props.title){
+        if(this.props.title) {
             return this.i18n(this.props.title);
         }
     },
@@ -28,26 +39,26 @@ const blockMixin = {
     * Render the a block container and the cild content of the block.
     * @return {DOM} React DOM element
     */
-    render: function renderBlock(){
-        const {actions, children} = this.props;
+    render() {
+        const {actions, actionsPosition, children} = this.props;
+        const shouldDisplayActionsTop = actions && includes(['both', 'top'], actionsPosition);
+        const shouldDisplayActionsBottom = actions && includes(['both', 'bottom'], actionsPosition);
         return (
-            <div>
-                <div className="mdl-card mdl-card--border mdl-shadow--8dp" data-focus='block'>
-                    <div className="mdl-card__title mdl-card--border" data-focus='block-title'>
-                        <Title label={this.heading()} />
-                        {actions &&
-                            <div className="actions">{actions()}</div>
-                        }
-                    </div>
-                    <div className="mdl-card__supporting-text">
-                        {children}
-                    </div>
-                    {actions &&
-                        <div className="mdl-card__actions mdl-card--border">
-                            {actions()}
-                        </div>
+            <div className="mdl-card mdl-card--border mdl-shadow--8dp" data-focus='block'>
+                <div className="mdl-card__title mdl-card--border" data-focus='block-title'>
+                    <Title label={this.heading()} />
+                    {shouldDisplayActionsTop &&
+                        <div className="actions">{actions()}</div>
                     }
                 </div>
+                <div className="mdl-card__supporting-text">
+                    {children}
+                </div>
+                {shouldDisplayActionsBottom &&
+                    <div className="mdl-card__actions mdl-card--border" data-focus='block-actions'>
+                        {actions()}
+                    </div>
+                }
             </div>
         );
     }
