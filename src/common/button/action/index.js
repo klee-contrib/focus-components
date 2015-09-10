b52792be6c540d8b5cd383d399e57f78c7e78c5b
Pierre Besson <pierre.besson7@gmail.com>
2015-09-10 11:19:26 +0200

    [back to top]

--- src/common/button/action/index.js
+++ src/common/button/action/index.js
@@ -104,9 +104,9 @@ const buttonMixin = {
     },
     /** inheritedDoc */
     render() {
-        const {type, label} = this.props;
+        const {type, label, style} = this.props;
         return (
-            <button alt={label} className={this._className()} data-focus="button-action" onClick={this.handleOnClick} title={label} type={type}>
+            <button alt={label} className={this._className()} data-focus="button-action" onClick={this.handleOnClick} style={style} title={label} type={type}>
                 {this._renderIcon()}
                 {this._renderLabel()}
             </button>
