78c6efa4daa3d0848cb4f9c86e2596a8acb155ef
Pierre Besson <pierre.besson7@gmail.com>
2015-09-10 14:39:22 +0200

    [column] Column component and example.

---
+++ src/common/column/index.js
@@ -0,0 +1,44 @@
+// see http://www.getmdl.io/components/index.html#layout-section/grid
+//dependencies
+const React = require('react');
+const {Component} = React;
+const types = require('focus').component.types;
+/**
+ * Column component.
+ */
+class Column extends Component{
+    constructor(props){
+        super(props);
+        this._className = this._className.bind(this);
+    }
+    _className(){
+        const {size, className} = this.props;
+        if(className){return className; }
+        const SIZE_CSS = size ? `mdl-cell--${size}-col` : '';
+        return `mdl-cell ${SIZE_CSS} `;
+    }
+    /** @inheriteDoc */
+    render() {
+        const {children, style} = this.props;
+        const className = this._className();
+        return (
+            <div className={className} style={style}>
+                {children}
+            </div>
+        );
+    }
+}
+
+//Static props.
+Column.displayName = 'Column';
+Column.defaultProps = {
+    size: 6
+};
+Column.propTypes = {
+    size: types('number'),
+    children: types('element'),
+    className: types('string'),
+    style: types('object')
+};
+
+module.exports = Column;
