29a6d19eb467cb8ea6a0225dce92ad553c864b59
Thomas <tgallon@gmail.com>
2015-09-16 01:04:03 +0200

    [display-text] display text now translate value

--- src/common/display/text/index.js
+++ src/common/display/text/index.js
@@ -1,38 +1,41 @@
 //Dependencies.
-const builder = require('focusjs').component.builder;
+const {builder, types} = require('focusjs').component.builder;
 const React = require('react');
-const type = require('focusjs').component.types;
+const i18nBehaviour = require('../../i18n/mixin');
 
 /**
- * Input text mixin.
- * @type {Object}
- */
+* Input text mixin.
+* @type {Object}
+*/
 const displayTextMixin = {
-  /** @inheritdoc */
-  getDefaultProps(){
-    return {
-      formatter: (data)=> data
-    };
-  },
-  /** @inheritdoc */
-  propTypes: {
-    type: type('string'),
-    value: type(['string', 'number']),
-    name: type('string'),
-    style: type('object')
-  },
-  /**
-   * Render the value.
-   * @return {string} The formated value.
-   */
-  renderValue(){
-      const {formatter, value} = this.props;
-      return formatter(value);
-  },
-   /** @inheritdoc */
-  render: function renderInput() {
-      return <div {...this.props}>{this.renderValue()}</div>;
-  }
+    mixins: [i18nBehaviour],
+    displayName: 'DisplayText',
+    /** @inheritdoc */
+    getDefaultProps(){
+        return {
+            formatter: (data)=> data
+        };
+    },
+    /** @inheritdoc */
+    propTypes: {
+        type: types('string'),
+        value: types(['string', 'number']),
+        name: types('string'),
+        style: types('object')
+    },
+    /**
+    * Render the value.
+    * @return {string} The formated value.
+    */
+    renderValue(){
+        const {formatter, value} = this.props;
+        const translatedValue = value ? this.i18n(value) : value;
+        return formatter(translatedValue);
+    },
+    /** @inheritdoc */
+    render: function renderInput() {
+        return <div {...this.props}>{this.renderValue()}</div>;
+    }
 };
 
 
