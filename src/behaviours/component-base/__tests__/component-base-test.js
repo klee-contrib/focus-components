a5d8859c2ee7cc48d2dbf53f8f0bdfdc6cb27fde
Pierre Besson <pierre.besson7@gmail.com>
2015-09-16 16:22:56 +0200

    [behaviour] Add component base behaviour

---
+++ src/behaviours/component-base/__tests__/component-base-test.js
@@ -0,0 +1,28 @@
+import ComponentBaseBehaviour from '../';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import i18n from 'i18next';
+
+describe('The component base behaviour', () => {
+    let renderedComponent;
+    before((done) => {
+        i18n.init({}, () => {
+            @ComponentBaseBehaviour
+            class TestComponent extends React.Component {
+                render() {
+                    return (
+                        <div>
+                            {this.i18n('my.translation.path')}
+                        </div>
+                    );
+                }
+            }
+            renderedComponent = TestUtils.renderIntoDocument(<TestComponent/>);
+            done();
+        });
+    });
+    it('should add at least the translation function to the provided component', () => {
+        const child = TestUtils.findRenderedDOMComponentWithTag(renderedComponent, 'div');
+        expect(ReactDOM.findDOMNode(child).innerHTML).to.equal('my.translation.path');
+    });
+});
