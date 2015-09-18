093c4b5e53b0eda78d6f05e5768a9ab95aeae72f
Stan <bernardstanislas@gmail.com>
2015-09-18 10:32:59 +0200

    [input date] Add initial skeleton

---
+++ src/input/date/__tests__/index.js
@@ -0,0 +1,21 @@
+import InputDate from '../';
+
+global.componentHandler = {
+    upgradeElement: sinon.stub(),
+    downgradeElements: sinon.stub()
+};
+
+describe.only('The input date', () => {
+    describe('when mounted', () => {
+        const now = new Date();
+        let renderedTest;
+        const onChangeSpy = sinon.spy();
+        before(() => {
+            renderedTest = TestUtils.renderIntoDocument(<InputDate onChange={onChangeSpy} value={now} />);
+        });
+
+        it('should hold the provided initial value', () => {
+            expect(renderedTest.getValue()).to.equal(now);
+        });
+    });
+});
