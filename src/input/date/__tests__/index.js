2f08b77b71e484e5c2b2c5d2e4c8b2baf9b34064
Stan <bernardstanislas@gmail.com>
2015-09-18 13:55:31 +0200

    [input date][test] Remove only

--- src/input/date/__tests__/index.js
+++ src/input/date/__tests__/index.js
@@ -5,7 +5,7 @@ global.componentHandler = {
     downgradeElements: sinon.stub()
 };
 
-describe.only('The input date', () => {
+describe('The input date', () => {
     describe('when mounted', () => {
         const now = new Date().toISOString();
         let renderedTest;
