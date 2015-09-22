50c391544dd284d1f7ea90359c7cf145da054095
Pierre Besson <pierre.besson7@gmail.com>
2015-09-22 17:47:15 +0200

    [select] Add TDD and rewrite the component using es6 classes a

--- src/components/input/select/__tests__/index.js
+++ src/components/input/select/__tests__/index.js
@@ -1,6 +1,66 @@
 import Select from '../';
+const {renderIntoDocument, Simulate} = TestUtils;
+import {identity} from 'lodash/lang';
+import fixture from './fixture';
+//onChangeSpy = sinon.spy();
 
 describe('The select ', () => {
-    describe('when mounted', () => {
+    describe('when called with no props', () => {
+        let component;
+        before(
+            () => {
+                const shallowRenderer = TestUtils.createRenderer();
+                shallowRenderer.render(<Select/>);
+                component = shallowRenderer.getRenderOutput();
+            }
+        );
+        it.skip('should render an empty select', ()=>{
+
+        });
     });
+    describe('when called with minimal props', () => {
+        let component, domNode;
+        const mockValues = fixture.VALUES;
+        before(
+            () => {
+
+                component = renderIntoDocument(<Select name='selectName' values={mockValues} onChange={identity}/>);
+                domNode = ReactDOM.findDOMNode(component);
+            }
+        );
+        it('should render an empty select', () => {
+            expect(domNode.tagName).to.equal('DIV');
+            expect(domNode.getAttribute('data-focus')).to.equal('select');
+            expect(ReactDOM.findDOMNode(component.refs.htmlSelect).tagName).to.equal('SELECT');
+        });
+        it('should not have a value', () => {
+            expect(component.getValue()).to.equal(null);
+        });
+    });
+    describe('when a value is provided', ()=>{
+        const {VALUE, VALUES} = fixture;
+        let component, domNode;
+        before(()=>{
+            component = renderIntoDocument(<Select name='selectName' onChange={identity} value={VALUE} values={VALUES} />);
+        });
+        it('should return the value when provided as a props', ()=> {
+            expect(component.getValue()).to.equal(VALUE);
+        });
+        it('should render the value in the DOM', ()=>{
+            expect(ReactDOM.findDOMNode(component.refs.htmlSelect).value).to.equal(`${VALUE}`);
+        });
+    });
+    describe('when the user select a new value', ()=>{
+        const {VALUE, VALUES} = fixture;
+        let onChangeSpy, component;
+        before(
+            () => {
+                onChangeSpy = sinon.spy();
+                component = renderIntoDocument(<Select name='selectName' onChange={identity} value={VALUE} values={VALUES} />);
+            }
+        );
+        it('should call onChange with the new value', ()=>{
+            Simulate.change(ReactDOM.findDOMNode(component.refs.htmlSelect), {target: {value: VALUE}});
+        });
+    })
 });
---
+++ src/components/input/select/index.js
@@ -0,0 +1,107 @@
+//dependencies
+import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+import ComponentBaseBehaviour from '../../../behaviours/component-base';
+import {isUndefined, isNull, isNumber} from 'lodash/lang';
+import {union} from 'lodash/array';
+const UNSELECTED_KEY = 'UNSELECTED_KEY';
+/**
+ * Parse the value.
+ * @param  {string | number} propsValue - The value given as props to read the type.
+ * @param  {string} rawValue   - The raw string value.
+ * @return {strint | number}  - The parsed value.
+ */
+function _parseValue(propsValue, rawValue){
+    return isNumber(propsValue) ? +rawValue : rawValue;
+}
+const propTypes = {
+    disabled: PropTypes.bool,
+    error: PropTypes.string,
+    hasUndefined: PropTypes.bool,
+    isRequired: PropTypes.bool,
+    labelKey: PropTypes.string,
+    mubtiple: PropTypes.bool,
+    name: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    placeHolder: PropTypes.string,
+    value: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]),
+    valueKey: PropTypes.string,
+    values: PropTypes.array.isRequired
+};
+
+const defaultProps = {
+    disabled: false,
+    hasUndefined: true,
+    labelKey: 'label',
+    multiple: false,
+    values: [],
+    valueKey: 'code',
+    parseValue: _parseValue
+};
+
+/**
+ * Component standing for an HTML input.
+ */
+@ComponentBaseBehaviour
+class Select extends Component {
+
+    /**
+     * Get the dom value of the component.
+     * @return {object} - The unformated dom value.
+     */
+    getValue = () => {
+        const {value} = this.props;
+        return isUndefined(value) ? null : value;
+    }
+    /**
+     * Handle the change on the select, it only propagates the value.
+     * @param  {object} evt - The react DOM event.
+     * @return {object} - The function onChange from the props, called.
+     */
+    _handleSelectChange = (evt) =>{
+        const {onChange, parseValue, value: propsValue} = this.props;
+        const {value} = evt.target;
+        return onChange(parseValue(propsValue, value));
+    }
+    /** inheritdoc */
+    _renderOptions({hasUndefined, labelKey, isRequired, value, values = [], valueKey}){
+        if(false === hasUndefined || (true === isRequired && !isUndefined(value) && !isNull(value))){
+            values = union(
+                [{[labelKey]: 'select.unSelected', [valueKey]: UNSELECTED_KEY}],
+                values
+            );
+        }
+        return values.map((val, idx)=>{
+            const optVal = `${val[valueKey]}`;
+            const optLabel = val[labelKey] || 'select.noLabel';
+            return <option key={idx} value={optVal}>{this.i18n(optLabel)}</option>;
+        });
+    }
+    /**
+     * @inheritdoc
+     * @override
+    */
+    render() {
+        const {error, name, placeholder, style, value, values, disabled, ...otherProps} = this.props;
+        //const pattern = error ? 'hasError' : null; //add pattern to overide mdl error style when displaying an focus error.
+        let selectOtherProps = disabled ? {disabled: 'disabled', ...otherProps} : otherProps;
+        return (
+            <div data-focus='select' ref='select' style={style}>
+                <select name={name} onChange={this._handleSelectChange} ref='htmlSelect' value={value} {...selectOtherProps} >
+                    {this._renderOptions(this.props)}
+                </select>
+                {error && <span className='mdl-textfield__error'>{error}</span>}
+            </div>
+        );
+    }
+}
+
+//Static props.
+Select.displayName = 'Select';
+Select.defaultProps = defaultProps;
+Select.propTypes = propTypes;
+
+export default Select;
