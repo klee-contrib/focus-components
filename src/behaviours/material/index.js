c74cbcc0f9166875bcfbd864d515c893d272c2e5
Stan <bernardstanislas@gmail.com>
2015-09-23 15:25:58 +0200

    [material behaviour] Fix issue with componentWillReceiveProps

--- src/behaviours/material/index.js
+++ src/behaviours/material/index.js
@@ -1,6 +1,6 @@
 const ReactDOM = require('react-dom');
 
-const Material = ref => Component => class MaterialComponent extends Component {
+const Material = (ref, watchedProp: 'error') => Component => class MaterialComponent extends Component {
     componentDidMount() {
         if (Component.prototype.componentDidMount) {
             Component.prototype.componentDidMount();
@@ -18,6 +18,17 @@ const Material = ref => Component => class MaterialComponent extends Component {
         const refNode = ReactDOM.findDOMNode(this.refs[ref]);
         if (refNode) {
             componentHandler.downgradeElements(refNode);
+        }
+    }
+
+    componentWillReceiveProps = nextProps => {
+        if (Component.prototype.componentWillReceiveProps) {
+            Component.prototype.componentWillReceiveProps(nextProps);
+        }
+        const newWatchedProp = nextProps[watchedProp];
+        if (newWatchedProp !== this.props[watchedProp]) {
+            const refNode = ReactDOM.findDOMNode(this.refs[ref]);
+            componentHandler.upgradeElement(refNode);
         }
     }
 };
