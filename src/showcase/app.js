fef6b1030f954ffa3dba628b97d41bfbb6c35869
Pierre Besson <pierre.besson7@gmail.com>
2015-09-23 18:21:34 +0200

    [showcase] Load all namespaces into windows and external libs

--- src/showcase/app.js
+++ src/showcase/app.js
@@ -1,13 +1,20 @@
+import 'material-design-lite/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import i18n from 'i18next-client';
+//Import focus components
+import FocusComponents from '../';
 import Catalog from './catalog';
 import Layout from './layout';
 import resStore from './resources';
+import './style/template.scss';
+import './style/demo.scss';
 
 // exposing in windows
 window.React = React;
 window.ReactDOM = ReactDOM;
+window.FocusComponents = FocusComponents;
+
 document.addEventListener('DOMContentLoaded', ()=> {
     // Render the showcase
     i18n.init({resStore, lng: 'dev'}, ()=>{
