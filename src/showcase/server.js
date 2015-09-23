0e57ceafd06deb84c56e7d0f90a2e2ae4232ed13
Pierre Besson <pierre.besson7@gmail.com>
2015-09-23 18:19:30 +0200

    [showcase] Update webpack config, serve svg, png, css

--- src/showcase/server.js
+++ src/showcase/server.js
@@ -6,9 +6,9 @@ new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true
-}).listen(3002, 'localhost', (err, result)=>{
+}).listen(3000, 'localhost', (err, result)=>{
     if (err) {
         console.log(err);
     }
-    console.log('Listening at localhost:3002');
+    console.log('Listening at localhost:3000');
 });
