fb0c0369891762771fe74675c7b4199f05bdc782
Pierre Besson <pierre.besson7@gmail.com>
2015-09-23 18:25:27 +0200

    [webpack] Add webpack conf.

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true
-}).listen(3000, 'localhost', (err, result)=>{
+}).listen(3001, 'localhost', (err, result)=>{
     if (err) {
         console.log(err);
     }
-    console.log('Listening at localhost:3000');
+    console.log('Listening at localhost:3001');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,35 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require('webpack');
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
-  output: {
-     path: path.join(__dirname, 'dist'),
-     filename: 'focus-components.js',
-     publicPath: '/dist/',
-     libraryTarget: 'var',
-     library: 'FocusComponents'
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3001',
+        'webpack/hot/only-dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'focus-components.js',
+        publicPath: '/dist/',
+        libraryTarget: 'var',
+        library: 'FocusComponents'
     },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-  }, {
-      test: /\.json$/,
-      loaders: ['json']
-  }]
-  }
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: ['react-hot', 'babel'],
+            include: path.join(__dirname, 'src')
+        }, {
+            test: /\.json$/,
+            loaders: ['json']
+        },
+        {
+            test: /\.scss$/,
+            loader: 'style!css!sass'
+        }]
+    }
 };
