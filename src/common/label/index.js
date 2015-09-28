a6ebcfc2d27910abaa222b8e93fa2a0f3d3bebe8
Pierre Besson <pierre.besson7@gmail.com>
2015-09-28 12:48:32 +0200

    [label] Add a text props, to change the content over the name.

--- src/common/label/index.js
+++ src/common/label/index.js
@@ -3,6 +3,7 @@
 const {builder, types} = require('focus-core').component;
 const i18nBehaviour = require('../i18n/mixin');
 const styleBehaviour = require('../../mixin/stylable');
+
 /**
 * Label mixin for form.
 * @type {Object}
@@ -11,14 +12,16 @@ const labelMixin = {
     mixins: [i18nBehaviour, styleBehaviour],
     /** @inheritdoc */
     propTypes: {
-        name: types('string').isRequired
+        name: types('string').isRequired,
+        text: types('string')
     },
     /** @inheritdoc */
     render() {
-        const {name, style} = this.props;
+        const {name, text, style} = this.props;
+        const content = text || name;
         return (
             <label className={style.className} htmlFor={name}>
-                {this.i18n(name)}
+                {this.i18n(content)}
             </label>
         );
     }
