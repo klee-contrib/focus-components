a76a012f9babf0d9dde947d1a13495385259c7ef
Stan <bernardstanislas@gmail.com>
2015-10-05 18:44:26 +0200

    [webpack] Update config

---
+++ webpack.config.prod.js
@@ -0,0 +1,15 @@
+const prodConfBuilder = require('webpack-focus').productionConfig;
+const focusComponentsConf = require('./webpack.config.dev');
+focusComponentsConf.externals = {
+    'focus-core': 'FocusCore',
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    moment: 'moment',
+    jquery: 'jQuery',
+    backbone: 'Backbone',
+    numeral: 'numeral',
+    'i18next-client': 'i18n'
+};
+const prodConf = prodConfBuilder(focusComponentsConf);
+console.log(prodConf);
+module.exports = prodConf;
