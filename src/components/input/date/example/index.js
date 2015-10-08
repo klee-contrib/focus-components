298c0c5f2b6d2a92f1f14698565ed4d3447158ee
Stan <bernardstanislas@gmail.com>
2015-10-08 18:05:10 +0200

    [date] Fix example

--- src/components/input/date/example/index.js
+++ src/components/input/date/example/index.js
@@ -2,54 +2,6 @@
 
 const InputDate = FocusComponents.components.input.Date;
 
-const locale = {
-    format: 'L',
-    months : 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
-    monthsShort : 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split('_'),
-    weekdays : 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
-    weekdaysShort : 'dim._lun._mar._mer._jeu._ven._sam.'.split('_'),
-    weekdaysMin : 'Di_Lu_Ma_Me_Je_Ve_Sa'.split('_'),
-    longDateFormat : {
-        LT : 'HH:mm',
-        LTS : 'HH:mm:ss',
-        L : 'DD/MM/YYYY',
-        LL : 'D MMMM YYYY',
-        LLL : 'D MMMM YYYY HH:mm',
-        LLLL : 'dddd D MMMM YYYY HH:mm'
-    },
-    calendar : {
-        sameDay: '[Aujourd\'hui à] LT',
-        nextDay: '[Demain à] LT',
-        nextWeek: 'dddd [à] LT',
-        lastDay: '[Hier à] LT',
-        lastWeek: 'dddd [dernier à] LT',
-        sameElse: 'L'
-    },
-    relativeTime : {
-        future : 'dans %s',
-        past : 'il y a %s',
-        s : 'quelques secondes',
-        m : 'une minute',
-        mm : '%d minutes',
-        h : 'une heure',
-        hh : '%d heures',
-        d : 'un jour',
-        dd : '%d jours',
-        M : 'un mois',
-        MM : '%d mois',
-        y : 'un an',
-        yy : '%d ans'
-    },
-    ordinalParse: /\d{1,2}(er|)/,
-    ordinal : function (number) {
-        return number + (number === 1 ? 'er' : '');
-    },
-    week : {
-        dow : 1, // Monday is the first day of the week.
-        doy : 4  // The week that contains Jan 4th is the first week of the year.
-    }
-}
-
 class InputDateSample extends React.Component {
     constructor(props) {
         super(props);
@@ -60,7 +12,7 @@ class InputDateSample extends React.Component {
 
     changeHandler = (id) => {
         return value => {
-            const {isValid, message} = this.refs[`date${id}`].validate(value);
+            const {isValid, message} = this.refs[`date${id}`].validate();
                 this.setState({
                     [`date${id}`]: value,
                     [`error${id}`]: isValid ? null : message
@@ -73,7 +25,7 @@ class InputDateSample extends React.Component {
         return (
             <div>
                 <h3>With value</h3>
-                <InputDate error={error1} locale={locale} name='date1' onChange={this.changeHandler(1)} ref='date1' value={date1} />
+                <InputDate error={error1} format='DD/MM/YYYY' locale='fr' name='date1' onChange={this.changeHandler(1)} ref='date1' value={date1} />
             </div>
         );
     }
