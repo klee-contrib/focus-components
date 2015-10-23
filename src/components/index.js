f53d797f5a06c5bb3a67a540846c0dd2b947c224
Thomas <tgallon@gmail.com>
2015-10-23 17:35:04 +0200

    [layout] publish layout header components

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,21 @@
+import headerActions from './layout/header-actions';
+import headerContent from './layout/header-content';
+import headerScrolling from './layout/header-scrolling';
+import headerTopRow from './layout/header-top-row';
 import input from './input';
-import panel from './panel';
-import scrollspyContainer from './scrollspy-container';
 import layout from './layout';
 import menuLeft from './menu';
+import panel from './panel';
+import scrollspyContainer from './scrollspy-container';
 
 export default {
+    HeaderActions: headerActions,
+    HeaderContent: headerContent,
+    HeaderScrolling: headerScrolling,
+    HeaderTopRow: headerTopRow,
     input,
-    Panel: panel,
-    ScrollspyContainer: scrollspyContainer,
     Layout: layout,
-    MenuLeft: menuLeft
+    MenuLeft: menuLeft,
+    Panel: panel,
+    ScrollspyContainer: scrollspyContainer
 }
