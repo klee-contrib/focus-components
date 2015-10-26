c0ff2450c501f48e6d159c4870d597ebada5f8f3
Stan <bernardstanislas@gmail.com>
2015-10-26 13:16:35 +0100

    [checkbox] Fix #614

--- src/components/input/checkbox/index.js
+++ src/components/input/checkbox/index.js
@@ -23,6 +23,15 @@ class InputCheckBox extends Component {
         return domElement.checked;
     }
 
+    componentDidUpdate = () => {
+        const {value} = this.props;
+        const method = value ? 'add' : 'remove';
+        const node = ReactDOM.findDOMNode(this.refs.mdlHolder);
+        if (node) {
+            node.classList[method]('is-checked');
+        }
+    }
+
     handleOnChange = ({target: {checked}}) => {
         const {onChange} = this.props;
         onChange(checked);
@@ -31,8 +40,8 @@ class InputCheckBox extends Component {
     render() {
         const {label, value} = this.props;
         return (
-            <label className='mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect' data-focus='input-checkbox' ref='mdlHolder'>
-                <input checked={value} className='mdl-checkbox__input' onChange={this.handleOnChange} ref='checkbox' type='checkbox' />
+            <label className={`mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect`} data-focus='input-checkbox' ref='mdlHolder'>
+                <input checked={value} className='mdl-checkbox__input' onChange={this.handleOnChange} ref='checkbox' type='checkbox'/>
                 {label && <span className='mdl-checkbox__label'>{this.i18n(label)}</span>}
             </label>
         );
