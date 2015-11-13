537ee28a772a1b4a8e1e0cd36a758396ce44c4db
Thomas <tgallon@gmail.com>
2015-11-13 19:08:04 +0100

    add react import in mixin message to work with webpack

--- src/message/index.js
+++ src/message/index.js
@@ -1,60 +1,68 @@
+const React = require('react');
 const {builder, types} = require('focus-core').component;
 const i18nBehaviour = require('../common/i18n/mixin');
 const Button = require('../common/button/action').component;
+
 const messageMixin = {
     /** @inheritedDoc */
-    getDefaultProps(){
+    getDefaultProps() {
         return {
             type: 'info',
             style: {}
         };
     },
+
     /** @inheritedDoc */
     propTypes: {
-        title: types('string'),
         content: types('string'),
-        type: types('string'),
+        style: types('object'),
+        title: types('string'),
         ttl: types('number'),
-        style: types('object')
+        type: types('string')
     },
+
     /** @inheritedDoc */
-    componentDidMount(){
-        if(this.props.ttl){
-            setTimeout(()=>{
+    componentDidMount() {
+        if(this.props.ttl) {
+            setTimeout(() => {
                 this._handleTimeToLeave();
             }, this.props.ttl);
         }
     },
+
     /** @inheritedDoc */
     mixins: [i18nBehaviour],
+
     /**
     * Time to leave handler.
     */
-    _handleTimeToLeave(){
+    _handleTimeToLeave() {
         let {handleTimeToLeave, id} = this.props;
-        if(handleTimeToLeave){
+        if(handleTimeToLeave) {
             handleTimeToLeave(id);
         }
     },
+
     /**
     * Handle click on the dismiss button.
     * @param {Event} event - Sanitize event.
     */
-    _handleOnClick(event){
+    _handleOnClick(event) {
         let {handleOnClick, id} = this.props;
-        if(handleOnClick){
+        if(handleOnClick) {
             handleOnClick(id);
         }
     },
+
     /**
     * Render an alert.
     * @return {JSX} The jsx.
     */
-    render(){
+    render() {
         const {type, id, content, title} = this.props;
         return (
-            <div data-focus='message' data-id={id} data-message-type={type} >
-                <Button handleOnClick={this._handleOnClick} icon='clear' shape='icon' type='button' />
+            <div data-focus='message' data-id={id} data-message-type={type}>
+                <Button handleOnClick={this._handleOnClick} icon='clear' shape='icon' type='button'/>
                 {title && <h4>{title}</h4>}
                 <p>{this.i18n(content)}</p>
             </div>
