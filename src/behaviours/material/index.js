eb624691fc1f2a20baf2acdd7c75ac9a36e7a421
Stan <bernardstanislas@gmail.com>
2015-11-26 17:56:00 +0100

    [material behaviour] Fix issue with binding

--- src/behaviours/material/index.js
+++ src/behaviours/material/index.js
@@ -2,33 +2,33 @@ const ReactDOM = require('react-dom');
 
 const Material = (ref, watchedProp: 'error') => Component => class MaterialComponent extends Component {
     componentDidMount() {
-        if (Component.prototype.componentDidMount) {
-            Component.prototype.componentDidMount();
-        }
         const refNode = ReactDOM.findDOMNode(this.refs[ref]);
         if (refNode) {
             componentHandler.upgradeElement(refNode);
+        }
+        if (Component.prototype.componentDidMount) {
+            Component.prototype.componentDidMount.call(this);
         }
     }
 
     componentWillUnmount() {
-        if (Component.prototype.componentWillUnmount) {
-            Component.prototype.componentWillUnmount();
-        }
         const refNode = ReactDOM.findDOMNode(this.refs[ref]);
         if (refNode) {
             componentHandler.downgradeElements(refNode);
         }
+        if (Component.prototype.componentWillUnmount) {
+            Component.prototype.componentWillUnmount.call(this);
+        }
     }
 
     componentWillReceiveProps = nextProps => {
-        if (Component.prototype.componentWillReceiveProps) {
-            Component.prototype.componentWillReceiveProps(nextProps);
-        }
         const newWatchedProp = nextProps[watchedProp];
         if (newWatchedProp !== this.props[watchedProp]) {
             const refNode = ReactDOM.findDOMNode(this.refs[ref]);
             componentHandler.upgradeElement(refNode);
+        }
+        if (Component.prototype.componentWillReceiveProps) {
+            Component.prototype.componentWillReceiveProps.call(this, nextProps);
         }
     }
 };
