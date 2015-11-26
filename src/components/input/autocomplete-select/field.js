e3013f8986eb93f28842cde1b265a165e6dc573d
Stan <bernardstanislas@gmail.com>
2015-11-26 17:56:45 +0100

    [autocomplete select] Add first files

---
+++ src/components/input/autocomplete-select/field.js
@@ -0,0 +1,66 @@
+import React, {Component, PropTypes} from 'react';
+import AutocompleteSelectEdit from './edit';
+import AutocompleteSelectConsult from './consult';
+
+class AutocompleteSelectField extends Component {
+    state = {}
+
+    static propTypes = {
+        isEdit: PropTypes.bool.isRequired,
+        keyResolver: PropTypes.func.isRequired,
+        onChange: PropTypes.func.isRequired,
+        querySearcher: PropTypes.func.isRequired
+    }
+
+    componentWillReceiveProps({error}) {
+        this.setState({customError: error});
+    }
+
+    getValue = () => {
+        const {isEdit, value} = this.props;
+        if (isEdit) {
+            return this.refs.autocomplete.getValue();
+        } else {
+            return value;
+        }
+    }
+
+    _handleAutocompleteBadInput = value => {
+        this.setState({customError: `${value} is not a valid entry.`});
+    }
+
+    _handleAutocompleteChange = value => {
+        const {onChange} = this.props;
+        this.setState({customError: null}, () => {
+            if (onChange) onChange(value);
+        });
+    }
+
+    _renderEdit = () => {
+        const {customError} = this.state;
+        return (
+            <AutocompleteSelectEdit
+                customError={customError}
+                onBadInput={this._handleAutocompleteBadInput}
+                onChange={this._handleAutocompleteChange}
+                ref='autocomplete'
+                {...this.props}
+            />
+        );
+    }
+
+    _renderConsult = () => {
+        return (
+            <AutocompleteSelectConsult
+                {...this.props}
+            />
+        );
+    }
+
+    render() {
+        const {isEdit} = this.props;
+        return isEdit ? this._renderEdit() : this._renderConsult();
+    }
+}
+
+export default AutocompleteSelectField;
