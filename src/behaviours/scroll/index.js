bea110083311c69c916f055f0c902307fbf2016e
Thomas <thomas.gallon@gmail.com>
2015-12-05 18:12:22 +0100

    [scrollspy-menu] correction scroll position on click

--- src/behaviours/scroll/index.js
+++ src/behaviours/scroll/index.js
@@ -7,16 +7,20 @@ const Scroll = Component => class ScrollComponent extends Component {
 
     /**
     * Get the scroll position from the top of the screen.
+    *
+    * https://developer.mozilla.org/fr/docs/Web/API/Element/getBoundingClientRect
+    *
     * @param {object} domNode domNoe to get the position from
     * @returns {int} - The position in pixel from the top of the scroll container.
     */
     scrollPosition(domNode) {
+        const y = window.pageYOffset || document.documentElement.scrollTop;
+        const x = window.pageXOffset || document.documentElement.scrollLeft;
         if(isUndefined(domNode)) {
-            const y = window.pageYOffset || document.documentElement.scrollTop;
-            const x = window.pageXOffset || document.documentElement.scrollLeft;
             return { top: y, left: x };
         }
-        return { top: domNode.scrollTop, left: domNode.scrollLeft };
+        const nodeRect = domNode.getBoundingClientRect();
+        return { left: nodeRect.left + x, top: nodeRect.top + y };
     }
 
 
