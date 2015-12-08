e5510a53a4b3b79a8d8cd056e6606e166501b033
Stan <bernardstanislas@gmail.com>
2015-12-08 17:17:52 +0100

    [autocomplete] Add more tests

--- src/components/input/autocomplete-select/__tests__/index.js
+++ src/components/input/autocomplete-select/__tests__/index.js
@@ -1,4 +1,4 @@
-import AutocompleteSelect from '../';
+import AutocompleteSelect from '../edit';
 
 const keyResolver = key => Promise.resolve(`Label for ${key}`);
 const querySearcher = query => {
@@ -49,8 +49,58 @@ describe.only('The autocomplete select', () => {
         before(() => {
             renderedTest = TestUtils.renderIntoDocument(<AutocompleteSelect keyResolver={keyResolver} querySearcher={querySearcher} value={value} inputTimeout={0}/>);
         });
-        it('should give a resolved object when getValue is called', () => {
+        it('should give a resolved value when getValue is called', () => {
             expect(renderedTest.getValue()).to.equal(value);
+        });
+    });
+    describe('when mounted', () => {
+        const value = 'value';
+        const keyResolverSpy = sinon.spy();
+        const keyResolver = data => {
+            keyResolverSpy(data);
+            return Promise.resolve(data);
+        };
+        before(() => {
+            renderedTest = TestUtils.renderIntoDocument(<AutocompleteSelect keyResolver={keyResolver} querySearcher={querySearcher} value={value} inputTimeout={0}/>);
+        });
+        it('should call the key resolver', () => {
+            expect(keyResolverSpy).to.have.been.calledWith(value);
+        });
+    });
+    describe('when the user types in the field', () => {
+        const query = 'query';
+        const querySearcherSpy = sinon.spy();
+        const querySearcher = data => {
+            querySearcherSpy(data);
+            return Promise.resolve({data: [], totalCount: 0});
+        };
+        before(() => {
+            renderedTest = TestUtils.renderIntoDocument(<AutocompleteSelect keyResolver={keyResolver} querySearcher={querySearcher} inputTimeout={0}/>);
+            const input = ReactDOM.findDOMNode(renderedTest.refs.htmlInput);
+            TestUtils.Simulate.change(input, {target: {value: query}});
+        });
+        it('should call the query searcher', () => {
+            expect(querySearcherSpy).to.have.been.calledWith(query);
+        });
+        it('should give a null value since the user did not select anything', () => {
+            expect(renderedTest.getValue()).to.be.null;
+        });
+    });
+    describe('when the user clears the input', () => {
+        const value = 'value';
+        before(done => {
+            const keyResolverStub = key => {
+                setTimeout(() => {
+                    const input = ReactDOM.findDOMNode(renderedTest.refs.htmlInput);
+                    TestUtils.Simulate.change(input, {target: {value: ''}});
+                    done();
+                }, 0);
+                return keyResolver(key);
+            }
+            renderedTest = TestUtils.renderIntoDocument(<AutocompleteSelect keyResolver={keyResolverStub} querySearcher={querySearcher} value={value} inputTimeout={0}/>);
+        });
+        it('should give a null value when getValue is called', () => {
+            expect(renderedTest.getValue()).to.be.null;
         });
     });
 });
