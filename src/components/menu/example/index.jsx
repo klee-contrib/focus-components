74529ac07d138df2dfdef963f85158c0a30fc462
GuenoleK <kikabouguenole@gmail.com>
2016-02-19 10:28:45 +0100

    Finalizing menu component example

--- src/components/menu/example/index.jsx
+++ src/components/menu/example/index.jsx
@@ -2,9 +2,8 @@ const {MenuLeft} = FocusComponents.components;
 const Popin = FocusComponents.application.popin.component;
 
 const MyMenu = React.createClass({
-    launchSecondPopin(MyMenu) {
-        const self = this;
-        return self.refs.secondPopin.togglePopin();
+    GoHome() {
+        window.location.href = '#'
     },
     itemsBuilder() {
         const self = this;
@@ -12,7 +11,7 @@ const MyMenu = React.createClass({
             {
                 icon:'home',
                 onClick() {
-                    window.location.href = '#';
+                    self.GoHome();
                 }
             },
             {
@@ -29,40 +28,32 @@ const MyMenu = React.createClass({
             }
         ]
     },
-    styleBuilder() {
-        return {
-            position: 'fixed',
-            left:'40%',
-            top:'30%',
-            width:'512px'
-        }
+    style: {
+        position: 'fixed',
+        left:'40%',
+        top:'30%',
+        width:'512px'
     },
     render() {
         return (
             <div>
-                <MenuLeft items={this.itemsBuilder()}>
+                <MenuLeft items={this.itemsBuilder()} handleBrandClick={this.GoHome}>
                 </MenuLeft>
-                <div style={this.styleBuilder()} className="demo-card-wide mdl-card mdl-shadow--2dp">
-                    <div className="mdl-card__title">
-                        <h1 className="mdl-card__title-text">Welcome</h1>
+                <div style={this.style} className='demo-card-wide mdl-card mdl-shadow--2dp'>
+                    <div className='mdl-card__title'>
+                        <h1 className='mdl-card__title-text'>Welcome</h1>
                     </div>
-                    <div className="mdl-card__supporting-text">
+                    <div className='mdl-card__supporting-text'>
                         <h6>
-                            Here you can see a example of a Focus menu bar.<br/>
-                            You can easily define actions to your icons and stay on the same wavelength as your application's logic.
-                        </h6>
-                    </div>
-                    <div className="mdl-card__actions mdl-card--border">
-                        <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" onClick={this.launchSecondPopin}>
-                            <b>See more</b>
-                        </a>
-                    </div>
+                            Here you can see a Focus menu bar example.<br/>
+                        You can easily define actions to your icons and stay on the same wavelength as your application's logic.
+                    </h6>
                 </div>
-                <MyPopin ref='popin'/>
-                <SecondPopin ref='secondPopin'/>
             </div>
-        );
-    }
+            <MyPopin ref='popin'/>
+        </div>
+    );
+}
 });
 
 const MyPopin = React.createClass({
@@ -71,40 +62,20 @@ const MyPopin = React.createClass({
     },
     render() {
         return (
-            <Popin ref='popin' size="small">
-                <h2>
-                    Popin Title
-                </h2><br/>
-                <h4>
-                    Hello there, <br/>
-                    I'm a Popin component.<br/>
-                    I Hope our demos are helpful...
-                </h4>
-                And I hope I'll got my demo page soon 😐 <br/>
-                <h5><i>Focus.</i></h5>
-            </Popin>
-        );
-    }
-});
-
-const SecondPopin = React.createClass({
-    togglePopin() {
-        this.refs.secondPopin.toggleOpen();
-    },
-    render() {
-        return (
-            <Popin ref='secondPopin' size="small">
+            <Popin ref='popin' size='small'>
                 <h2>
                     News
                 </h2><br/>
                 <h4>
-                    By the moment, i don't have my page...
-                </h4>
-                And I hope I'll got it soon 😐 <br/>
-                <h5><i>Focus.</i></h5>
-            </Popin>
-        );
-    }
+                    Hello there, <br/>
+                I'm a Popin component.<br/>
+            I Hope our demos are helpful...
+        </h4>
+        And I hope I'll got my demo page soon 😐 <br/>
+    <h5><i>Focus.</i></h5>
+</Popin>
+);
+}
 });
 
 module.exports = MyMenu;
