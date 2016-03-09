2f31d1cb5c14e60ce80a9e16814866830822fe09
GuenoleK <kikabouguenole@gmail.com>
2016-03-09 16:51:21 +0100

    Updating example page

--- src/common/button/action/example/index.jsx
+++ src/common/button/action/example/index.jsx
@@ -13,10 +13,49 @@ const resources = {
 i18nInitializer({resStore: resources});
 
 const ButtonExample = React.createClass({
+    flexContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        flexFlow: 'wrap'
+    },
+    flexItem: {
+        padding: '15px',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    flexItemTitle: {
+        textAlign: 'center',
+        width: '100%'
+    },
     render() {
         return(
             <div>
-                <Button label='BOUTON' type='button' shape='raised' color='colored' handleOnClick={() => console.log(this)} />
+                <div style={this.flexContainer}>
+                    <h4 style={this.flexItemTitle}>BUTTON RIPPLE</h4>
+                    <div style={this.flexItem}><Button label='TEST' type='button' hasRipple={true} shape='raised' color='primary' handleOnClick={() => console.log(this)} /><br/><br/>Without ripple</div>
+                    <div style={this.flexItem}><Button label='TEST' type='button' hasRipple={true} shape='raised' color='accent' handleOnClick={() => console.log(this)} /><br/><br/>With ripple</div>
+                </div>
+
+                <div style={this.flexContainer}>
+                    <h4 style={this.flexItemTitle}>SIMPLE BUTTON</h4>
+                    <div style={this.flexItem}><Button label='TEST' type='button' hasRipple={true} shape='raised' handleOnClick={() => console.log(this)} /><br/><br/>Button</div>
+                    <div style={this.flexItem}><Button label='TEST' type='button' hasRipple={true} shape='raised' color='primary' handleOnClick={() => console.log(this)} /><br/><br/>Primary color</div>
+                    <div style={this.flexItem}><Button label='TEST' type='button' hasRipple={true} shape='raised' color='accent' handleOnClick={() => console.log(this)} /><br/><br/>Accent color</div>
+                </div>
+
+                <div style={this.flexContainer}>
+                    <h4 style={this.flexItemTitle}>FAB</h4>
+                    <div style={this.flexItem}><Button icon='add' type='button' shape='fab' handleOnClick={() => console.log(this)} /><br/><br/>FAB</div>
+                    <div style={this.flexItem}><Button icon='add' type='button' shape='fab' color='primary' handleOnClick={() => console.log(this)} /><br/><br/>Primary color</div>
+                    <div style={this.flexItem}><Button icon='add' type='button' shape='fab' color='accent' handleOnClick={() => console.log(this)} /><br/><br/>Accent color</div>
+                </div>
+
+                <div style={this.flexContainer}>
+                    <h4 style={this.flexItemTitle}>MINI-FAB</h4>
+                    <div style={this.flexItem}><Button icon='add' type='button' shape='mini-fab' handleOnClick={() => console.log(this)} /><br/><br/>Mini FAB</div>
+                    <div style={this.flexItem}><Button icon='add' type='button' shape='mini-fab' color='primary' handleOnClick={() => console.log(this)} /><br/><br/>Primary color</div>
+                    <div style={this.flexItem}><Button icon='add' type='button' shape='mini-fab' color='accent' handleOnClick={() => console.log(this)} /><br/><br/>Accent color</div>
+                </div>
             </div>
         );
     }
