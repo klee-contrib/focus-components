60fc12386ab79abcfcc1ccf58469116729647953
GuenoleK <kikabouguenole@gmail.com>
2016-03-21 12:18:21 +0100

    Updating search bar tests

--- src/search/search-bar/__tests__/scoped-search-bar-test.js
+++ src/search/search-bar/__tests__/scoped-search-bar-test.js
@@ -4,13 +4,6 @@ const SearchBar = require('../../search-bar').component;
 import {quickSearchStore} from 'focus-core/search/built-in-store';
 import actionBuilder from 'focus-core/search/action-builder';
 
-const action = {
-    updateProperties() {
-        return {
-            loading: true
-        };
-    }
-};
 
 describe('SearchBar with no scope', () => {
     describe('Check if a default search bar works fine', () => {
@@ -31,25 +24,57 @@ describe('SearchBar with no scope', () => {
             expect(component.refs.query.props.name).to.equal('searchbarinput');
         });
     });
-    describe.only('Check configured SearchBar\' behaviour', () => {
-        let component, onChangeSpy, input, inputChange;
+    describe('Check configured SearchBar\' behaviour', () => {
+        const action = {
+            updateProperties() {
+                return configuredComponent.state.query;
+            }
+        };
+        let configuredComponent = renderIntoDocument(<SearchBar hasScopes={false} store={quickSearchStore} action={action} />);
+        let onChangeSpy, input, inputChange, initialValue;
         before( () => {
-            component = renderIntoDocument(<SearchBar hasScopes={false} store={quickSearchStore} action={action} />);
-            input = component.refs.query.refs.htmlInput;
+            input = configuredComponent.refs.query.refs.htmlInput;
+            initialValue = configuredComponent.state.query;
             inputChange = input.props.onChange.bind(input);
             onChangeSpy = sinon.spy(inputChange);
         });
-        it('should change loading state to true', () => {
-            input.value = 'Boy';
-            Simulate.change(input);
-            console.log(component.state);
-
-            //expect(input.props.onChange.bind(input)).to.be.a('function');
-            //Simulate.change(input.props.onChange());
-            //input.value = 'test';
-            //Simulate.change(input.props.onChange(), {target: {value: 'a'}});
-            //expect(onChangeSpy).to.have.been.CallOnce;
-            //console.log(component.state);
+        describe('Simulate onChange behaviour', function() {
+            it('should set the loading state to true', () => {
+                input.value = ' ';
+                Simulate.change(input);
+                if(initialValue != configuredComponent.state.query)
+                    configuredComponent.setState({loading: true});
+                expect(onChangeSpy).to.have.been.CalledOnce;
+                expect(configuredComponent.state.loading).to.be.equal(true);
+            });
+            it('should change the query state (default query state is \'undefined\')', () => {
+                input.value = 'Boy';
+                Simulate.change(input);
+                expect(onChangeSpy).to.have.been.CalledOnce;
+                expect(initialValue).to.be.equal(undefined);
+                expect(configuredComponent.state.query).to.be.equal('Boy');
+            });
+        });
+        describe.only('Simulate onKeyPress behaviour', function() {
+            const action = {
+                updateProperties() {
+                    return configuredComponent.state.query;
+                }
+            };
+            let secondComponent = renderIntoDocument(<SearchBar hasScopes={false} store={quickSearchStore} action={action} />);
+            let onKeyPressSpy, input, inputChange, initialValue;
+            before( () => {
+                input = secondComponent.refs.query.refs.htmlInput;
+                initialValue = secondComponent.state.query;
+                //inputKeyPress = input.props.onKeyPress().bind(input);
+                //onKeyPressSpy = sinon.spy(inputKeyPress);
+            });
+            it('set the loading state to true', () => {
+                console.log(input.props.onKeyPress);
+                //Simulate.keyDown(input, {key: "Enter"});
+                //expec(onKeyPressSpy).to.have.been.CalledOnce;
+                // Put the test code here
+            });
         });
     });
 });
