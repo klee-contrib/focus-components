07a4044c928fed8677a7ae3296c3a4280809715a
Pierre Besson <pierre.besson7@gmail.com>
2016-03-22 14:06:25 +0100

    [input] Move the input text to the legacy dir

--- src/common/input/index.js
+++ src/common/input/index.js
@@ -1,7 +1,6 @@
 module.exports = {
     checkbox: require('./checkbox'),
     markdown: require('./markdown'),
-    text: require('./text'),
     textarea: require('./textarea'),
     toggle: require('./toggle'),
     radio: require('./radio')
