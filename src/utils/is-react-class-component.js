641e7254da7b9b27539c64e975c9a755681ea6b2
Pierre Besson <pierre.besson7@gmail.com>
2016-03-29 12:04:20 +0200

    [isreact compoennt]

--- src/utils/is-react-class-component.js
+++ src/utils/is-react-class-component.js
@@ -1,11 +1,18 @@
 //This function aims to test if a component is a
 
 // is a component a react class.
-const isReactClassComponent = ComponentToTest => {
+export const isReactClassComponent = ComponentToTest => {
  const prototype = ComponentToTest.prototype;
   if (!prototype) {
     return false;
   }
   return typeof prototype.render === 'function';
 }
-export default isReactClassComponent;
+
+export const addRefToPropsIfNotPure = (Component, props, ref) => (isReactClassComponent(Component) ? {...props, ref} : props);
+
+
+export LIST = 'list';
+export LINE = 'line';
+export INPUT = 'input';
+export DISPLAY = 'display';
