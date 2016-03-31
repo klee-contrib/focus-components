82dfc7839468dfa351d260dd495ce74c78bc9037
GuenoleK <kikabouguenole@gmail.com>
2016-03-31 15:26:25 +0200

    Updating style and form

--- src/components/input/autocomplete-text/consult.js
+++ src/components/input/autocomplete-text/consult.js
@@ -1,21 +1,11 @@
 import {Component} from 'react';
-import ComponentBaseBehaviour from '../../../behaviours/component-base';
 
-@ComponentBaseBehaviour
-class AutocompleteTextConsult extends Component {
-    state = {};
-    render() {
-        const {label, name, type, value} = this.props;
-        return (
-            <div
-                label={label}
-                name={name}
-                type={type}
-            >
-                {value}
-            </div>
-        );
-    }
+function AutocompleteTextConsult({label, name, type, value}) {
+    return (
+        <div label={label} name={name} type={type}>
+            {value}
+        </div>
+    );
 }
 
 export default AutocompleteTextConsult;
