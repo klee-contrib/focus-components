ae149efb5bae2053c1748c588c205a9801084e0c
GuenoleK <kikabouguenole@gmail.com>
2016-04-07 18:41:01 +0200

    deleting unused files

--- src/components/button/back/index.js
+++ src/components/button/back/index.js
@@ -1,27 +1,36 @@
-import  from 'react';
+import React from 'react';
 
 // Dependencies
 import builder from 'focus-core/component/builder';
 import historic from 'focus-core/history';
 
 // Mixins
+
 const i18nMixin = require('../../i18n/mixin');
-import {translate} from 'focus-core/translation';
+const stylableMixin = require('../../../mixin/stylable');
 
 // Components
-const ButtonAction = require('../action');
 
-import React {Component} from 'react';
+const Button = require('../action').component;
 
-const ButtonBack = ({label, name, type, value}) => {
-    return (
-        <Button
-            handleOnClick={() => {historic.history.back()}}
-            icon='keyboard_backspace'
-            label={translate('button.back')}
-            shape={null}
-            type='button' />
-    );
+/**
+* Mixin button.
+* @type {Object}
+*/
+const buttonBackMixin = {
+    /** inheritedDoc */
+    mixins: [i18nMixin, stylableMixin],
+    /** inheritedDoc */
+    render() {
+        return (
+            <Button
+                handleOnClick={() => {historic.history.back()}}
+                icon='keyboard_backspace'
+                label={this.i18n('button.back')}
+                shape={null}
+                type='button' />
+        );
+    }
 };
 
-export default ButtonBack;
+module.exports = builder(buttonBackMixin);
