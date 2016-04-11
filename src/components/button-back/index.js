bc2479546223cc63504097a58e30e3063cd6e2eb
bernardstanislas <bernardstanislas@gmail.com>
2016-04-11 10:14:17 +0200

    [button-back] Pure function

--- src/components/button-back/index.js
+++ src/components/button-back/index.js
@@ -1,24 +1,16 @@
-import React, {Component, PropTypes} from 'react';
+import React from 'react';
 import Button from '../button';
 
 // Dependencies
-import builder from 'focus-core/component/builder';
 import historic from 'focus-core/history';
-import TanslationBehaviour from '../../behaviours/translation';
 
-@TanslationBehaviour
-class ButtonBack extends Component {
-    render = () => {
-        return (
-            <Button
-                handleOnClick={() => {historic.history.back()}}
-                icon='keyboard_backspace'
-                label='button.back'
-                shape={null}
-                type='button' />
-        );
-    }
-}
+const ButtonBack = () => (
+    <Button
+        handleOnClick={() => {historic.history.back()}}
+        icon='keyboard_backspace'
+        label='button.back'
+        shape={null}
+        type='button' />
+);
 
-ButtonBack.displayName = 'ButtonBack';
 export default ButtonBack;
