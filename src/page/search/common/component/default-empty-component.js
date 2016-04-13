7801206c24902705cf356e18f7e901311e5f82e9
GuenoleK <kikabouguenole@gmail.com>
2016-04-13 13:51:25 +0200

    Writing component as a function

--- src/page/search/common/component/default-empty-component.js
+++ src/page/search/common/component/default-empty-component.js
@@ -3,14 +3,12 @@ import React, {Component} from 'react';
 import builder from 'focus-core/component/builder';
 import {translate} from 'focus-core/translation';
 
-class DefaultEmpty extends Component {
-    render() {
-        return (
-            <div data-focus='empty-result'>
-                {translate('search.empty')}
-            </div>
-        );
-    }
+function DefaultEmpty() {
+    return (
+        <div data-focus='empty-result'>
+            {translate('search.empty')}
+        </div>
+    );
 }
 
 module.exports = DefaultEmpty;
