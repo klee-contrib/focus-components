c3d90baa88c5a2e6d7ff2252958b3456e3099072
GuenoleK <kikabouguenole@gmail.com>
2016-04-14 15:16:54 +0200

    Adding warning message

--- src/common/empty/index.js
+++ src/common/empty/index.js
@@ -1,6 +1,9 @@
 import builder from 'focus-core/component/builder';
 var React = require('react');
 let emptyMixin = {
+    componentWillMount () {
+        console.warn('FocusComponents v0.15: the \'Empty\' component from FocusComponents.common is deprecated, please use FocusComponents.components.Empty');
+    },
     render() {
         return <div data-focus='empty'></div>
     }
