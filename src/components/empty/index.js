e79d389c3f9cb5cf66a493c0140fef1d19252c36
GuenoleK <kikabouguenole@gmail.com>
2016-04-14 15:28:05 +0200

    Rewriting the component

--- src/components/empty/index.js
+++ src/components/empty/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
-function Empty() {
-    return(
-        <div data-focus='empty'></div>
-    );
-}
+const = () => <div data-focus='empty'></div>
 
 Empty.displayName = 'Empty';
 
