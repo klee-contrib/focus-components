0a996986c9e016f43cc97c7bfa3c5fabe97fe0bc
GuenoleK <kikabouguenole@gmail.com>
2016-04-18 14:11:09 +0200

    displayCheckbox refactoring

--- src/components/display/checkbox/index.js
+++ src/components/display/checkbox/index.js
@@ -1,50 +1,35 @@
-//Dependencies.
-import builder from 'focus-core/component/builder';
-var React = require('react');
-import type from 'focus-core/component/types';
-var i18nBehaviour = require('../../i18n/mixin');
+import React, {PropTypes} from 'react';
+import {translate} from 'focus-core/translation';
+
 /**
- * Input text mixin.
- * @type {Object}
- */
-var displayCheckboxMixin = {
-    mixins: [i18nBehaviour],
-  /** @inheritdoc */
-    getDefaultProps: function getInputDefaultProps() {
-        return {
-            value: undefined,
-            name: undefined,
-            style: {}
-        };
-    },
-  /** @inheritdoc */
-    propTypes: {
-        type: type('string'),
-        value: type('bool'),
-        name: type('string'),
-        style: type('object')
-    },
-  /**
-   * Render the boolean value.
-   */
-    renderValue: function renderValueDisplayText() {
-        var stringValue = this.props.value === true ? 'true' : 'false';
-        return this.i18n(`display.checkbox.${stringValue}`);
-    },
-  /**
-   * Render a display field.
-   * @return {DOM} - The dom of an input.
-   */
-    render: function renderInput() {
-        return (
-      <div
-          id={this.props.name}
-          name={this.props.name}
-          className={this.props.style.class}
-      >{this.renderValue()}</div>
+* Render the boolean value.
+*/
+renderValue: function renderValueDisplayText(value) {
+    var stringValue = value === true ? 'true' : 'false';
+    return translate(`display.checkbox.${stringValue}`);
+}
+
+function displayCheckbox({name, value, style}) {
+    return(
+        <div id={name} name={name} className={style.class}>
+            {this.renderValue(value)}
+        </div>
     );
-    }
+}
+
+displayCheckbox.defaultProps = {
+    value: undefined,
+    name: undefined,
+    style: {}
 };
 
+displayCheckbox.propTypes = {
+    type: PropTypes.string,
+    value: PropTypes.bool,
+    name: PropTypes.string,
+    style: PropTypes.object,
+};
 
-module.exports = builder(displayCheckboxMixin);
+displayCheckbox.displayName = 'DisplayCheckbox';
+
+module.exports = displayCheckbox;
