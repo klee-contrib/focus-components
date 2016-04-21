9e10d7c55c9a2959418e24c02de1c248591f4d9b
GuenoleK <kikabouguenole@gmail.com>
2016-04-21 17:50:03 +0200

    Reactoring displayText

--- src/components/display/text/index.js
+++ src/components/display/text/index.js
@@ -1,37 +1,34 @@
 import React, {PropTypes, Component} from 'react';
-import Translation from '../../../behaviours/translation';
+import {translate} from 'focus-core/translation';
+// import Translation from '../../../behaviours/translation';
 
-@Translation
-class DisplayText extends Component {
-    static defaultProps = {
-        formatter: (data) => data
-    };
-
-    static propTypes = {
-        type: PropTypes.string,
-        value: PropTypes.oneOfType(['string', 'number']),
-        name: PropTypes.string,
-        style: PropTypes.object
-    };
-
-    /**
-    * Render the value.
-    * @return {string} The formated value.
-    */
-    renderValue() {
-        const {formatter, value} = this.props;
-        return formatter(value);
-    }
-
-    render() {
-        return(
-            <div {...this.props}>
-                {this.renderValue()}
-            </div>
-        );
-    }
+/**
+* Render the value.
+* @return {string} The formated value.
+*/
+function renderValue(formatter, value) {
+    return formatter(value);
 }
+
+function DisplayText({formatter, value, ...otherProps}) {
+    return(
+        <div {...otherProps}>
+            {translate(this.renderValue(formatter, value))}
+        </div>
+    );
+}
+
+DisplayText.defaultProps = {
+    formatter: (data) => data
+};
+
+DisplayText.propTypes = {
+    type: PropTypes.string,
+    value: PropTypes.oneOfType(['string', 'number']),
+    name: PropTypes.string,
+    style: PropTypes.object
+};
 
 DisplayText.displayName = 'DisplayText';
 
-export default DisplayText;
+module.exports = DisplayText;
