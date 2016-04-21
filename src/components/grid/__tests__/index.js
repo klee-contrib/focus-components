898c8d696a95a89ff30efa5f6cdc763be1aa4296
GuenoleK <kikabouguenole@gmail.com>
2016-04-21 14:16:03 +0200

    Adding grid tests

---
+++ src/components/grid/__tests__/index.js
@@ -0,0 +1,25 @@
+import Grid from '../';
+const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} = TestUtils;
+
+const Wrapper = React.createClass({
+    render: function() {
+        return (
+            <div>{this.props.children}</div>
+        );
+    }
+});
+
+describe.only('Select Grid Component', () => {
+    describe('Rendering', () => {
+        describe('When a default grid is rendered', () => {
+            let renderedGrid, arr;
+            before(() => {
+                renderedGrid = renderIntoDocument(<Wrapper><Grid /></Wrapper>);
+                arr = scryRenderedDOMComponentsWithTag(renderedGrid, 'div');
+            });
+            it('should the default cell className', () => {
+                expect(arr[1].className.trim()).to.equal('mdl-grid')
+            });
+        });
+    });
+});
