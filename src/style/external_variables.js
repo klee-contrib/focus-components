d32307ee5e5a2f97da1c06e0840281072b48acc1
JoanaDalmeida <celestina.pricilia@gmail.com>
2016-05-02 16:33:25 +0200

    Extract external varaibles.

--- src/style/external_variables.js
+++ src/style/external_variables.js
@@ -1,5 +1,30 @@
 module.exports = {
+    //external variables from application/menu
     '$menu-brand-background-color': '#000000',
     '$menu-background-color': '#36b7b9',
-    '$menu-size': '60px'
+    '$menu-size': '60px',
+
+    //external variables from application/bar
+    '$bar-menu-size': '60px',
+    '$bar-padding': '10px',
+    '$bar-summary-left': '200px',
+    '$bar-summary-right':'200px',
+
+    //external variables from application/header
+    '$header-shadow-color': '#EAEAEA',
+
+    //external variables from application/layout
+    '$layout-background-color': '#FFF',
+    '$layout-scrollbar-color': 'rgb(161, 175, 181)',
+    '$layout-scrollbar-hover-color': '#6AB2E5',
+    '$layout-scrollbar-background-color': '#e2e2e2',
+
+    //external variables from application/popin
+    '$small-size': '100/3',
+    '$medium-size': '100/2',
+    '$large-size': '200/3',
+    '$overlay-background-color': 'rgba(0,0,0, 0.70)',
+    '$full-margin-top': '80px',
+    '$menu-width': '60px',
+    '$fill-height-popin-margin': '200px'
 };
--- src/style/index.js
+++ src/style/index.js
@@ -1,5 +1,6 @@
-import 'react-date-picker/index.css';
-import 'material-design-lite/src/material-design-lite.scss';
+import '../application/popin/style/popin.scss';
+/*import 'react-date-picker/index.css';
+//import 'material-design-lite/src/material-design-lite.scss';
 //import '../style/_mdl_variables.scss';
 //---------------------------------------------------------------------------------------
 
@@ -81,4 +82,4 @@ import '../search/search-bar/style/search-bar.scss';
 
 //STYLE FOLDER
 import '../style/animation.scss';
-import '../style/spinner.scss';
+import '../style/spinner.scss';*/
