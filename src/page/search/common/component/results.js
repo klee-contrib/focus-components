057daf4dba6938fed5f4572605f92ca911ef5ca6
JoanaDalmeida <celestina.pricilia@gmail.com>
2016-05-04 16:44:43 +0200

    Update _getGroupCounts method to handle the server response which contains only one group with results

--- src/page/search/common/component/results.js
+++ src/page/search/common/component/results.js
@@ -93,15 +93,15 @@ const Results = {
             if (this.props.renderSingleGroupDecoration) {
                 return (
                     <GroupWrapper
-                        count={count}
-                        groupComponent={this.props.groupComponent}
-                        groupKey={key}
-                        initialRowsCount={initialRowsCount}
-                        isUnique
-                        list={list}
-                        ref={`group-${key}`}
-                        renderResultsList={this._renderResultsList}
-                        showAllHandler={this._showAllHandler}
+                    count={count}
+                    groupComponent={this.props.groupComponent}
+                    groupKey={key}
+                    initialRowsCount={initialRowsCount}
+                    isUnique
+                    list={list}
+                    ref={`group-${key}`}
+                    renderResultsList={this._renderResultsList}
+                    showAllHandler={this._showAllHandler}
                     />
                 );
             } else {
@@ -110,15 +110,15 @@ const Results = {
         } else {
             return (
                 <GroupWrapper
-                    count={count}
-                    groupComponent={this.props.groupComponent}
-                    groupKey={key}
-                    initialRowsCount={initialRowsCount}
-                    key={key}
-                    list={list}
-                    ref={`group-${key}`}
-                    renderResultsList={this._renderResultsList}
-                    showAllHandler={this._showAllHandler}
+                count={count}
+                groupComponent={this.props.groupComponent}
+                groupKey={key}
+                initialRowsCount={initialRowsCount}
+                key={key}
+                list={list}
+                ref={`group-${key}`}
+                renderResultsList={this._renderResultsList}
+                showAllHandler={this._showAllHandler}
                 />
             );
         }
@@ -158,28 +158,28 @@ const Results = {
         const hasMoreData = isUnique !== undefined && isUnique && list.length < count;
         return (
             <div>
-                <ListSelection
-                    data={list}
-                    data-focus='results-list'
-                    fetchNextPage={this._onScrollReachedBottom}
-                    hasMoreData={hasMoreData}
-                    idField={idField}
-                    isSelection={isSelection}
-                    LineComponent={LineComponent}
-                    onLineClick={lineClickHandler}
-                    onSelection={lineSelectionHandler}
-                    operationList={lineOperationList}
-                    parentSelector={scrollParentSelector}
-                    ref={`list-${key}`}
-                    selectionData={selectionData}
-                    selectionStatus={selectionStatus}
-                    {...otherProps}
-                />
-                {this.state.loading &&
-                    <div data-focus='loading-more-results'>
-                        {translate('search.loadingMore')}
-                    </div>
-                }
+            <ListSelection
+            data={list}
+            data-focus='results-list'
+            fetchNextPage={this._onScrollReachedBottom}
+            hasMoreData={hasMoreData}
+            idField={idField}
+            isSelection={isSelection}
+            LineComponent={LineComponent}
+            onLineClick={lineClickHandler}
+            onSelection={lineSelectionHandler}
+            operationList={lineOperationList}
+            parentSelector={scrollParentSelector}
+            ref={`list-${key}`}
+            selectionData={selectionData}
+            selectionStatus={selectionStatus}
+            {...otherProps}
+            />
+            {this.state.loading &&
+                <div data-focus='loading-more-results'>
+                {translate('search.loadingMore')}
+                </div>
+            }
             </div>
         );
     },
@@ -273,14 +273,15 @@ const Results = {
         resultsMap = resultsMap ? resultsMap : this.props.resultsMap;
         // resultMap can be either an Array or an Object depending of the search being grouped or not.
         if (resultsMap && isArray(resultsMap) && 1 === resultsMap.length) {
+
             //Check if the resultMap contains an entry which is an array.
             const isResultMapEntryAnArray = isArray(resultsMap[0]);
             if(isResultMapEntryAnArray) {
-              return {
-                  [resultsMap[0][0]]: {
-                      count: this.props.totalCount
-                  }
-              };
+                return {
+                    [resultsMap[0][0]]: {
+                        count: this.props.totalCount
+                    }
+                };
             }
             //this case occurs when the server response contains only one group with results.
             return {
