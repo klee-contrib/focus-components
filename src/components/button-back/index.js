0f8cdfb3ec4a0107010a0214f4292814851e43bb
GuenoleK <kikabouguenole@gmail.com>
2016-05-20 15:46:20 +0200

    Updating the button back to have the right behaviour when it backbone or react-router

--- src/components/button-back/index.js
+++ src/components/button-back/index.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
 import Button from '../button';
+import Translation from '../../behaviours/translation';
 
 // Dependencies
-import historic from 'focus-core/history';
+import {history, goBack} from 'focus-core/history';
 
-const ButtonBack = () => (
-    <Button
-        handleOnClick={() => {historic.history.back()}}
-        icon='keyboard_backspace'
-        label='button.back'
-        shape={null}
-        type='button' />
-);
+@Translation
+class ButtonBack extends Component {
+
+    static propTypes = {
+        reactRouterNavigation: PropTypes.bool
+    };
+
+    _renderButtonBack() {
+        const {reactRouterNavigation} = this.props;
+        switch (reactRouterNavigation) {
+            case true:
+            return <Button handleOnClick={() => {goBack()}} icon='keyboard_backspace' label={this.i18n('button.back')} shape={null} type='button' />
+            break;
+            default:
+            return <Button handleOnClick={() => {history.back()}} icon='keyboard_backspace' label={this.i18n('button.back')} shape={null} type='button' />
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                {this._renderButtonBack()}
+            </div>
+        );
+    }
+}
+
+ButtonBack.displayName = 'ButtonBack';
+ButtonBack.defaultProps = {
+    reactRouterNavigation: false
+}
 
 export default ButtonBack;
