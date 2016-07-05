16086d7224aea29cfa7c72a067cf7923d35a6c3d
Damien Frikha <JabX@users.noreply.github.com>
2016-07-05 15:17:15 +0200

    Default back function on ButtonBack

--- src/components/button-back/index.js
+++ src/components/button-back/index.js
@@ -3,20 +3,23 @@ import Button from '../button';
 import {back} from 'focus-core/history';
 
 // Dependencies
-import historic from 'focus-core/history';
+import history from 'focus-core/history';
 
 const propTypes = {
-    back: PropTypes.func.isRequired
+    back: PropTypes.func
 };
 
-const ButtonBack = ({back}) => (
-    <Button
-        handleOnClick={back}
-        icon='keyboard_backspace'
-        label='button.back'
-        shape={null}
-        type='button' />
-);
+function ButtonBack({back}) {
+    return (
+        <Button
+            handleOnClick={back || history.back}
+            icon='keyboard_backspace'
+            label='button.back'
+            shape={null}
+            type='button' 
+        />
+    );
+}
 
 ButtonBack.propTypes = propTypes;
 
