64b099ce947609e9706a1e8a59bd62afd76a55ad
Thomas <tgallon@gmail.com>
2016-07-26 14:33:38 +0200

    remove reactid from autocomplete select

--- src/components/input/autocomplete-select/edit.js
+++ src/components/input/autocomplete-select/edit.js
@@ -1,8 +1,11 @@
 import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
-import debounce from 'lodash/function/debounce';
 import ComponentBaseBehaviour from '../../../behaviours/component-base';
 import MDBehaviour from '../../../behaviours/material';
+
+import closest from 'closest';
+import debounce from 'lodash/function/debounce';
+import uniqueId from 'lodash/utility/uniqueId';
 
 const ENTER_KEY_CODE = 13;
 const TAB_KEY_CODE = 27;
@@ -43,6 +46,10 @@ class Autocomplete extends Component {
         isLoading: false,
         customError: this.props.customError,
         totalCount: 0
+    };
+
+    componentWillMount() {
+        this.autocompleteId = uniqueId('autocomplete-text-');
     };
 
     componentDidMount() {
@@ -101,12 +108,9 @@ class Autocomplete extends Component {
         const {focus, inputValue} = this.state;
         const {onBadInput} = this.props;
         if (focus) {
-            const dataset = target ? target.dataset: null;
-            const reactid = dataset ? dataset.reactid : null;
-            const [options, input] = ['options', 'htmlInput'].map(ref => ReactDOM.findDOMNode(this.refs[ref]));
-            const optionsId = options ? options.dataset.reactid : null;
-            const inputId = input ? input.dataset.reactid : null;
-            if (reactid && optionsId && inputId && reactid.indexOf(optionsId) === -1 && reactid.indexOf(inputId) === -1) {
+            const closestACParent = closest(target, `[data-id='${this.autocompleteId}']`, true);
+            console.log(closestACParent);
+            if(closestACParent === undefined) {
                 this.setState({focus: false}, () => {
                     if (onBadInput && this.getValue() === null && inputValue !== '') {
                         onBadInput(inputValue);
@@ -118,121 +122,121 @@ class Autocomplete extends Component {
 
     _handleQueryChange = ({target: {value}}) => {
         if (value === '') { // the user cleared the input, don't call the querySearcher
-            const {onChange} = this.props;
-            this.setState({inputValue: value, fromKeyResolver: false});
-            if (onChange) onChange(null);
-        } else {
-            this.setState({inputValue: value, fromKeyResolver: false, isLoading: true});
-            this._debouncedQuerySearcher(value);
-        }
-    };
+        const {onChange} = this.props;
+        this.setState({inputValue: value, fromKeyResolver: false});
+        if (onChange) onChange(null);
+    } else {
+        this.setState({inputValue: value, fromKeyResolver: false, isLoading: true});
+        this._debouncedQuerySearcher(value);
+    }
+};
 
-    _querySearcher = value => {
-        const {querySearcher, keyName, labelName} = this.props;
-        querySearcher(value).then(({data, totalCount}) => {
-            // TODO handle the incomplete option list case
-            const options = new Map();
-            data.forEach(item => {
-                options.set(item[keyName], item[labelName]);
-            });
-            this.setState({options, isLoading: false, totalCount});
-        }).catch(error => this.setState({customError: error.message}));
-    };
-
-    _handleQueryFocus = () => {
-        this.refs.options.scrollTop = 0;
-        if (this.props.onFocus) {
-            this.props.onFocus.call(this);
-        }
-        this.setState({active: '', focus: true});
-    };
-
-    _handleQueryKeyDown = (event) => {
-        event.stopPropagation();
-        const {which} = event;
-        const {active, options} = this.state;
-        if (which === ENTER_KEY_CODE && active) this._select(active);
-        if (which === TAB_KEY_CODE) this.setState({focus: false}, () => this.refs.htmlInput.blur());
-        if ([DOWN_ARROW_KEY_CODE, UP_ARROW_KEY_CODE].indexOf(which) !== -1) { // the user pressed on an arrow key, change the active key
-            const optionKeys = [];
-            for (let key of options.keys()) {
-                optionKeys.push(key);
-            }
-            const currentIndex = optionKeys.indexOf(active);
-            let newIndex = currentIndex + (which === DOWN_ARROW_KEY_CODE ? 1 : -1);
-            if (newIndex >= options.size) {
-                newIndex -= options.size
-            }
-            if (newIndex < 0) {
-                newIndex += options.size;
-            }
-            this.setState({active: optionKeys[newIndex]});
-        }
-    };
-
-    _handleSuggestionHover = key => {
-        this.setState({active: key});
-    };
-
-    _select(key) {
-        const {options} = this.state;
-        const {onChange, keyName, labelName} = this.props;
-        const resolvedLabel = options.get(key) || '';
-        this.refs.htmlInput.blur();
-        this.setState({inputValue: this.i18n(resolvedLabel), selected: key, focus: false}, () => {
-            if (onChange) onChange(key);
+_querySearcher = value => {
+    const {querySearcher, keyName, labelName} = this.props;
+    querySearcher(value).then(({data, totalCount}) => {
+        // TODO handle the incomplete option list case
+        const options = new Map();
+        data.forEach(item => {
+            options.set(item[keyName], item[labelName]);
         });
-    }
+        this.setState({options, isLoading: false, totalCount});
+    }).catch(error => this.setState({customError: error.message}));
+};
 
-    _renderOptions = () => {
-        const {active, options, focus} = this.state;
-        const renderedOptions = [];
-        for (let [key, value] of options) {
-            const isActive = active === key;
-            renderedOptions.push(
-                <li
-                    data-active={isActive}
-                    data-focus='option'
-                    key={key}
-                    onClick={this._select.bind(this, key)}
-                    onMouseOver={this._handleSuggestionHover.bind(this, key)}
-                >
-                    {this.i18n(value)}
-                </li>
-            );
+_handleQueryFocus = () => {
+    this.refs.options.scrollTop = 0;
+    if (this.props.onFocus) {
+        this.props.onFocus.call(this);
+    }
+    this.setState({active: '', focus: true});
+};
+
+_handleQueryKeyDown = (event) => {
+    event.stopPropagation();
+    const {which} = event;
+    const {active, options} = this.state;
+    if (which === ENTER_KEY_CODE && active) this._select(active);
+    if (which === TAB_KEY_CODE) this.setState({focus: false}, () => this.refs.htmlInput.blur());
+    if ([DOWN_ARROW_KEY_CODE, UP_ARROW_KEY_CODE].indexOf(which) !== -1) { // the user pressed on an arrow key, change the active key
+        const optionKeys = [];
+        for (let key of options.keys()) {
+            optionKeys.push(key);
         }
-        return (
-            <ul data-focus='options' ref='options' data-focussed={focus}>
-                {renderedOptions}
-            </ul>
-        );
-    };
+        const currentIndex = optionKeys.indexOf(active);
+        let newIndex = currentIndex + (which === DOWN_ARROW_KEY_CODE ? 1 : -1);
+        if (newIndex >= options.size) {
+            newIndex -= options.size
+        }
+        if (newIndex < 0) {
+            newIndex += options.size;
+        }
+        this.setState({active: optionKeys[newIndex]});
+    }
+};
 
-    render () {
-        const {customError, placeholder, renderOptions, ...inputProps} = this.props;
-        const {inputValue, isLoading} = this.state;
-        const {_handleQueryFocus, _handleQueryKeyDown, _handleQueryChange} = this;
-        return (
-            <div data-focus='autocomplete'>
-                <div className={`mdl-textfield mdl-js-textfield${customError ? ' is-invalid' : ''}`} data-focus='input-text' ref='inputText'>
-                    <div data-focus='loading' data-loading={isLoading} className='mdl-progress mdl-js-progress mdl-progress__indeterminate' ref='loader'/>
-                    <input
-                        className='mdl-textfield__input'
-                        {...inputProps}
-                        onChange={_handleQueryChange}
-                        onFocus={_handleQueryFocus}
-                        onKeyDown={_handleQueryKeyDown}
-                        ref='htmlInput'
-                        type='text'
-                        value={inputValue}
-                    />
-                    <label className='mdl-textfield__label'>{this.i18n(placeholder)}</label>
-                    <span className='mdl-textfield__error'>{this.i18n(customError)}</span>
-                </div>
-                {renderOptions ? renderOptions.call(this) : this._renderOptions()}
-            </div>
+_handleSuggestionHover = key => {
+    this.setState({active: key});
+};
+
+_select(key) {
+    const {options} = this.state;
+    const {onChange, keyName, labelName} = this.props;
+    const resolvedLabel = options.get(key) || '';
+    this.refs.htmlInput.blur();
+    this.setState({inputValue: this.i18n(resolvedLabel), selected: key, focus: false}, () => {
+        if (onChange) onChange(key);
+    });
+}
+
+_renderOptions = () => {
+    const {active, options, focus} = this.state;
+    const renderedOptions = [];
+    for (let [key, value] of options) {
+        const isActive = active === key;
+        renderedOptions.push(
+            <li
+            data-active={isActive}
+            data-focus='option'
+            key={key}
+            onClick={this._select.bind(this, key)}
+            onMouseOver={this._handleSuggestionHover.bind(this, key)}
+            >
+            {this.i18n(value)}
+            </li>
         );
     }
+    return (
+        <ul data-focus='options' ref='options' data-focussed={focus}>
+        {renderedOptions}
+        </ul>
+    );
+};
+
+render () {
+    const {customError, placeholder, renderOptions, ...inputProps} = this.props;
+    const {inputValue, isLoading} = this.state;
+    const {_handleQueryFocus, _handleQueryKeyDown, _handleQueryChange} = this;
+    return (
+        <div data-focus='autocomplete' data-id={this.autocompleteId}>
+        <div className={`mdl-textfield mdl-js-textfield${customError ? ' is-invalid' : ''}`} data-focus='input-text' ref='inputText'>
+        <div data-focus='loading' data-loading={isLoading} className='mdl-progress mdl-js-progress mdl-progress__indeterminate' ref='loader'/>
+        <input
+        className='mdl-textfield__input'
+        {...inputProps}
+        onChange={_handleQueryChange}
+        onFocus={_handleQueryFocus}
+        onKeyDown={_handleQueryKeyDown}
+        ref='htmlInput'
+        type='text'
+        value={inputValue}
+        />
+        <label className='mdl-textfield__label'>{this.i18n(placeholder)}</label>
+        <span className='mdl-textfield__error'>{this.i18n(customError)}</span>
+        </div>
+        {renderOptions ? renderOptions.call(this) : this._renderOptions()}
+        </div>
+    );
+}
 }
 
 export default Autocomplete;
