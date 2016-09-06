954602e81066ae71c81f52cb492e6a8fb86e3a45
Thomas <tgallon@gmail.com>
2016-09-06 15:41:42 +0200

    build

--- src/style/index.js
+++ src/style/index.js
@@ -4,39 +4,33 @@ import 'material-design-lite/src/material-design-lite.scss';
 //---------------------------------------------------------------------------------------
 
 //APPLICATION FOLDER
-import '../application/confirmation-popin/style/confirmation-popin.scss';
-import '../application/menu/style/menu.scss';
 import '../application/popin/style/popin.scss';
 
 // COMPONENTS FOLDER
 import '../components/button/style/button.scss';
 import '../components/button-back/style/back.scss';
-import '../components/draggable-iframe/style/draggable-iframe.scss';
 import '../components/button-back-to-top/style/back-to-top.scss';
-import '../components/label/style/label.scss';
-import '../components/icon-dropdown/style.scss';
+import '../components/chips/index.scss';
+import '../components/confirmation-popin/index.scss';
+import '../components/draggable-iframe/style/draggable-iframe.scss';
+import '../components/dropdown/index.scss';
+import '../components/icon-dropdown/index.scss';
 import '../components/input/autocomplete-select/style.scss';
 import '../components/input/autocomplete-text/style.scss';
 import '../components/input/checkbox/style/checkbox.scss';
 import '../components/input/date/style/index.scss';
 import '../components/input/textarea/style/textarea.scss';
 import '../components/input/radio/style/radio.scss';
+import '../components/input/select-mdl/index.scss';
 import '../components/input/select/style/select.scss';
 import '../components/input/select-radio/style/select-radio.scss';
+import '../components/label/style/label.scss';
 import '../components/menu/style/menu.scss';
 import '../components/message-center/style/message-center.scss';
 import '../components/panel/style/panel.scss'
 import '../components/progress-bar/style/progress-bar.scss';
 import '../components/scrollspy-container/style/scrollspy-container.scss';
 import '../components/scrollspy-container/style/sticky-menu.scss';
-
-// LIST FOLDER
-// import '../list/action-bar/style/action-bar.scss';
-// import '../list/action-contextual/style/contextual-ation.scss';
-// import '../list/selection/style/list.scss';
-// import '../list/summary/style/list-summary.scss';
-// import '../list/table/style/table.scss';
-// import '../list/timeline/style/timeline.scss';
 
 //STYLE FOLDER
 import '../style/animation.scss';
