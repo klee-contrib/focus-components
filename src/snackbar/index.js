788025e2ddffce11fd91b725cd3de1f65dcab858
Pierre Besson <pierre.besson7@gmail.com>
2016-09-16 16:19:33 +0200

    [snackbar] Fix material behaviour.

--- src/snackbar/index.js
+++ src/snackbar/index.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
 
-function Snackbar({id,content, title, deleteMessage, actionHandler, actionText}){
+function Snackbar(props){
   return (
     <div className="mdl-js-snackbar mdl-snackbar mdl-snackbar--active animated slideInUp" data-upgraded="MaterialSnackbar" aria-hidden="false">
       <div className="mdl-snackbar__text">{props.content}</div>
