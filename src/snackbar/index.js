45847769438173ad753d6fb29fcfa858e25afa7b
Pierre Besson <pierre.besson7@gmail.com>
2016-09-16 14:11:01 +0200

    [snackbar]

--- src/snackbar/index.js
+++ src/snackbar/index.js
@@ -1,19 +1,18 @@
 import React, {PropTypes} from 'react';
-import i18next from 'i18next';
 
-const Snackbar = ({actionHandler, actionText, message, deleteMessage, type}) => {
-    const otherProps = { 'aria-hidden': !!message, 'aria-live':'assertive', 'aria-atomic':'true', 'aria-relevant': 'text' };
-    const hasAction = actionText && actionHandler;
-    return (
-        <div data-focus='snackbar' data-upgraded='MaterialSnackbar' data-message-type={type} className='mdl-js-snackbar mdl-snackbar mdl-snackbar--active animated slideInUp' {...otherProps}>
-            <div className='mdl-snackbar__text'>{i18next.t(message)}</div>
-            {hasAction &&
-                <button className='mdl-snackbar__action' type='button' onClick={actionHandler}>{i18next.t(actionText)}</button>
+function Snackbar({id,content, title, deleteMessage, actionHandler, actionText}){
+  return (
+    <div className="mdl-js-snackbar mdl-snackbar mdl-snackbar--active animated slideInUp" data-upgraded="MaterialSnackbar" aria-hidden="false">
+      <div className="mdl-snackbar__text">{props.content}</div>
+      {actionHandler && actionText &&
+                <button className='mdl-snackbar__action' type='button' onClick={() =>{
+                actionHandler(props); deleteMessage({id});
+                }}>{actionText}</button>
             }
-            <button className='mdl-snackbar__close' type='button' onClick={deleteMessage}><i className='material-icons'>clear</i></button>
-        </div>
-    );
-};
+      <button className="mdl-snackbar__action" type="button" onClick={() => deleteMessage({id})}>Close</button>
+    </div>
+  );
+}
 Snackbar.displayName = 'Snackbar';
 Snackbar.propTypes = {
     actionHandler: PropTypes.func,
