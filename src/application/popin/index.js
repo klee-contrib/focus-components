03dd8a88612069d7cb72a69ea29c4c05ddfff459
marine sabbah <marine.sabbah@kleegroup.com>
2017-05-22 17:37:05 +0200

    Popin : ajout de role="dialog"  et aria-labelledBy Panel : ajout de l'id en lien avec la popin
    # Conflicts:
    #	src/components/panel/index.js

--- src/application/popin/index.js
+++ src/application/popin/index.js
@@ -163,7 +163,7 @@ const popin = {
             <div data-focus='popin' data-level={level} data-size={this._validateSize()} data-type={type} >
                 {this.state.opened &&
                     <Overlay clickHandler={modal && this.toggleOpen} ref='popin-overlay' resize={'full' === type} show={overlay}>
-                        <div {...this._getAnimationProps() } data-focus='popin-window' role='dialog' onClick={this._preventPopinClose} ref='popin-window'>
+                        <div {...this._getAnimationProps() } data-focus='popin-window' {...optionnalModalProps} role='dialog' onClick={this._preventPopinClose} ref='popin-window'>
                             <i className='material-icons' data-focus='popin-window-close' onClick={this.toggleOpen}>close</i>
                             <div onWheel={this._onWheel}>
                                 {children}
