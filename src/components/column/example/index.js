a9ea95f18fccffeb6f6276a646dda2715f4cf55b
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2017-08-01 16:08:03 +0200

    clean column

--- src/components/column/example/index.js
+++ src/components/column/example/index.js
@@ -1,24 +1,27 @@
-const {Grid} = FocusComponents.components;
-const {Column} = FocusComponents.components;
+import Grid from 'focus-components/components/grid'
+import Column from 'focus-components/components/column';
 
-// Blue square to  fill the grid.
+const style = {
+    backgroundColor: '#BDBDBD',
+    height: '200px',
+    color: 'white'
+};
 
-const style = {backgroundColor: '#BDBDBD', height: '200px', color: 'white'};
-function BlueSquare({label}) {
-    return(
-        <div style={style}>{label}</div>;
+function BlueSquare({ label }) {
+    return (
+        <div style={style}>{label}</div>
     );
 }
 
 function GridColumnSample() {
-    return(
+    return (
         <Grid>
-            <Column size='4'><BlueSquare label='size4'/></Column>
-            <Column size='3'><BlueSquare label='size3'/></Column>
-            <Column size='5'><BlueSquare label='size5'/></Column>
-            <Column size='4'><BlueSquare label='size4'/></Column>
+            <Column size='4'><BlueSquare label='size4' /></Column>
+            <Column size='3'><BlueSquare label='size3' /></Column>
+            <Column size='5'><BlueSquare label='size5' /></Column>
+            <Column size='4'><BlueSquare label='size4' /></Column>
         </Grid>
     );
 }
 
-return <GridColumnSample/>;
+export default GridColumnSample;
--- src/components/column/index.js
+++ src/components/column/index.js
@@ -1,34 +1,41 @@
 // see http://www.getmdl.io/components/index.html#layout-section/grid
-//dependencies
-import React, {Component, PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import filterProps from '../../utils/filter-html-attributes';
+
+/**
+ * Compute class name.
+ * @param {string} className base class name.
+ * @param {number} size number of columns.
+ */
+function _className(className, size) {
+    const SIZE_CSS = size ? `mdl-cell--${size}-col` : '';
+    return `mdl-cell ${SIZE_CSS} ${className}`;
+}
 
 /**
  * Column component.
  */
-
-function _className(className, size) {
-    const SIZE_CSS = size ? `mdl-cell--${size}-col` : '';
-    return `mdl-cell ${SIZE_CSS} ${className ? className : ''}`;
-};
-
-function Column({size, className, children, ...otherProps}) {
-
-    return(
-        <div className={_className(className, size) } {...filterProps(otherProps)}>
+function Column({ size, className, children, ...otherProps }) {
+    return (
+        <div
+            className={_className(className, size)}
+            {...filterProps(otherProps)}
+        >
             {children}
         </div>
     );
 }
 
-//Static props.
 Column.displayName = 'Column';
-Column.defaultProps = {
-    size: 6
-};
+
 Column.propTypes = {
     size: PropTypes.number,
     className: PropTypes.string
 };
 
-module.exports = Column;
+Column.defaultProps = {
+    size: 6,
+    className: ''
+};
+
+export default Column;
