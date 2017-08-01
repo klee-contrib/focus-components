bcf06e0194059d50aa420dd27d02f8a0ceb2b154
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2017-08-01 17:23:44 +0200

    clean input-text

--- src/components/input/text/example/index.jsx
+++ src/components/input/text/example/index.jsx
@@ -1,6 +1,7 @@
-const Input = FocusComponents.components.input.Text;
+import Input from 'focus-components/components/input/text';
 
 class InputTextExample extends React.Component {
+
     state = {
         inputWithValue: 'myAwesomeValue',
         inputWithError: 'errorValue'
@@ -8,26 +9,26 @@ class InputTextExample extends React.Component {
 
     onChangeInput(name) {
         return value => {
-            this.setState({[name]: value});
+            this.setState({ [name]: value });
         };
     }
 
     render() {
-        const {inputWithValue, inputWithoutValue, inputWithError, inputGetTheValue} = this.state;
+        const { inputWithValue, inputWithoutValue, inputWithError, inputGetTheValue } = this.state;
         return (
             <form>
                 <h3>Input with value</h3>
                 <Input name='inputWithValue' value={inputWithValue} onChange={this.onChangeInput('inputWithValue')} />
 
                 <h3>Without value</h3>
-                <Input placeholder='Put your value here...' name='inputWithoutValue' value={inputWithoutValue} onChange={this.onChangeInput('inputWithoutValue')}/>
+                <Input placeholder='Put your value here...' name='inputWithoutValue' value={inputWithoutValue} onChange={this.onChangeInput('inputWithoutValue')} />
 
                 <h3>Input with error</h3>
-                <Input value='Lorem Ipsum' error="Hey! you've done someting wrong!" name='inputWithError' value={inputWithError} onChange={this.onChangeInput('inputWithError')}/>
+                <Input value='Lorem Ipsum' error="Hey! you've done someting wrong!" name='inputWithError' value={inputWithError} onChange={this.onChangeInput('inputWithError')} />
 
                 <h3>Get the value</h3>
-                <Input value='Lorem Ipsum' ref='myInputText' name='inputGetTheValue' value={inputGetTheValue} onChange={this.onChangeInput('inputGetTheValue')}/>
-                <button className='mdl-button mdl-js-button mdl-button--raised mdl-button--colored' onClick={() => {alert(this.refs.myInputText.getValue())}}>
+                <Input value='Lorem Ipsum' ref='myInputText' name='inputGetTheValue' value={inputGetTheValue} onChange={this.onChangeInput('inputGetTheValue')} />
+                <button className='mdl-button mdl-js-button mdl-button--raised mdl-button--colored' onClick={() => { alert(this.refs.myInputText.getValue()) }}>
                     Get the input value
                 </button>
             </form>
@@ -35,4 +36,4 @@ class InputTextExample extends React.Component {
     }
 }
 
-module.exports = InputTextExample;
+export default InputTextExample;
