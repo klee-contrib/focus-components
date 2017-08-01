b3d55f862b2f397893d682808b632183fd0415bc
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2017-08-01 16:55:10 +0200

    clean empty

--- src/components/empty/index.js
+++ src/components/empty/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const Empty = () => <div data-focus='empty'></div>;
+const Empty = () => (
+    <div data-focus='empty' />
+);
 
 Empty.displayName = 'Empty';
 
-module.exports = Empty;
+export default Empty;
