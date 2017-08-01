90d659b8edf03121f266ee7e2d81b95767b4dd91
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2017-08-01 17:01:25 +0200

    clean icon

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,24 +1,32 @@
 import React, { PropTypes } from 'react';
 import filterProps from '../../utils/filter-html-attributes';
 
-function Icon({name, library, onClick, style}) {
+function Icon({ name, library, onClick, style }) {
     switch (library) {
         case 'material':
-            return <i className='material-icons' onClick={onClick} {...filterProps(style)}>{name}</i>;
+            return (
+                <i className='material-icons' onClick={onClick} {...filterProps(style) }>{name}</i>
+            );
         case 'font-awesome':
-            return <i className={`fa fa-${name}`} onClick={onClick} {...filterProps(style)} />;
+            return (
+                <i className={`fa fa-${name}`} onClick={onClick} {...filterProps(style) } />
+            );
         case 'font-custom':
-            return <span className={`icon-${name}`} {...filterProps(style)}/>;
+            return (
+                <span className={`icon-${name}`} {...filterProps(style) } />
+            );
         default:
             return null;
     }
-} 
+}
 
 Icon.displayName = 'Icon';
+
 Icon.defaultProps = {
     name: '',
     library: 'material'
 };
+
 Icon.propTypes = {
     onClick: PropTypes.func,
     library: PropTypes.oneOf(['material', 'font-awesome', 'font-custom']),
