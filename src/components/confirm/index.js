4a8540f4607eb9b7ab8c8099f5ad41a80aaf0602
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2017-08-01 16:46:44 +0200

    clean confirm

--- src/components/confirm/index.js
+++ src/components/confirm/index.js
@@ -1,10 +1,12 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import {isString} from 'lodash/lang';
-import {component as ConfirmationModal} from '../../application/confirmation-popin';
-import Connect from '../../behaviours/store/connect';
+import { isString } from 'lodash/lang';
+import { component as ConfirmationModal } from '../../application/confirmation-popin';
+import connect from '../../behaviours/store/connect';
 import application from 'focus-core/application';
-const {builtInStore: applicationStore} = application;
+
+const { builtInStore: applicationStore } = application;
+
 const propTypes = {
     isVisible: PropTypes.bool,
     ConfirmContentComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
@@ -17,24 +19,44 @@ const defaultProps = {
     ConfirmContentComponent: null
 };
 
-
-@Connect(
-    [{store: applicationStore, properties: ['confirmConfig']}],
+const connector = connect(
+    [{
+        store: applicationStore,
+        properties: ['confirmConfig']
+    }],
     () => {
-        const {isVisible = false, Content: ConfirmContentComponent = null, handleCancel: cancelHandler, handleConfirm: confirmHandler, ...contentProps} = applicationStore.getConfirmConfig() || {};
-        return {isVisible, ConfirmContentComponent, cancelHandler, confirmHandler, contentProps};
+        const {
+            isVisible = false,
+            Content: ConfirmContentComponent = null,
+            handleCancel: cancelHandler,
+            handleConfirm: confirmHandler,
+            ...contentProps
+        } = applicationStore.getConfirmConfig() || {};
+
+        return { isVisible, ConfirmContentComponent, cancelHandler, confirmHandler, contentProps };
     }
 )
-class ConfirmWrapper extends Component {
-    render() {
-        const {isVisible, ConfirmContentComponent, cancelHandler, confirmHandler, contentProps} = this.props;
-        const ConfirmContent = isString(ConfirmContentComponent) ? (() => <span>{ConfirmContentComponent}</span>) : ConfirmContentComponent;
-        return isVisible ? <ConfirmationModal open cancelHandler={cancelHandler} confirmHandler={confirmHandler} {...contentProps}>{ConfirmContent ? <ConfirmContent /> : null}</ConfirmationModal> : null;
-    }
+
+const ConfirmWrapper = () => {
+    const { isVisible, ConfirmContentComponent, cancelHandler, confirmHandler, contentProps } = this.props;
+    const ConfirmContent = isString(ConfirmContentComponent)
+        ? () => (<span>{ConfirmContentComponent}</span>)
+        : ConfirmContentComponent;
+
+    return isVisible ? (
+        <ConfirmationModal
+            open
+            cancelHandler={cancelHandler}
+            confirmHandler={confirmHandler}
+            {...contentProps}
+        >
+            {ConfirmContent ? (<ConfirmContent />) : null}
+        </ConfirmationModal>
+    ) : null;
 }
 
 ConfirmWrapper.propTypes = propTypes;
 ConfirmWrapper.defaultProps = defaultProps;
 ConfirmWrapper.displayName = 'ConfirmWrapper';
 
-export default ConfirmWrapper;
+export default connector(ConfirmWrapper);
