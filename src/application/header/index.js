9ef4f1f854a9926c3f883e8aa19c1510314d0683
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2017-08-04 15:31:12 +0200

    clean application/header

--- src/application/header/index.js
+++ src/application/header/index.js
@@ -1,13 +1,14 @@
 import builder from 'focus-core/component/builder';
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom'
 import type from 'focus-core/component/types';
-const {pluck, sortBy} = require('lodash/collection');
-const applicationStateBehaviour = require('./mixin/application-state');
+import { pluck, sortBy } from 'lodash/collection';
+import applicationStateBehaviour from './mixin/application-state';
+
 const headerMixin = {
     mixins: [applicationStateBehaviour],
     /** @inheriteddoc */
-    getDefaultProps: function getMenuDefaultProps() {
+    getDefaultProps() {
         return {
             /**
             * Selector for the domNode on which the scroll is attached.
@@ -29,11 +30,11 @@ const headerMixin = {
             * @type {Object}
             */
             sizeMap: {
-                'small': {
-                    'sizeBorder': 5
+                small: {
+                    sizeBorder: 5
                 },
-                'medium': {
-                    'sizeBorder': 0
+                medium: {
+                    sizeBorder: 0
                 }
             },
             /**
@@ -58,7 +59,7 @@ const headerMixin = {
         processSize: type(['func', 'object'])
     },
     /** @inheritdoc */
-    getInitialState: function getMenuDefaultState() {
+    getInitialState() {
         /** @inheriteddoc */
         return {
             open: this.props.open,
@@ -67,26 +68,28 @@ const headerMixin = {
     },
 
     /** @inheriteddoc */
-    componentWillMount: function barWillMount() {
+    componentWillMount() {
         this._processSizes();
-        this.scrollTargetNode = (this.props.scrollTargetSelector && this.props.scrollTargetSelector !== '') ? document.querySelector(this.props.scrollTargetSelector) : window;
+        this.scrollTargetNode = (this.props.scrollTargetSelector && this.props.scrollTargetSelector !== '')
+            ? document.querySelector(this.props.scrollTargetSelector)
+            : window;
     },
     /** @inheriteddoc */
-    componentDidMount: function barDidMount() {
+    componentDidMount() {
         this.attachScrollListener();
     },
     /** @inheriteddoc */
-    componentWillUnmount: function barWillUnMount() {
+    componentWillUnmount() {
         this.detachScrollListener();
         this.appStateWillUnmount();
     },
     /**
     * Process the sizeMap in order to sort them by border size and create a sizes array.
     */
-    _processSizes: function processSizes() {
-        var sizes = [];
-        for(var sz in this.props.sizeMap) {
-            sizes.push({name: sz, sizeBorder: this.props.sizeMap[sz].sizeBorder});
+    _processSizes() {
+        let sizes = [];
+        for (let sz in this.props.sizeMap) {
+            sizes.push({ name: sz, sizeBorder: this.props.sizeMap[sz].sizeBorder });
         }
         this.sizes = pluck(sortBy(sizes, 'sizeBorder'), 'name');
     },
@@ -94,14 +97,14 @@ const headerMixin = {
     * Get the current element size.
     * @returns {int} - The size in pixel of the current element in the browser.
     */
-    _processElementSize: function processElementSize() {
+    _processElementSize() {
         return ReactDOM.findDOMNode(this).offsetHeight;
     },
     /**
     * Get the scroll position from the top of the screen.
     * @returns {int} - The position in pixel from the top of the scroll container.
     */
-    _getScrollPosition: function getScrollPosition() {
+    _getScrollPosition() {
         //The pageYOffset is done in order to deal with the window case. Another possibility would have been to use window.docment.body as a node for scrollTop.
         //But the scrollListener on the page is only on the window element.
         return this.scrollTargetNode.pageYOffset !== undefined ? this.scrollTargetNode.pageYOffset : this.scrollTargetNode.scrollTop;
@@ -109,8 +112,8 @@ const headerMixin = {
     /**
     * Notify other elements that the size has changed.
     */
-    _notifySizeChange: function notifySizeChanged() {
-        if(this.props.notifySizeChange) {
+    _notifySizeChange() {
+        if (this.props.notifySizeChange) {
             this.props.notifySizeChange(this.state.size);
         }
     },
@@ -119,32 +122,32 @@ const headerMixin = {
     * @param {string} newSize - The new size.
     * @returns {undefined} -  A way to stop the propagation.
     */
-    _changeSize: function changeSize(newSize) {
+    _changeSize(newSize) {
         // Todo: see if the notification of the changed size can be called before.
-        return this.setState({size: newSize}, this._notifySizeChange);
+        return this.setState({ size: newSize }, this._notifySizeChange);
     },
     /**
     * Process the size in order to know if the size should be changed depending on the scroll position and the border of each zone.
     * @returns {object} - The return is used to stop the treatement.
     */
-    _processSize: function _processSize() {
+    _processSize() {
         //Allow the user to redefine the process size function.
-        if(this.props.processSize) {
+        if (this.props.processSize) {
             return this.props.processSize();
         }
-        var currentIndex = this.sizes.indexOf(this.state.size);
-        var currentScrollPosition = this._getScrollPosition();
+        let currentIndex = this.sizes.indexOf(this.state.size);
+        let currentScrollPosition = this._getScrollPosition();
         //Process increase treatement.
-        if(currentIndex < (this.sizes.length - 1)) {
-            var increaseBorder = this.props.sizeMap[this.sizes[currentIndex + 1]].sizeBorder;
-            if(currentScrollPosition > increaseBorder) {
+        if (currentIndex < (this.sizes.length - 1)) {
+            let increaseBorder = this.props.sizeMap[this.sizes[currentIndex + 1]].sizeBorder;
+            if (currentScrollPosition > increaseBorder) {
                 return this._changeSize(this.sizes[currentIndex + 1]);
             }
         }
         //Process decrease treatement.
-        if(currentIndex > 0) {
-            var decreaseBorder = this.props.sizeMap[this.sizes[currentIndex - 1]].sizeBorder;
-            if(currentScrollPosition <= decreaseBorder) {
+        if (currentIndex > 0) {
+            let decreaseBorder = this.props.sizeMap[this.sizes[currentIndex - 1]].sizeBorder;
+            if (currentScrollPosition <= decreaseBorder) {
                 return this._changeSize(this.sizes[currentIndex - 1]);
             }
         }
@@ -153,14 +156,14 @@ const headerMixin = {
     * Handle the scroll event in order to resize the page.
     * @param {object} event [description]
     */
-    handleScroll: function handleScrollEvent(event) {
+    handleScroll(event) {
         this._processSize();
     },
 
     /**
     * Attach scroll listener on the scroll target node.
     */
-    attachScrollListener: function attachScrollListener() {
+    attachScrollListener() {
         this.scrollTargetNode.addEventListener('scroll', this.handleScroll);
         this.scrollTargetNode.addEventListener('resize', this.handleScroll);
     },
@@ -168,19 +171,21 @@ const headerMixin = {
     /**
     * Detach scroll handler on the scroll target node.
     */
-    detachScrollListener: function detachScrollListener() {
+    detachScrollListener() {
         this.scrollTargetNode.removeEventListener('scroll', this.handleScroll);
         this.scrollTargetNode.removeEventListener('resize', this.handleScroll);
     },
     /** @inheriteddoc */
-    render: function renderBar() {
+    render() {
         const className = `header-${this.state.size}`;
         return (
             <header className={className} data-focus='header' data-route={this.state.route} data-mode={this.state.mode} data-size={this.state.size}>
-            {this.props.children}
+                {this.props.children}
             </header>
         );
     }
 };
 
-module.exports = builder(headerMixin);
+const { mixin, component } = builder(headerMixin);
+export { mixin, component };
+export default { mixin, component };
