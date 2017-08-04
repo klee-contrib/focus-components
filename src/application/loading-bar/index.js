8b1acb35f8e6b395e73c1c47a204f7d272173ba9
Cédric BOULETKESSLER <cedric.bouletkessler@gmail.com>
2017-08-04 15:37:16 +0200

    clean application/loading-bar

--- src/application/loading-bar/index.js
+++ src/application/loading-bar/index.js
@@ -1,10 +1,9 @@
 import builder from 'focus-core/component/builder';
-let React = require('react');
-import type from 'focus-core/component/types';
-import {builtInStore as requestStore} from 'focus-core/network';
-let assign = require('object-assign');
-let ProgressBar = require('../../common/progress-bar').component;
-let Icon = require('../../common/icon').component;
+import React from 'react';
+import { builtInStore as requestStore } from 'focus-core/network';
+import { component as ProgressBar } from '../../common/progress-bar';
+import { component as Icon } from '../../common/icon';
+
 let LoadingBarMixin = {
     /** @inheriteddoc */
     getInitialState() {
@@ -16,25 +15,25 @@ let LoadingBarMixin = {
         requestStore.addClearRequestsListener(this._handleClearRequests);
     },
     /** @inheriteddoc */
-    componentWillUnmount: function cartridgeWillUnMount() {
+    componentWillUnmount() {
         requestStore.removeUpdateRequestListener(this._handleRequestsUpdate);
         requestStore.removeClearRequestsListener(this._handleClearRequests);
     },
-    _getStateFromStore: function getCartridgeStateFromStore() {
+    _getStateFromStore() {
         return requestStore.getRequests();
     },
-    _handleRequestsUpdate: function _handlePushMessage(messageId) {
+    _handleRequestsUpdate(messageId) {
         this.setState(this._getStateFromStore());
     },
     _handleClearRequests() {
-        this.setState({requests: {}});
+        this.setState({ requests: {} });
     },
     /** @inheriteddoc */
-    render: function renderProgressBar() {
-        var completed = +((this.state.total - this.state.pending)/this.state.total)*100;
-        var visible = false;
-        const {displayDevBar} = this.props;
-        if(completed < 100) {
+    render() {
+        let completed = +((this.state.total - this.state.pending) / this.state.total) * 100;
+        let visible = false;
+        const { displayDevBar } = this.props;
+        if (completed < 100) {
             visible = true;
         }
         //Else empty the loading list?
@@ -43,11 +42,11 @@ let LoadingBarMixin = {
                 {visible && <ProgressBar completed={completed} />}
                 {displayDevBar &&
                     <ul className='fa-ul'>
-                        <li><Icon name='swap_vert'/> pending {this.state.pending}</li>
-                        <li><Icon name='not_interested'/> cancelled {this.state.cancelled}</li>
-                        <li><Icon name='check_circle'/> success {this.state.success}</li>
-                        <li><Icon name='error'/> error {this.state.error}</li>
-                        <li><Icon name='functions'/> total {this.state.total}</li>
+                        <li><Icon name='swap_vert' /> pending {this.state.pending}</li>
+                        <li><Icon name='not_interested' /> cancelled {this.state.cancelled}</li>
+                        <li><Icon name='check_circle' /> success {this.state.success}</li>
+                        <li><Icon name='error' /> error {this.state.error}</li>
+                        <li><Icon name='functions' /> total {this.state.total}</li>
                     </ul>
                 }
             </div>
@@ -55,4 +54,6 @@ let LoadingBarMixin = {
     }
 };
 
-module.exports = builder(LoadingBarMixin);
+const { mixin, component } = builder(LoadingBarMixin);
+export { mixin, component };
+export default { mixin, component };
