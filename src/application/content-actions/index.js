c8707d3d3dd71c5e76b1d606c0c2f3d61457a312
c3dr0x <cedric.bouletkessler@gmail.com>
2017-09-28 16:37:57 +0200

    add iconLibrary options to content-actions

--- src/application/content-actions/index.js
+++ src/application/content-actions/index.js
@@ -1,3 +1,6 @@
+// Libs
+import isArray from 'lodash/lang/isArray';
+import isObject from 'lodash/lang/isObject';
 // Dependencies
 import builder from 'focus-core/component/builder';
 // Stores
@@ -9,19 +12,24 @@ import Button from '../../components/button';
 import { component as Dropdown } from '../../common/select-action';
 
 const ContentActions = {
+
     mixins: [stylableBehaviour],
-    /** @inheriteddoc */
+
+    /** @inheritdoc */
     getInitialState() {
         return this._getStateFromStore();
     },
-    /** @inheriteddoc */
+
+    /** @inheritdoc */
     componentWillMount() {
         applicationStore.addActionsChangeListener(this._handleComponentChange);
     },
-    /** @inheriteddoc */
+
+    /** @inheritdoc */
     componentWillUnmount() {
         applicationStore.removeActionsChangeListener(this._handleComponentChange);
     },
+
     /**
      * Get state from store
      * @return {Object} actions extracted from the store
@@ -31,29 +39,56 @@ const ContentActions = {
             actions: applicationStore.getActions() || { primary: [], secondary: [] }
         };
     },
+
     /**
      * Component change handler
      */
     _handleComponentChange() {
         this.setState(this._getStateFromStore());
     },
-    /** @inheriteddoc */
+
+    /** @inheritdoc */
     render() {
-        const { actions } = this.state;
+        const { actions: { primary, secondary } } = this.state;
         return (
             <div className={this._getStyleClassName()} data-focus='content-actions'>
-                {actions.primary.map((primary) => {
+                {primary.map((primary) => {
                     if (Array.isArray(primary.action)) {
                         return (
-                            <Dropdown iconProps={{ name: primary.icon }} operationList={primary.action} shape='fab' />
+                            <Dropdown
+                                iconProps={{ name: primary.icon, iconLibrary: primary.iconLibrary }}
+                                operationList={primary.action}
+                                shape='fab'
+                            />
                         );
                     } else {
                         return (
-                            <Button handleOnClick={primary.action} icon={primary.icon} label={primary.label} shape='fab' style={{ className: primary.className }} type='button' />
+                            <Button
+                                handleOnClick={primary.action}
+                                icon={primary.icon}
+                                iconLibrary={primary.iconLibrary}
+                                label={primary.label}
+                                shape='fab'
+                                style={{ className: primary.className }}
+                                type='button'
+                            />
                         );
                     }
                 })}
-                <Dropdown iconProps={{ name: 'more_vert' }} operationList={actions.secondary} shape='fab' />
+                {isArray(secondary) && (
+                    <Dropdown
+                        iconProps={{ name: 'more_vert' }}
+                        operationList={secondary}
+                        shape='fab'
+                    />
+                )}
+                {isObject(secondary) && (
+                    <Dropdown
+                        iconProps={{ name: secondary.icon || 'more_vert', iconLibrary: secondary.iconLibrary }}
+                        operationList={secondary.action}
+                        shape='fab'
+                    />
+                )}
             </div>
         );
     }
