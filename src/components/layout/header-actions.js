ad74f0b5230b587fa29486acca6dd1ec26a9f58f
c3dr0x <cedric.bouletkessler@gmail.com>
2017-10-28 14:19:01 +0200

    Refactor comp & fix CSS
    Fix default props deep for icon-dropdown
    Rewrite header-actions in function style
    Fix CSS for fabs positioning (go flex)

--- src/components/layout/header-actions.js
+++ src/components/layout/header-actions.js
@@ -1,7 +1,7 @@
 // Libs
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 import isObject from 'lodash/lang/isObject';
-
+import filterProps from '../../utils/filter-html-attributes';
 // Stores
 import applicationStore from 'focus-core/application/built-in-store';
 // Components
@@ -9,70 +9,104 @@ import Button from '../../components/button';
 import Dropdown from '../../components/icon-dropdown';
 import storeConnect from '../../behaviours/store/connect';
 
-/**
-* HeaderActions component.
-*/
-@storeConnect([
-    { store: applicationStore, properties: ['actions'] }
-], () => ({ actions: applicationStore.getActions() || { primary: [], secondary: [] } }))
-class HeaderActions extends Component {
+/** Connector for HeaderActions. */
+const connector = storeConnect([{
+    store: applicationStore,
+    properties: ['actions']
+}], () => ({
+    actions: applicationStore.getActions() || { primary: [], secondary: [] }
+}));
 
-    /** Display name. */
-    static displayName = 'HeaderActions';
+/** Sub-action type. */
+const subActionType = PropTypes.arrayOf(PropTypes.shape({
+    label: PropTypes.string.isRequired,
+    action: PropTypes.func.isRequired
+}));
 
-    /**
-     * Render a list fab component.
-     * @param {object} fab Fab.
-     * @returns {JSXElement} Component.
-     */
-    renderFabListAction(fab) {
-        if (Array.isArray(fab.action) && fab.action.length > 0) {
-            const { icon, iconLibrary, action, ...otherProps } = fab;
-            return (
-                <Dropdown
-                    iconProps={{ name: icon, iconLibrary }}
-                    operationList={action}
-                    shape='fab'
-                    {...otherProps}
-                />
-            );
-        }
+/** Action type. */
+const actionType = PropTypes.oneOfType([
+    { // Icon action
+        label: PropTypes.string.isRequired,
+        action: PropTypes.func.isRequired,
+        icon: PropTypes.string.isRequired,
+        iconLibrary: PropTypes.string,
+        className: PropTypes.string
+    },
+    { // Dropdown action
+        icon: PropTypes.string,
+        iconLibrary: PropTypes.string,
+        action: subActionType.isRequired
     }
+]);
 
-    /**
-     * Render a fab component.
-     * @param {object} fab Fab.
-     * @returns {JSXElement} Component.
-     */
-    renderFabAction(fab) {
-        const { action, className, icon, iconLibrary, label, ...otherProps } = fab;
+/** Proptypes validation for HeaderActions. */
+const propTypes = {
+    actions: PropTypes.shape({
+        primary: PropTypes.arrayOf(actionType),
+        secondary: PropTypes.oneOfType([
+            subActionType,
+            actionType
+        ])
+    }).isRequired
+};
+
+/**
+ * Render a list fab component.
+ * @param {object} fab Fab.
+ * @returns {JSXElement} Component.
+ */
+function renderFabListAction(fab) {
+    if (Array.isArray(fab.action) && fab.action.length > 0) {
+        const { icon, iconLibrary, action, ...otherProps } = fab;
         return (
-            <Button
-                key={`header-action-${label}`}
-                className={className}
-                handleOnClick={action}
-                icon={icon}
-                iconLibrary={iconLibrary}
-                label={label}
+            <Dropdown
+                iconProps={{ name: icon, iconLibrary }}
+                operationList={action}
                 shape='fab'
-                type='button'
                 {...otherProps}
             />
         );
     }
-
-    /** @inheritdoc */
-    render() {
-        const { actions: { primary, secondary }, others } = this.props;
-
-        return (
-            <div data-focus='header-actions' {...others}>
-                {primary.map((action) => action && Array.isArray(action.action) ? this.renderFabListAction(action) : this.renderFabAction(action))}
-                {Array.isArray(secondary) && this.renderFabListAction({ action: secondary })}
-                {isObject(secondary) && this.renderFabListAction(secondary)}
-            </div>
-        );
-    }
 }
 
-export default HeaderActions;
+/**
+ * Render a fab component.
+ * @param {object} fab Fab.
+ * @returns {JSXElement} Component.
+ */
+function renderFabAction(fab) {
+    const { action, className, icon, iconLibrary, label, ...otherProps } = fab;
+    return (
+        <Button
+            key={`header-action-${label}`}
+            className={className}
+            handleOnClick={action}
+            icon={icon}
+            iconLibrary={iconLibrary}
+            label={label}
+            shape='fab'
+            type='button'
+            {...otherProps}
+        />
+    );
+}
+
+/**
+ * HeaderActions component.
+ * @returns {JSXElement} Component.
+ */
+const HeaderActions = ({ actions: { primary, secondary }, ...otherProps }) => {
+    return (
+        <div data-focus='header-actions' {...filterProps(otherProps)}>
+            {primary.map((action) => {
+                return action && Array.isArray(action.action) ? renderFabListAction(action) : renderFabAction(action)
+            })}
+            {Array.isArray(secondary) && renderFabListAction({ action: secondary })}
+            {isObject(secondary) && renderFabListAction(secondary)}
+        </div>
+    );
+};
+HeaderActions.displayName = 'HeaderActions';
+HeaderActions.propTypes = propTypes;
+
+export default connector(HeaderActions);
