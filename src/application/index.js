bfe34f8b964677eb9cd98c67bc22afca4f6cbb21
Bazire Houssin <bazire.houssin@kleegroup.com>
2017-11-09 14:26:00 +0100

    Cleaning deprecated components

--- src/application/index.js
+++ src/application/index.js
@@ -1,21 +1,13 @@
-import header from './header';
-import bar from './bar';
-import cartridge from './cartridge';
-import menu from './menu';
 import popin from './popin';
 import confirmationPopin from './confirmation-popin';
-import messageCenter from './message-center';
 import errorCenter from './error-center';
-import contentBar from './content-bar';
-import contentActions from './content-actions';
-import layout from './layout';
 import loadingBar from './loading-bar';
 
 export {
-    header, bar, cartridge, menu, popin, confirmationPopin, messageCenter,
-    errorCenter, contentBar, contentActions, layout, loadingBar
+    popin, confirmationPopin,
+    errorCenter, loadingBar
 };
 export default {
-    header, bar, cartridge, menu, popin, confirmationPopin, messageCenter,
-    errorCenter, contentBar, contentActions, layout, loadingBar
+    popin, confirmationPopin,
+    errorCenter, loadingBar
 };
